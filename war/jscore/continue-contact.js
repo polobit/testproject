b3007c96b96a24957ab7d455783a4e072f3f7e1b
Rammohan K <rammohan@invox.com>
2012-08-29 16:56:46 +0530

    Filter contacts multiple add and remove.

--- war/jscore/continue-contact.js
+++ war/jscore/continue-contact.js
@@ -154,4 +154,17 @@ $(function () {
             trigger: true
         });
     });
+    
+    // Filter Contacts- Clone Multiple 
+    $("i.filter-contacts-multiple-add").die().live('click', function (e) {
+    	var htmlContent = $(this).closest("tr").clone();
+    	$(htmlContent).find("i.filter-contacts-multiple-remove").css("display", "block");
+    	$(this).parents("tbody").append(htmlContent);
+    });
+   
+    // Filter Contacts- Remove Multiple
+    $("i.filter-contacts-multiple-remove").die().live('click', function (e) {
+ 
+    		$(this).closest("tr").remove();
+    });
 });
