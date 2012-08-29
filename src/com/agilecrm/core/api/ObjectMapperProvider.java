daec64f89909d90319c787d9267627a9ba43d2bb
Manohar Chapalamadugu <chapalamadugu@me.com>
2012-08-29 14:47:01 +0530

    Added default mapper

---
+++ src/com/agilecrm/core/api/ObjectMapperProvider.java
@@ -0,0 +1,26 @@
+package com.agilecrm.core.api;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.ObjectMapper;
+
+@Provider
+public class ObjectMapperProvider implements ContextResolver<ObjectMapper>
+{
+   ObjectMapper mapper;
+
+   public ObjectMapperProvider(){
+       mapper = new ObjectMapper();
+       mapper.configure(
+    	DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+   }
+
+   @Override
+   public ObjectMapper getContext(Class<?> type) {
+       return mapper;
+   }
+}
+
