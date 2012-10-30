a2bbea51ff1b3145654428b4f9a3353c5c594eb1
yaswanth praveen <praveen@invox.com>
2012-10-30 19:54:04 +0530

    pad-content correction

--- war/jscore/empty-collection.js
+++ war/jscore/empty-collection.js
@@ -1,5 +1,6 @@
 
 function fillSlate(id, el)
 {
-	$("#" + id, el).html(getTemplate("empty-collection-model", CONTENT_JSON[Current_Route]));
+	if(CONTENT_JSON[Current_Route])
+		$("#" + id, el).html(getTemplate("empty-collection-model", CONTENT_JSON[Current_Route]));
 }
