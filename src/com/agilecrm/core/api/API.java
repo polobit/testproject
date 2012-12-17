5d1533b282ceb920c948e5a5b58a28697b8abd96
yaswanth praveen <praveen@invox.com>
2012-12-17 09:55:37 +0530

    Cutom Views code cleaned up and documented

--- src/com/agilecrm/core/api/API.java
+++ src/com/agilecrm/core/api/API.java
@@ -7,10 +7,8 @@ import java.util.Map;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -19,12 +17,9 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.agilecrm.account.APIKey;
-import com.agilecrm.contact.ContactView;
 import com.agilecrm.core.DomainUser;
 import com.agilecrm.user.AgileUser;
 import com.agilecrm.user.IMAPEmailPrefs;
@@ -36,322 +31,251 @@ import com.agilecrm.util.Util;
 import com.google.appengine.api.NamespaceManager;
 
 @Path("/api")
-public class API
-{
+public class API {
 
-    // This method is called if TEXT_PLAIN is request
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String sayPlainTextHello()
-    {
-	return "Invalid Path";
-    }
-
-    // Send Current User Info
-    @Path("current-user")
-    @GET
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public DomainUser getCurrentUser()
-    {
-	try
-	{
-	    DomainUser domainUser = DomainUser.getDomainCurrentUser();
-	    System.out.println(domainUser);
-	    return domainUser;
-	}
-	catch (Exception e)
-	{
-	    e.printStackTrace();
-	    return null;
-	}
-    }
-
-    // Logs
-
-    // User Prefs -
-
-    // Register
-    @Path("domain-availability/{domain}")
-    @POST
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public Map<String, String> register(@PathParam("email") String domain)
-    {
-
-	// Get Users for this domain to see if it is free
-	List<DomainUser> usersList = DomainUser.getUsers(domain);
-
-	if (!usersList.isEmpty())
-	{
-	    Hashtable<String, String> result = new Hashtable<String, String>();
-	    result.put("error", "domain does not exist");
-	    return result;
+	// This method is called if TEXT_PLAIN is request
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayPlainTextHello() {
+		return "Invalid Path";
 	}
 
-	Hashtable<String, String> result = new Hashtable<String, String>();
-	result.put("success", "domain available");
-
-	// Set the session variable to register
-	return result;
-    }
-
-    @Path("send-email")
-    @POST
-    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public void createEmail(@QueryParam("from") String fromEmail, @QueryParam("to") String to,
-	    @QueryParam("subject") String subject, @QueryParam("body") String body)
-    {
-	try
-	{
-	    Util.sendMail(fromEmail, fromEmail, to, subject, fromEmail, body, null);
-	}
-	catch (Exception e)
-	{
-
-	    e.printStackTrace();
-	}
-    }
-
-    // API Key
-    // This method is called if TEXT_PLAIN is request
-    @Path("api-key")
-    @GET
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public APIKey getAPIKey()
-    {
-	return APIKey.getAPIKey();
-    }
-
-    // Get List of Contact Views Author: Yaswanth 08-10-2012
-    @Path("contact-view")
-    @GET
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public List<ContactView> getContactViews()
-    {
-	return ContactView.getContactViewList();
-
-    }
-
-    // Contact view Save Author: Yaswanth 08-10-2012
-    @Path("contact-view")
-    @POST
-    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public ContactView createContactView(ContactView contact_view)
-    {
-	contact_view.save();
-	return contact_view;
-
-    }
-
-    // Contact view Save Author: Yaswanth 08-10-2012
-    @Path("contact-view")
-    @PUT
-    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public ContactView updateContactView(ContactView contact_view)
-    {
-	System.out.println("updateing view");
-	contact_view.save();
-	return contact_view;
-
-    }
-
-    // Get Contact View by id Author: Yaswanth 09-02-2012
-    @Path("contact-view/{id}")
-    @GET
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public ContactView getContactView(@PathParam("id") Long id)
-    {
-	return ContactView.getContactView(id);
-
-    }
-
-    // Bulk delete Contact Views by id Author: Yaswanth 11-08-2012
-    @Path("contact-view/delete/bulk")
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public void deleteBulkContactViews(@FormParam("model_ids") String model_ids)
-	    throws JSONException
-    {
-	JSONArray ContactViewsJSONArray = new JSONArray(model_ids);
-	ContactView.dao.deleteBulkByIds(ContactViewsJSONArray);
-    }
-
-    // Contact view Save Author: Yaswanth 08-10-2012
-    @Path("stats")
-    @GET
-    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    @Produces({ MediaType.APPLICATION_JSON })
-    public String getStats(@QueryParam("e") String searchEmail)
-    {
-
-	// Get API Key
-	APIKey api = APIKey.getAPIKey();
-	String apiKey = api.api_key;
-
-	// Hit Stats Server
-	String url = "https://stats.agilecrm.com:90/get?email=" + searchEmail + "&agile_id="
-		+ apiKey;
-
-	return Util.accessURL(url);
-    }
-
-    // Contact view Save Author: Yaswanth 08-10-2012
-    @Path("email")
-    @GET
-    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    @Produces({ MediaType.APPLICATION_JSON })
-    public String getEmails(@QueryParam("e") String searchEmail, @QueryParam("c") String count,
-	    @QueryParam("o") String offset)
-    {
-
-	String url = null;
-
-	// Get Imap Prefs
-	IMAPEmailPrefs imapPrefs = IMAPEmailPrefs.getIMAPPrefs(AgileUser.getCurrentAgileUser());
-	if (imapPrefs != null)
-	{
-	    String userName = imapPrefs.user_name;
-	    String host = imapPrefs.server_name;
-	    String password = imapPrefs.password;
-	    String port = "993";
-
-	    // URL Encode Params
-	    userName = URLEncoder.encode(userName);
-	    host = URLEncoder.encode(host);
-	    password = URLEncoder.encode(password);
-	    port = URLEncoder.encode(port);
-
-	    url = "http://stats.agilecrm.com:8080/AgileCRMEmail/imap?user_name=" + userName
-		    + "&search_email=" + searchEmail + "&host=" + host + "&port=" + port
-		    + "&offset=" + offset + "&count=" + count + "&command=imap_email&password="
-		    + password;
-	}
-	else
-	{
-	    // Get Gmail Social Prefs
-	    Type socialPrefsTypeEnum = SocialPrefs.Type.GMAIL;
-	    SocialPrefs gmailPrefs = SocialPrefs.getPrefs(AgileUser.getCurrentAgileUser(),
-		    socialPrefsTypeEnum);
-
-	    if (gmailPrefs != null)
-	    {
-
-		String userName = gmailPrefs.email;
-		String host = "imap.gmail.com";
-		String port = "993";
-		String consumerKey = "anonymous";
-		String consumerSecret = "anonymous";
-
-		String oauth_key = gmailPrefs.token;
-		String oauth_secret = gmailPrefs.secret;
-
-		// URL Encode Params
-		userName = URLEncoder.encode(userName);
-		host = URLEncoder.encode(host);
-		port = URLEncoder.encode(port);
-		consumerKey = URLEncoder.encode(consumerKey);
-		consumerSecret = URLEncoder.encode(consumerSecret);
-		oauth_key = URLEncoder.encode(oauth_key);
-		oauth_secret = URLEncoder.encode(oauth_secret);
-
-		url = "http://stats.agilecrm.com:8080/AgileCRMEmail/imap?command=oauth_email&user_name="
-			+ userName
-			+ "&search_email="
-			+ searchEmail
-			+ "&host="
-			+ host
-			+ "&port="
-			+ port
-			+ "&offset="
-			+ offset
-			+ "&count="
-			+ count
-			+ "&consumer_key="
-			+ consumerKey
-			+ "&consumer_secret="
-			+ consumerSecret
-			+ "&oauth_key="
-			+ oauth_key + "&oauth_secret=" + oauth_secret;
-	    }
+	// Send Current User Info
+	@Path("current-user")
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public DomainUser getCurrentUser() {
+		try {
+			DomainUser domainUser = DomainUser.getDomainCurrentUser();
+			System.out.println(domainUser);
+			return domainUser;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
-	String jsonResult = "";
-	if (url != null)
-	    jsonResult = Util.accessURL(url);
+	// Logs
 
-	if (url == null || jsonResult == null)
-	{
-	    // If url is null throw exception to configure email prefs
-	    if (url == null)
-		throw new WebApplicationException(
-			Response.status(Response.Status.BAD_REQUEST)
-				.entity("You have not yet configured your email. Please click <a href='#email'>here</a> to get started.")
-				.build());
-	    else
-		throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
-			.entity("No Emails.").build());
+	// User Prefs -
+
+	// Register
+	@Path("domain-availability/{domain}")
+	@POST
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Map<String, String> register(@PathParam("email") String domain) {
+
+		// Get Users for this domain to see if it is free
+		List<DomainUser> usersList = DomainUser.getUsers(domain);
+
+		if (!usersList.isEmpty()) {
+			Hashtable<String, String> result = new Hashtable<String, String>();
+			result.put("error", "domain does not exist");
+			return result;
+		}
+
+		Hashtable<String, String> result = new Hashtable<String, String>();
+		result.put("success", "domain available");
+
+		// Set the session variable to register
+		return result;
+	}
+
+	@Path("send-email")
+	@POST
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public void createEmail(@QueryParam("from") String fromEmail,
+			@QueryParam("to") String to, @QueryParam("subject") String subject,
+			@QueryParam("body") String body) {
+		try {
+			Util.sendMail(fromEmail, fromEmail, to, subject, fromEmail, body,
+					null);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+	}
+
+	// API Key
+	// This method is called if TEXT_PLAIN is request
+	@Path("api-key")
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public APIKey getAPIKey() {
+		return APIKey.getAPIKey();
+	}
+
+	// Contact view Save Author: Yaswanth 08-10-2012
+	@Path("stats")
+	@GET
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public String getStats(@QueryParam("e") String searchEmail) {
+
+		// Get API Key
+		APIKey api = APIKey.getAPIKey();
+		String apiKey = api.api_key;
+
+		// Hit Stats Server
+		String url = "https://stats.agilecrm.com:90/get?email=" + searchEmail
+				+ "&agile_id=" + apiKey;
+
+		return Util.accessURL(url);
+	}
+
+	// Contact view Save Author: Yaswanth 08-10-2012
+	@Path("email")
+	@GET
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public String getEmails(@QueryParam("e") String searchEmail,
+			@QueryParam("c") String count, @QueryParam("o") String offset) {
+
+		String url = null;
+
+		// Get Imap Prefs
+		IMAPEmailPrefs imapPrefs = IMAPEmailPrefs.getIMAPPrefs(AgileUser
+				.getCurrentAgileUser());
+		if (imapPrefs != null) {
+			String userName = imapPrefs.user_name;
+			String host = imapPrefs.server_name;
+			String password = imapPrefs.password;
+			String port = "993";
+
+			// URL Encode Params
+			userName = URLEncoder.encode(userName);
+			host = URLEncoder.encode(host);
+			password = URLEncoder.encode(password);
+			port = URLEncoder.encode(port);
+
+			url = "http://stats.agilecrm.com:8080/AgileCRMEmail/imap?user_name="
+					+ userName
+					+ "&search_email="
+					+ searchEmail
+					+ "&host="
+					+ host
+					+ "&port="
+					+ port
+					+ "&offset="
+					+ offset
+					+ "&count="
+					+ count + "&command=imap_email&password=" + password;
+		} else {
+			// Get Gmail Social Prefs
+			Type socialPrefsTypeEnum = SocialPrefs.Type.GMAIL;
+			SocialPrefs gmailPrefs = SocialPrefs.getPrefs(
+					AgileUser.getCurrentAgileUser(), socialPrefsTypeEnum);
+
+			if (gmailPrefs != null) {
+
+				String userName = gmailPrefs.email;
+				String host = "imap.gmail.com";
+				String port = "993";
+				String consumerKey = "anonymous";
+				String consumerSecret = "anonymous";
+
+				String oauth_key = gmailPrefs.token;
+				String oauth_secret = gmailPrefs.secret;
+
+				// URL Encode Params
+				userName = URLEncoder.encode(userName);
+				host = URLEncoder.encode(host);
+				port = URLEncoder.encode(port);
+				consumerKey = URLEncoder.encode(consumerKey);
+				consumerSecret = URLEncoder.encode(consumerSecret);
+				oauth_key = URLEncoder.encode(oauth_key);
+				oauth_secret = URLEncoder.encode(oauth_secret);
+
+				url = "http://stats.agilecrm.com:8080/AgileCRMEmail/imap?command=oauth_email&user_name="
+						+ userName
+						+ "&search_email="
+						+ searchEmail
+						+ "&host="
+						+ host
+						+ "&port="
+						+ port
+						+ "&offset="
+						+ offset
+						+ "&count="
+						+ count
+						+ "&consumer_key="
+						+ consumerKey
+						+ "&consumer_secret="
+						+ consumerSecret
+						+ "&oauth_key="
+						+ oauth_key + "&oauth_secret=" + oauth_secret;
+			}
+		}
+
+		String jsonResult = "";
+		if (url != null)
+			jsonResult = Util.accessURL(url);
+
+		if (url == null || jsonResult == null) {
+			// If url is null throw exception to configure email prefs
+			if (url == null)
+				throw new WebApplicationException(
+						Response.status(Response.Status.BAD_REQUEST)
+								.entity("You have not yet configured your email. Please click <a href='#email'>here</a> to get started.")
+								.build());
+			else
+				throw new WebApplicationException(Response
+						.status(Response.Status.BAD_REQUEST)
+						.entity("No Emails.").build());
+
+		}
+
+		return jsonResult;
 
 	}
 
-	return jsonResult;
+	// Get Agile Users
+	@Path("agileusers")
+	@GET
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public List<AgileUser> getAgileUsers() {
 
-    }
-
-    // Get Agile Users
-    @Path("agileusers")
-    @GET
-    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    @Produces({ MediaType.APPLICATION_JSON })
-    public List<AgileUser> getAgileUsers()
-    {
-
-	return AgileUser.getUsers();
-    }
-
-    // Get All Users
-    @Path("deal-owners")
-    @GET
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public List<UserPrefs> getAllUserPrefs()
-    {
-	return UserPrefs.getAllUserPrefs();
-    }
-
-    // Get Stats
-    @Path("stats2")
-    @GET
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public JSONObject getStats()
-    {
-	return Util.getNamespaceCount();
-    }
-
-    // Get Stats
-    @Path("namespace-stats")
-    @GET
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public String getNamespaceStats()
-    {
-	return Util.getNamespaceStats().toString();
-    }
-
-    /**
-     * Delete subscription object of the domain and deletes related customer
-     */
-    @Path("delete/account")
-    @DELETE
-    public void deleteAccount()
-    {
-	try
-	{
-	    DBUtil.deleteNamespace(NamespaceManager.get());
+		return AgileUser.getUsers();
 	}
-	catch (Exception e)
-	{
-	    throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
-		    .entity(e.getMessage()).build());
+
+	// Get All Users
+	@Path("deal-owners")
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public List<UserPrefs> getAllUserPrefs() {
+		return UserPrefs.getAllUserPrefs();
 	}
-    }
+
+	// Get Stats
+	@Path("stats2")
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public JSONObject getStats() {
+		return Util.getNamespaceCount();
+	}
+
+	// Get Stats
+	@Path("namespace-stats")
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public String getNamespaceStats() {
+		return Util.getNamespaceStats().toString();
+	}
+
+	/**
+	 * Delete subscription object of the domain and deletes related customer
+	 */
+	@Path("delete/account")
+	@DELETE
+	public void deleteAccount() {
+		try {
+			DBUtil.deleteNamespace(NamespaceManager.get());
+		} catch (Exception e) {
+			throw new WebApplicationException(Response
+					.status(Response.Status.BAD_REQUEST).entity(e.getMessage())
+					.build());
+		}
+	}
 }
---
+++ war/jscore/contacts/custom-view/contact-view.js
@@ -0,0 +1,154 @@
+/**
+ * contactTableView is customized function (customization of appendedItem
+ * function in Base_Collection_View), when custom view is selected, this
+ * function is called whenever a new contact is added or contact list is fetched
+ * (called on each contact model from Base_collection_View render function) .
+ * when a custom view is selected then collection view is initialized with
+ * modelData option for view is "custom view" object, since model data is custom
+ * view object, it includes fields_set which defines the order of the files
+ * (first_name, last_name, job title, organization..etc). This function iterates
+ * through fields_set in CustomView object and loads the template according to
+ * the field (Template are set for each field with name 'contacts-custom-view-' +
+ * field name) and creates a view model template which is appended to collection
+ * template.
+ * 
+ * @param base_model
+ */
+function contactTableView(base_model) {
+
+	// Creates list view for
+	var itemView = new Base_List_View({
+		model : base_model,
+		template : 'contacts-custom-view-model',
+		tagName : this.options.individual_tag_name
+	});
+
+	// Reads the modelData (customView object)
+	var modelData = this.options.modelData;
+
+	// Reads fields_set from modelData
+	var fields = modelData['fields_set'];
+
+	// Converts base_model (contact) in to JSON
+	var contact = base_model.toJSON();
+
+	// Clears the template, because all the fields are appended, has to be reset
+	// for each contact
+	$('#contacts-custom-view-model-template').empty();
+
+	// Iterates through, each field name and appends the field according to
+	// order of the fields
+	$.each(fields, function(index, field_name) {
+		$('#contacts-custom-view-model-template').append(
+				getTemplate('contacts-custom-view-' + field_name, contact));
+	});
+
+	// Appends model to model-list template in collection template
+	$(('#contacts-custom-view-model-list'), this.el).append(
+			itemView.render().el);
+
+	// Sets data to tr
+	$(('#contacts-custom-view-model-list'), this.el).find('tr:last').data(
+			base_model);
+
+}
+
+/**
+ * Gets the list of custom fields saved by the user, and shown in the Html
+ * element with "view-list" in the Html element sent to this method. It fetches
+ * the list of custom fields and on rendering the collection unordered list of
+ * created and appended in view-list element in contacts page. If custom view
+ * selected from the list, this function is called with button name from the
+ * customView function, which is set on the list button.
+ * 
+ * @param cel
+ *            html element
+ * @param button_name
+ *            name of the button (name of the view)
+ */
+function setupViews(cel, button_name) {
+
+	// Creates a view for custom views
+	var customView = new Base_Collection_View({
+		url : 'core/api/contact-view',
+		restKey : "contactView",
+		templateKey : "contact-view",
+		individual_tag_name : 'li',
+		id : 'view-list',
+		postRenderCallback : function(el) {
+
+			// If button_name is defined, then view is selected then the name of
+			// the view is show in the custom view button.
+			if (button_name)
+				$(el).find('.custom_view').append(button_name);
+		}
+
+	});
+
+	// Fetches the list of custom fields, and shows is the the contact page
+	customView.collection.fetch({
+		success : function() {
+			$("#view-list", cel).html(customView.el);
+		}
+	})
+}
+
+/**
+ * Init function to define actions on events on the custom view list
+ * 
+ */
+$(function() {
+
+	/*
+	 * If any custom view is selected, sets the cookie saves the id of the
+	 * custom view, to show custom view even after refreshing. Also Load the
+	 * contacts with custom view by reading the custom view id from the element
+	 * which is selected and calls customView function is called to to custom
+	 * view of contacts
+	 */
+	$('.ContactView').die().live(
+			'click',
+			function(e) {
+
+				e.preventDefault();
+
+				// Gets id of the view
+				var id = $(this).attr('id');
+
+				// Saves contact_view id as cookie, so on refreshing shows the
+				// custom view based on the cookie, and cookie deleted if
+				// default view is selected
+				createCookie("contact_view", id);
+
+				/*
+				 * Even when custom view is selected, have to check if user sets
+				 * any filter so custom view should be shown on the filter
+				 * results instead of showing view on default contacts, so if
+				 * contact_filter cookie is set the sets the url to filter with
+				 * filter id from cookie, then results are fetched from custom
+				 * views
+				 */
+				if (filter_id = readCookie('contact_filter')) {
+					App_Contacts.customView(id, undefined,
+							'core/api/filters/query/' + filter_id);
+					return;
+				}
+
+				// If filter is not set then show view on the default contacts
+				// list
+				App_Contacts.customView(id);
+			});
+
+	// If default view is selected, contacts are loaded with default view and
+	// removes the view cookie set when view is selected
+	$('.DefaultView').die().live('click', function(e) {
+		e.preventDefault();
+
+		// Erases the cookie
+		eraseCookie("contact_view");
+
+		// Loads the contacts
+		App_Contacts.contacts();
+
+	});
+});
