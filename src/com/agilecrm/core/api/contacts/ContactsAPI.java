84d69679aac08d0d2d20101c18a04f72409ab103
yaswanth praveen <praveen@invox.com>
2012-12-17 18:29:43 +0530

    Contacts import documented

--- src/com/agilecrm/core/api/contacts/ContactsAPI.java
+++ src/com/agilecrm/core/api/contacts/ContactsAPI.java
@@ -34,356 +34,334 @@ import com.agilecrm.deals.Opportunity;
 import com.agilecrm.util.Util;
 
 @Path("/api/contacts")
-public class ContactsAPI
-{
+public class ContactsAPI {
 
-    // This method is called if TEXT_PLAIN is request
-    @GET
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public List<Contact> getContacts(@QueryParam("cursor") String cursor,
-	    @QueryParam("page_size") String count)
-    {
-	if (count != null)
-	{
+	// This method is called if TEXT_PLAIN is request
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public List<Contact> getContacts(@QueryParam("cursor") String cursor,
+			@QueryParam("page_size") String count) {
+		if (count != null) {
 
-	    System.out.println("Fetching page by page");
-	    return Contact.getAllContacts(Integer.parseInt(count), cursor);
+			System.out.println("Fetching page by page");
+			return Contact.getAllContacts(Integer.parseInt(count), cursor);
+		}
+
+		return Contact.getAllContacts();
 	}
 
-	return Contact.getAllContacts();
-    }
+	// This method is called if TEXT_PLAIN is request
+	@Path("/companies")
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public List<Contact> getCompanies(@QueryParam("cursor") String cursor,
+			@QueryParam("page_size") String count) {
+		if (count != null) {
 
-    // This method is called if TEXT_PLAIN is request
-    @Path("/companies")
-    @GET
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public List<Contact> getCompanies(@QueryParam("cursor") String cursor,
-	    @QueryParam("page_size") String count)
-    {
-	if (count != null)
-	{
+			System.out.println("Fetching companies page by page");
+			return Contact.getAllCompanies(Integer.parseInt(count), cursor);
+		}
 
-	    System.out.println("Fetching companies page by page");
-	    return Contact.getAllCompanies(Integer.parseInt(count), cursor);
+		return Contact.getAllContacts();
 	}
 
-	return Contact.getAllContacts();
-    }
+	@POST
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public Contact createContact(Contact contact) {
 
-    @POST
-    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public Contact createContact(Contact contact)
-    {
+		// Check if the email exists with the current email address
+		Contact currentContact = Contact.searchContactByEmail(contact
+				.getContactFieldValue("EMAIL"));
 
-	// Check if the email exists with the current email address
-	Contact currentContact = Contact.searchContactByEmail(contact
-		.getContactFieldValue("EMAIL"));
+		// Throw non-200 if it exists
+		if (currentContact != null) {
+			throw new WebApplicationException(
+					Response.status(Response.Status.BAD_REQUEST)
+							.entity("Sorry, duplicate contact found with the same email address.")
+							.build());
+		}
 
-	// Throw non-200 if it exists
-	if (currentContact != null)
-	{
-	    throw new WebApplicationException(
-		    Response.status(Response.Status.BAD_REQUEST)
-			    .entity("Sorry, duplicate contact found with the same email address.")
-			    .build());
+		contact.save();
+		return contact;
 	}
 
-	contact.save();
-	return contact;
-    }
+	/**
+	 * Accepts list of contacts, and save the list of contacts iterating through
+	 * each contact
+	 * 
+	 * @param contacts
+	 *            {@link List} of {@link Contact}
+	 * @return {@link List} of contacts
+	 */
+	@Path("multi/upload")
+	@POST
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public List<Contact> createMultipleContact(List<Contact> contacts) {
+		for (Contact contact : contacts)
+			contact.save();
 
-    @Path("multi/upload")
-    @POST
-    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public List<Contact> createMultipleContact(List<Contact> contacts)
-    {
-	for (Contact contact : contacts)
-	    contact.save();
-
-	return contacts;
-    }
-
-    // File Upload
-    @Path("upload")
-    // @Consumes(MediaType.MULTIPART_FORM_DATA)
-    @POST
-    public String post(@Context HttpServletRequest request)
-    {
-	try
-	{
-
-	    String filename = request.getHeader("X-File-Name");
-	    InputStream file = request.getInputStream();
-
-	    String csv = IOUtils.toString(file);
-	    System.out.println(csv);
-
-	    JSONObject success = new JSONObject();
-	    success.put("success", true);
-
-	    Hashtable result = Util.convertCSVToJSONArray2(csv, "Email");
-	    JSONArray csvArray = (JSONArray) result.get("result");
-
-	    success.put("data", csvArray);
-
-	    return success.toString();
-	}
-	catch (Exception e)
-	{
-	    e.printStackTrace();
+		return contacts;
 	}
 
-	return null;
-    }
+	// File Upload
+	/**
+	 * Handle request sent using file uploader, reads the details from the
+	 * uploaded file are returns the data which is processed and stored in to
+	 * map, so fields can be shown at the client side using the map
+	 * 
+	 * @param request
+	 *            {@link HttpServletRequest}
+	 * @return {@link String}
+	 */
+	@Path("upload")
+	// @Consumes(MediaType.MULTIPART_FORM_DATA)
+	@POST
+	public String post(@Context HttpServletRequest request) {
+		try {
 
-    @PUT
-    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public Contact updateContact(Contact contact)
-    {
-	contact.save();
-	return contact;
-    }
+			// Reads data from the request object
+			InputStream file = request.getInputStream();
 
-    @Path("/{contact-id}")
-    @DELETE
-    public void deleteContact(@PathParam("contact-id") Long id)
-    {
-	Contact contact = Contact.getContact(id);
-	if (contact != null)
-	    contact.delete();
-    }
+			// Converts the inputsream in to a string
+			String csv = IOUtils.toString(file);
 
-    // This method is called if XML is request
-    @Path("/{contact-id}")
-    @GET
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public Contact getContact(@PathParam("contact-id") Long id)
-    {
-	Contact contact = Contact.getContact(id);
-	return contact;
-    }
+			JSONObject success = new JSONObject();
+			success.put("success", true);
 
-    // Opportunities of contact in contact details
-    @Path("/{contact-id}/deals")
-    @GET
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public List<Opportunity> getCurrentContactOpportunity(
-	    @PathParam("contact-id") Long id)
-    {
-	return Opportunity.getCurrentContactDeals(id);
-    }
+			// Stores results in to a map
+			Hashtable result = Util.convertCSVToJSONArray2(csv, "Email");
+			JSONArray csvArray = (JSONArray) result.get("result");
 
-    // Notes of contact in contact details
-    @Path("/{contact-id}/tasks")
-    @GET
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public List<Task> getTasks(@PathParam("contact-id") Long id)
-    {
-	try
-	{
-	    return TaskUtil.getContactTasks(id);
-	}
-	catch (Exception e)
-	{
-	    e.printStackTrace();
-	    return null;
-	}
-    }
+			// returns CSV file as a json object with key "data"
+			success.put("data", csvArray);
 
-    // Notes
-    @Path("/{contact-id}/notes")
-    @GET
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public List<Note> getNotes(@PathParam("contact-id") Long id)
-    {
-	try
-	{
-	    return Note.getNotes(id);
-	}
-	catch (Exception e)
-	{
-	    e.printStackTrace();
-	    return null;
-	}
-    }
+			return success.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-    // Delete Notes
-    @Path("/{contact-id}/notes/{id}")
-    @DELETE
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public void deleteNote(@PathParam("contact-id") Long contactId,
-	    @PathParam("id") Long noteId)
-    {
-	try
-	{
-	    Note.deleteNote(noteId, contactId);
-	}
-	catch (Exception e)
-	{
-	    e.printStackTrace();
-	}
-    }
-
-    // This method is called if XML is request
-    @Path("/search/email/{email}")
-    @GET
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public Contact searchContactByEmail(@PathParam("email") String email)
-    {
-	return Contact.searchContactByEmail(email);
-    }
-
-    // Searching contacts by emaillist
-    @Path("/search/email")
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces({ MediaType.APPLICATION_JSON })
-    public List<Contact> searchContactsByEmailList(
-	    @FormParam("email_ids") String email_ids) throws JSONException
-    {
-	JSONArray contactsJSONArray = new JSONArray(email_ids);
-	List<Contact> contacts_list = new ArrayList<Contact>();
-	for (int i = 0; i < contactsJSONArray.length(); i++)
-	{
-	    try
-	    {
-		Contact contactDetails = Contact
-			.searchContactByEmail(contactsJSONArray.getString(i));
-		contacts_list.add(contactDetails);
-	    }
-	    catch (JSONException e)
-	    {
-		e.printStackTrace();
-	    }
-	}
-	return contacts_list;
-    }
-
-    /**
-     * Deletes selected contacts based on ids
-     * 
-     * @param model_ids
-     *            array of contact ids as String
-     * @throws JSONException
-     */
-    @Path("bulk")
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public void deleteContacts(@FormParam("model_ids") String model_ids)
-	    throws JSONException
-    {
-
-	JSONArray contactsJSONArray = new JSONArray(model_ids);
-
-	for (int i = 0; i < contactsJSONArray.length(); i++)
-	{
-	    Contact contact = Contact.getContact(Long
-		    .parseLong(contactsJSONArray.getString(i)));
-
-	    if (contact != null)
-		contact.delete();
+		return null;
 	}
 
-    }
+	@PUT
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public Contact updateContact(Contact contact) {
+		contact.save();
+		return contact;
+	}
 
-    /**
-     * Change the owner of selected contacts
-     * 
-     * @param contact_ids
-     *            array of contact ids as String
-     * @param new_owner
-     *            id of new owner (DomainUser id)
-     * 
-     * @throws JSONException
-     */
-    @Path("bulk/owner/{new_owner}")
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public void changeOwnerToContacts(
-	    @FormParam("contact_ids") String contact_ids,
-	    @PathParam("new_owner") String new_owner) throws JSONException
-    {
-	JSONArray contactsJSONArray = new JSONArray(contact_ids);
-	Contact.changeOwnerToContactsBulk(contactsJSONArray, new_owner);
-    }
+	@Path("/{contact-id}")
+	@DELETE
+	public void deleteContact(@PathParam("contact-id") Long id) {
+		Contact contact = Contact.getContact(id);
+		if (contact != null)
+			contact.delete();
+	}
 
-    /**
-     * Add tags to selected contacts
-     * 
-     * @param contact_ids
-     *            array of contact ids as String
-     * @param tagsString
-     *            array of tags as string
-     * @throws JSONException
-     */
-    @Path("bulk/{tags}")
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public void addTagsTOContacts(@FormParam("contact_ids") String contact_ids,
-	    @PathParam("tags") String tagsString) throws JSONException
-    {
+	// This method is called if XML is request
+	@Path("/{contact-id}")
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Contact getContact(@PathParam("contact-id") Long id) {
+		Contact contact = Contact.getContact(id);
+		return contact;
+	}
 
-	JSONArray contactsJSONArray = new JSONArray(contact_ids);
-	String tags_array[] = tagsString.split(",");
-	Contact.addTagsToContactsBulk(contactsJSONArray, tags_array);
-    }
+	// Opportunities of contact in contact details
+	@Path("/{contact-id}/deals")
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public List<Opportunity> getCurrentContactOpportunity(
+			@PathParam("contact-id") Long id) {
+		return Opportunity.getCurrentContactDeals(id);
+	}
 
-    // Bulk operations - delete tasks bulk related to a contact
-    /**
-     * Deletes all selected tasks for a particular contact
-     * 
-     * @param model_ids
-     *            array of task ids as String
-     * @throws JSONException
-     */
-    @Path("/tasks/bulk")
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public void deleteTasks(@FormParam("model_ids") String model_ids)
-	    throws JSONException
-    {
+	// Notes of contact in contact details
+	@Path("/{contact-id}/tasks")
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public List<Task> getTasks(@PathParam("contact-id") Long id) {
+		try {
+			return TaskUtil.getContactTasks(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-	JSONArray tasksJSONArray = new JSONArray(model_ids);
-	Task.dao.deleteBulkByIds(tasksJSONArray);
-    }
+	// Notes
+	@Path("/{contact-id}/notes")
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public List<Note> getNotes(@PathParam("contact-id") Long id) {
+		try {
+			return Note.getNotes(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-    // Bulk operations - delete notes bulk related to a contact
-    /**
-     * Deletes all selected notes of a particular contact.
-     * 
-     * @param model_ids
-     *            array of note ids as String
-     * @throws JSONException
-     */
-    @Path("/notes/bulk")
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public void deleteNotes(@FormParam("model_ids") String model_ids)
-	    throws JSONException
-    {
+	// Delete Notes
+	@Path("/{contact-id}/notes/{id}")
+	@DELETE
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public void deleteNote(@PathParam("contact-id") Long contactId,
+			@PathParam("id") Long noteId) {
+		try {
+			Note.deleteNote(noteId, contactId);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-	JSONArray notesJSONArray = new JSONArray(model_ids);
-	Note.dao.deleteBulkByIds(notesJSONArray);
-    }
+	// This method is called if XML is request
+	@Path("/search/email/{email}")
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Contact searchContactByEmail(@PathParam("email") String email) {
+		return Contact.searchContactByEmail(email);
+	}
 
-    // Bulk operations - delete notes bulk related to a contact
-    /**
-     * Deletes all selected deals of a particular contact
-     * 
-     * @param model_ids
-     *            array of deal ids as String
-     * @throws JSONException
-     */
-    @Path("/deals/bulk")
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public void deleteDeals(@FormParam("model_ids") String model_ids)
-	    throws JSONException
-    {
-	JSONArray dealsJSONArray = new JSONArray(model_ids);
-	Note.dao.deleteBulkByIds(dealsJSONArray);
-    }
+	// Searching contacts by emaillist
+	@Path("/search/email")
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces({ MediaType.APPLICATION_JSON })
+	public List<Contact> searchContactsByEmailList(
+			@FormParam("email_ids") String email_ids) throws JSONException {
+		JSONArray contactsJSONArray = new JSONArray(email_ids);
+		List<Contact> contacts_list = new ArrayList<Contact>();
+		for (int i = 0; i < contactsJSONArray.length(); i++) {
+			try {
+				Contact contactDetails = Contact
+						.searchContactByEmail(contactsJSONArray.getString(i));
+				contacts_list.add(contactDetails);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		return contacts_list;
+	}
+
+	/**
+	 * Deletes selected contacts based on ids
+	 * 
+	 * @param model_ids
+	 *            array of contact ids as String
+	 * @throws JSONException
+	 */
+	@Path("bulk")
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void deleteContacts(@FormParam("model_ids") String model_ids)
+			throws JSONException {
+
+		JSONArray contactsJSONArray = new JSONArray(model_ids);
+
+		for (int i = 0; i < contactsJSONArray.length(); i++) {
+			Contact contact = Contact.getContact(Long
+					.parseLong(contactsJSONArray.getString(i)));
+
+			if (contact != null)
+				contact.delete();
+		}
+
+	}
+
+	/**
+	 * Change the owner of selected contacts
+	 * 
+	 * @param contact_ids
+	 *            array of contact ids as String
+	 * @param new_owner
+	 *            id of new owner (DomainUser id)
+	 * 
+	 * @throws JSONException
+	 */
+	@Path("bulk/owner/{new_owner}")
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void changeOwnerToContacts(
+			@FormParam("contact_ids") String contact_ids,
+			@PathParam("new_owner") String new_owner) throws JSONException {
+		JSONArray contactsJSONArray = new JSONArray(contact_ids);
+		Contact.changeOwnerToContactsBulk(contactsJSONArray, new_owner);
+	}
+
+	/**
+	 * Add tags to selected contacts
+	 * 
+	 * @param contact_ids
+	 *            array of contact ids as String
+	 * @param tagsString
+	 *            array of tags as string
+	 * @throws JSONException
+	 */
+	@Path("bulk/{tags}")
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void addTagsTOContacts(@FormParam("contact_ids") String contact_ids,
+			@PathParam("tags") String tagsString) throws JSONException {
+
+		JSONArray contactsJSONArray = new JSONArray(contact_ids);
+		String tags_array[] = tagsString.split(",");
+		Contact.addTagsToContactsBulk(contactsJSONArray, tags_array);
+	}
+
+	// Bulk operations - delete tasks bulk related to a contact
+	/**
+	 * Deletes all selected tasks for a particular contact
+	 * 
+	 * @param model_ids
+	 *            array of task ids as String
+	 * @throws JSONException
+	 */
+	@Path("/tasks/bulk")
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void deleteTasks(@FormParam("model_ids") String model_ids)
+			throws JSONException {
+
+		JSONArray tasksJSONArray = new JSONArray(model_ids);
+		Task.dao.deleteBulkByIds(tasksJSONArray);
+	}
+
+	// Bulk operations - delete notes bulk related to a contact
+	/**
+	 * Deletes all selected notes of a particular contact.
+	 * 
+	 * @param model_ids
+	 *            array of note ids as String
+	 * @throws JSONException
+	 */
+	@Path("/notes/bulk")
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void deleteNotes(@FormParam("model_ids") String model_ids)
+			throws JSONException {
+
+		JSONArray notesJSONArray = new JSONArray(model_ids);
+		Note.dao.deleteBulkByIds(notesJSONArray);
+	}
+
+	// Bulk operations - delete notes bulk related to a contact
+	/**
+	 * Deletes all selected deals of a particular contact
+	 * 
+	 * @param model_ids
+	 *            array of deal ids as String
+	 * @throws JSONException
+	 */
+	@Path("/deals/bulk")
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void deleteDeals(@FormParam("model_ids") String model_ids)
+			throws JSONException {
+		JSONArray dealsJSONArray = new JSONArray(model_ids);
+		Note.dao.deleteBulkByIds(dealsJSONArray);
+	}
 }
--- war/jscore/contacts/import.js
+++ war/jscore/contacts/import.js
@@ -1,5 +1,42 @@
+/**
+ * Reads the file and sends request to to URL given, using fileuploader.js.
+ * Element is required to be specified, on clicking on the element file selector
+ * window is opened, after selecting the file to be uploaded request is sent to
+ * the URL specified as action attribute. This function using file uploader
+ * sends request to "core/api/contacts/upload" which processes the request and
+ * returns data as a map, is shown in a template using handlebars.
+ */
+function fileUploadInit() {
+	var uploader = new qq.FileUploader({
+		element : document.getElementById('file-upload-div'),
+		action : '/core/api/contacts/upload',
+		debug : true,
+		onComplete : function(id, fileName, data) {
+
+			// import-contacts template is populated with the processed data
+			$('#content').html(getTemplate("import-contacts-2", data));
+
+			// Sets up typeahead for tags
+			setup_tags_typeahead();
+		}
+	});
+
+}
+
+
+/**
+ * Defines actions on events on imports contacts element, which does validation 
+ * on the import template, whether contact have first_name last_name which are 
+ * mandatory fields. If first naRme and last name are not specified or specified 
+ * same label for different fields then error message is shown and will not send 
+ * request to save.
+ */
 $(function(){
 	
+	/* On clicking on import contacts it checks whether all mandatory field are 
+	 * set and ensure no duplicate field names are set. If all the validation 
+	 * checks are passed, the request the backend to save the contacts
+	 */
 	 $('#import-contacts').die().live('click', function (e) {
 		 
 		 	var models = [];
@@ -11,6 +48,12 @@ $(function(){
 		 	$(".lname-duplicate-error").hide();
 		 
 		 	// Headings validation Rammohan: 10-09-12
+		 	/*
+		 	 * Reads all the table heading set after importing contacts list from 
+		 	 * CSV and ensures that first_name and last_name fields are set, which
+		 	 * are mandatory fields. Checks if duplicate table headings are set. If
+		 	 * validations failed the error alerts a explaining the cause are shown
+		 	 */
 		 	var firstNameCount = $(".import-select").filter(function() {
 				 return $(this).val() == "first_name";
 			 });
@@ -36,23 +79,29 @@ $(function(){
 		 		return false;
 		 	}
 		 	
-		 	// Show Please wait
+		 	/*
+		 	 * After validation checks are passed then loading is shown
+		 	 */
 		 	$waiting =  $('<div style="display:inline-block;padding-left:5px"><small><p class="text-success"><i><span id="status-message">Please wait</span></i></p></small></div>');
     		$waiting.insertAfter($('#import-contacts'));
     		
-	        // Iterate through all tbody tr
+    		/*
+    		 * Iterates through all tbody tr's and reads the table heading from the 
+    		 * table, push the table name as property name and value as property 
+    		 * value as ContactField properties.
+    		 */
 	        $('#import-tbody tr').each(function () {
 	            var properties = [];
 	            
-
 	            $(this).find("td").each(function (i) {
 
+	            	// Empty property map (Represents ContactField in contact.java)
 	                var property = {};
+	                
+	                // Read the name of the property from table heading
 	                var name = $(this).parents('table').find('th').eq(i).find('select').val();
-	                console.log($(this));
-	                //console.log("Name is " + name);
-
-
+	                
+	                // Reads the sub type of the fields
 	                if (name.indexOf("-") != -1) {
 	                    var splits = name.split("-");
 	                    name = splits[1];
@@ -63,14 +112,14 @@ $(function(){
 	                var value = $(this).html();
 	                //console.log("Column value is " + value);
 
+	                // Set the value and name fields
 	                property["value"] = value;
 	                property["name"] = name;
 
+	                // Push in to properties array (represents ContactField array)
 	                properties.push(property);
 
 	            });
-
-	            //console.log(properties);
 
 	            var model = {};
 	            model.properties = properties;
@@ -82,6 +131,7 @@ $(function(){
 	            var tags = get_tags('import-contact-tags');
 	            if (tags != undefined) model.tags = tags[0].value;
 
+	            // Pushes model (represents contact) in to an array of models 
 	            models.push(model);
 
 	        });
@@ -89,12 +139,14 @@ $(function(){
 	        console.log(models);
 	        
 	        
-	        // Show Updating
+	        // Shows Updating
 	        $waiting.find('#status-message').html('Uploading to server');
 
 	        var contact = models;
 	        //contact.contact = models;
 
+	        // List of contacts built out of csv, post request is sent to 
+	        // 'core/api/contacts/multi/upload' with list of contacts
 	        $.ajax({
 	            type: 'POST',
 	            url: '/core/api/contacts/multi/upload',
@@ -102,15 +154,14 @@ $(function(){
 	            contentType: "application/json; charset=utf-8",
 	            success: function (data) {
 	                
-	            	
+	            	// After saving the contacts, loading is removed
 	            	$waiting.find('#status-message').html('Uploaded successfully');
 	            	
+	            	// Navigates back to contacts
 	            	Backbone.history.navigate('contacts', {
                         trigger: true
                     });
-	            	
-	            	console.log("Uploaded successfully");
-		            	
+
 	                //console.log(data);
 	            	//App_Contacts.contactsListView.collection.add(data.contact);
 	            },
@@ -119,26 +170,14 @@ $(function(){
 
 	    });
 	 
+	 // Cancels import, removes the contacts uploaded in to table, still calls fileUploadInit, 
+	 // so user can upload again if required 
 	 $('#import-cancel').die().live('click', function (e) {
+		 
+		 // Sends empty JSON to remove contact uploaded
 		 $('#content').html(getTemplate("import-contacts", {}));
 	        head.js('lib/fileuploader-min.js', function(){
 	        	fileUploadInit();
 	        });
 	 });
 });
-
-function fileUploadInit() {
-	var uploader = new qq.FileUploader({
-        element: document.getElementById('file-upload-div'),
-        action: '/core/api/contacts/upload',
-        debug: true,
-        onComplete: function (id, fileName, data) {
-
-            console.log(data);
-            $('#content').html(getTemplate("import-contacts-2", data));
-           
-            setup_tags_typeahead(); // ??
-        }
-    });
-
-}
