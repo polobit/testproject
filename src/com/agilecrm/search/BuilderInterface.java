3476ccf71924f8f478155eb2c1c55f40a86f66d4
yaswanth praveen <praveen@invox.com>
2012-12-19 11:32:31 +0530

    Search and query interfaces documented

--- src/com/agilecrm/search/BuilderInterface.java
+++ src/com/agilecrm/search/BuilderInterface.java
@@ -1,12 +1,39 @@
 package com.agilecrm.search;
 
-
+/**
+ * <code> BuilderInterface </code> is root interface to build search on
+ * entities. It includes method declarations to add an entity to search, edit
+ * and delete entities added to search (Appengine search)
+ * 
+ * @author Yaswanth
+ * 
+ * @since November 2012
+ * 
+ */
 public interface BuilderInterface
 {
+    /**
+     * Defines an entity in search, so search is enabled on entity
+     * 
+     * @param entity
+     *            {@link Object}
+     */
     public void add(Object entity);
 
+    /**
+     * Declares edit functionality on an entity in search
+     * 
+     * @param entity
+     */
     public void edit(Object entity);
 
+    /**
+     * Declares delete of an entity from search, removes object form search.
+     * Whenever an entity is deleted from database, this method is called to
+     * remove object from search
+     * 
+     * @param id
+     */
     public void delete(String id);
 
 }
--- src/com/agilecrm/search/QueryInterface.java
+++ src/com/agilecrm/search/QueryInterface.java
@@ -5,9 +5,36 @@ import java.util.List;
 
 import com.agilecrm.search.ui.serialize.SearchRule;
 
+/**
+ * <code>QueryInterface</code> is root interface for building search queries on
+ * any entities. It declares two methods simpleSearch (to search based on
+ * keywords) and an advancedSearch functionality (search based on complex
+ * queries)
+ * 
+ * @author Yaswanth
+ * 
+ * @since November 2012
+ * 
+ */
 public interface QueryInterface
 {
+    /**
+     * Declaration of simple search method, takes keyword as an argument and
+     * searches based on keyword and returns entities
+     * 
+     * @param keyword
+     *            {@link String} searches based in th keyword
+     * @return {@link Collection} of entities returned after querying
+     */
     public Collection<?> simpleSearch(String keyword);
 
+    /**
+     * It declares advancedSearch based on the list of search Rules, should
+     * build queries base on searchRule which includes condition of the query
+     * 
+     * @param rule
+     *            {@link List} of {@link SearchRule}
+     * @return {@link Collection} of entities
+     */
     public Collection<?> advancedSearch(List<SearchRule> rule);
 }
