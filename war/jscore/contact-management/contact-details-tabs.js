e9b1248b40792f02c96795fd1bde2f527ad33f70
sukanyaagile <sukanya@invox.com>
2013-03-01 12:49:16 +0530

    contactus

--- war/jscore/contact-management/contact-details-tabs.js
+++ war/jscore/contact-management/contact-details-tabs.js
@@ -241,18 +241,7 @@ $(function(){
             trigger: true
         });
 	});	
-	
-	//Sends help mail
-	$('#helpMail').die().live('click',function(e){
-		        e.preventDefault();
-				var json = serializeForm("helpmailForm");
-				var url =  'core/api/send-email?from=' + encodeURIComponent(json.from) + '&to=' + 
-				   										 encodeURIComponent(json.to) + '&subject=' + encodeURIComponent(json.subject) + '&body=' + 
-				   										 encodeURIComponent(json.body);
-				$.post(url);
-			
-	});
-	
+
 	/**
 	 * Subscribes the contact to the selected campaign on clicking "Add"
 	 * button and activates the "Timeline" tab to inserts the campaign 
