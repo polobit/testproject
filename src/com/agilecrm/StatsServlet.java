05bbf33abbd397caa6a60c6fb12cf52656f94445
Naresh <naresh@faxdesk.com>
2013-03-29 15:00:45 +0530

    Added methods to run select queries in GoogleSQL and changes to agile-min.js

--- src/com/agilecrm/StatsServlet.java
+++ src/com/agilecrm/StatsServlet.java
@@ -8,10 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import com.agilecrm.googlesql.GoogleSQL;
+import com.agilecrm.db.GoogleSQL;
 import com.google.appengine.api.NamespaceManager;
 
 /**
@@ -40,12 +38,16 @@ public class StatsServlet extends HttpServlet
 	String guid = req.getParameter("guid");
 	String sid = req.getParameter("sid");
 	String url = req.getParameter("url");
+	String email = req.getParameter("email");
 
 	String ip = null;
 	String newOne = null;
 	String ref = null;
 	String userAgent = null;
-	String appHeaderJson = null;
+	String country = null;
+	String region = null;
+	String city = null;
+	String cityLatLong = null;
 
 	// If new
 	if (!StringUtils.isEmpty(req.getParameter("new")))
@@ -58,15 +60,10 @@ public class StatsServlet extends HttpServlet
 	    userAgent = req.getHeader("User-Agent");
 
 	    // App-Engine Headers
-	    String country = req.getHeader("X-AppEngine-Country");
-	    String region = req.getHeader("X-AppEngine-Region");
-	    String city = req.getHeader("X-AppEngine-City");
-	    String cityLatLong = req.getHeader("X-AppEngine-CityLatLong");
-
-	    // Add app-engine headers to json
-	    appHeaderJson = addAppEngineHeadersToJSON(country, region, city,
-		    cityLatLong);
-
+	    country = req.getHeader("X-AppEngine-Country");
+	    region = req.getHeader("X-AppEngine-Region");
+	    city = req.getHeader("X-AppEngine-City");
+	    cityLatLong = req.getHeader("X-AppEngine-CityLatLong");
 	}
 
 	// Get request time
@@ -74,22 +71,18 @@ public class StatsServlet extends HttpServlet
 	long reqTime = date.getTime() / 1000;
 
 	// Insert into table
-	addToPageViews(domain, guid, sid, url, ip, newOne, ref, userAgent,
-		appHeaderJson, reqTime);
+	GoogleSQL.addToPageViews(domain, guid, email, sid, url, ip, newOne,
+		ref, userAgent, country, region, city, cityLatLong, reqTime);
 
 	// If email
 	if (!StringUtils.isEmpty(req.getParameter("email")))
 	{
-	    String gUid = req.getParameter("guid");
-	    String email = req.getParameter("email");
-
-	    // Add to map
-	    addToMap(domain, gUid, email);
+	    GoogleSQL.getFromPageViews(email);
 	}
     }
 
     /**
-     * Gets client IP address using request header 'x-forwarded-for'. If it is
+     * Gets client IP address using request header 'X-Forwarded-For'. If it is
      * not empty, gets ip address from forwarded ip, otherwise gets remote
      * address.
      * 
@@ -101,7 +94,7 @@ public class StatsServlet extends HttpServlet
     {
 	String ipAddress = null;
 
-	String forwardedIpsStr = req.getHeader("x-forwarded-for");
+	String forwardedIpsStr = req.getHeader("X-Forwarded-For");
 
 	// Check if forwardedIps not empty
 	if (!StringUtils.isEmpty(forwardedIpsStr))
@@ -120,120 +113,4 @@ public class StatsServlet extends HttpServlet
 	return ipAddress;
     }
 
-    /**
-     * Updates domain, guid and email in map table.
-     * 
-     * @param domain
-     *            - current namespace.
-     * @param guid
-     *            - guid.
-     * @param email
-     *            - email
-     */
-    private static void addToMap(String domain, String guid, String email)
-    {
-	String insertToMap = "INSERT INTO map (domain,guid,email) VALUES(\'"
-		+ domain + "\'," + guid + "," + email + ")";
-
-	System.out.println("insertToMap query: " + insertToMap);
-
-	try
-	{
-	    GoogleSQL.executeNonQuery(insertToMap);
-	}
-	catch (Exception e)
-	{
-	    e.printStackTrace();
-	}
-    }
-
-    /**
-     * Inserts values into page_views table
-     * 
-     * @param domain
-     *            - current namespace.
-     * @param guid
-     *            - guid.
-     * @param sid
-     *            - sid.
-     * @param url
-     *            - url.
-     * @param ip
-     *            - ip address.
-     * @param newOne
-     *            - if new
-     * @param ref
-     *            -reference.
-     * @param userAgent
-     *            - userAgent header.
-     * @param appHeader
-     *            - appengine header values.
-     * @param time
-     *            - requested time.
-     */
-    private static void addToPageViews(String domain, String guid, String sid,
-	    String url, String ip, String newOne, String ref, String userAgent,
-	    String appHeader, long time)
-    {
-	String insertToPageViews = "INSERT INTO page_views (domain,guid,sid,url,ip,new_one,ref,user_agent,app_header,time) VALUES(\'"
-		+ domain
-		+ "\',"
-		+ guid
-		+ ","
-		+ sid
-		+ ","
-		+ url
-		+ ",\'"
-		+ ip
-		+ "\',"
-		+ newOne
-		+ ","
-		+ ref
-		+ ",\'"
-		+ userAgent
-		+ "\',\'"
-		+ appHeader + "\'," + time + ")";
-
-	try
-	{
-	    GoogleSQL.executeNonQuery(insertToPageViews);
-	}
-	catch (Exception e)
-	{
-	    e.printStackTrace();
-	}
-    }
-
-    /**
-     * Adds app-engine header data to json.
-     * 
-     * @param country
-     *            - country
-     * @param region
-     *            - region
-     * @param city
-     *            - city
-     * @param cityLatLong
-     *            - city latitudes and longitudes.
-     * @return
-     */
-    private static String addAppEngineHeadersToJSON(String country,
-	    String region, String city, String cityLatLong)
-    {
-	JSONObject appHeaders = new JSONObject();
-	try
-	{
-	    appHeaders.put("country", country);
-	    appHeaders.put("region", region);
-	    appHeaders.put("city", city);
-	    appHeaders.put("cityLatLong", cityLatLong);
-	}
-	catch (JSONException e)
-	{
-	    e.printStackTrace();
-	    return null;
-	}
-
-	return appHeaders.toString();
-    }
 }
