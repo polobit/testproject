4533430f9b8b86817b102989320f07d4ca3c63d4
Naresh <naresh@faxdesk.com>
2013-04-01 18:13:41 +0530

    Added URL Visited functionality to campaigns

--- src/com/campaignio/tasklets/agile/URLVisited.java
+++ src/com/campaignio/tasklets/agile/URLVisited.java
@@ -1,14 +1,8 @@
 package com.campaignio.tasklets.agile;
 
-import java.net.URLEncoder;
-
-import org.apache.commons.lang.StringUtils;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.agilecrm.account.APIKey;
-import com.agilecrm.util.DBUtil;
-import com.agilecrm.util.HTTPUtil;
+import com.agilecrm.db.GoogleSQL;
 import com.campaignio.tasklets.TaskletAdapter;
 import com.campaignio.tasklets.util.TaskletUtil;
 
@@ -43,62 +37,20 @@ public class URLVisited extends TaskletAdapter
     {
 	// Get URL value
 	String url = getStringValue(nodeJSON, subscriberJSON, data, URL);
-	String subscriberId = DBUtil.getId(subscriberJSON);
 
-	// Get API Key from campaignJSON domainUser Id.
-	String apiKey = APIKey.getAPIKeyFromCampaignJSON(campaignJSON);
+	// Gets URL count from table.
+	int count = GoogleSQL.getCountForGivenURL(url);
 
-	if (StringUtils.isEmpty(apiKey))
+	if (count == 0)
 	{
-	    // Execute Next One in Loop
+	    log(campaignJSON, subscriberJSON, "URL Not Visited : " + url);
 	    TaskletUtil.executeTasklet(campaignJSON, subscriberJSON, data,
-		    nodeJSON, null);
+		    nodeJSON, BRANCH_NO);
 	    return;
 	}
 
-	url += "&agile_id=" + URLEncoder.encode(apiKey) + "&subscriber_id="
-		+ URLEncoder.encode(subscriberId);
-
-	// Access given URL
-	String output = HTTPUtil.accessURL(url);
-
-	// Checks whether output string can be converted to JSONObject
-	boolean validJSON = isValidJSON(output);
-
-	// Execute Next One in Loop
-	if (validJSON)
-	{
-	    log(campaignJSON, subscriberJSON, "URL Visited : " + url);
-	    TaskletUtil.executeTasklet(campaignJSON, subscriberJSON, data,
-		    nodeJSON, BRANCH_YES);
-	    return;
-	}
-
-	log(campaignJSON, subscriberJSON, "URL Not Visited : " + url);
+	log(campaignJSON, subscriberJSON, "URL Visited : " + url);
 	TaskletUtil.executeTasklet(campaignJSON, subscriberJSON, data,
-		nodeJSON, BRANCH_NO);
-    }
-
-    /**
-     * Returns true if output is converted to JSONObject successfully, otherwise
-     * false
-     * 
-     * @param output
-     *            - Output string obtained by accessing given url.
-     * @return boolean value
-     */
-    private boolean isValidJSON(String output)
-    {
-	try
-	{
-	    JSONObject jsonURL = new JSONObject(output);
-	    System.out.println(jsonURL);
-	    return true;
-	}
-	catch (JSONException e)
-	{
-	    System.out.println("Exception occured : " + e);
-	    return false;
-	}
+		nodeJSON, BRANCH_YES);
     }
 }
