6d0e7babbd975c8090d0750dbac4b9566fd33310
Manohar Chapalamadugu <manohar@invox.com>
2013-04-06 11:28:26 +0530

    Removed the code which was using Twilio TwiML client

--- src/com/agilecrm/TwilioVoiceServlet.java
+++ src/com/agilecrm/TwilioVoiceServlet.java
@@ -6,15 +6,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.twilio.sdk.verbs.Dial;
-import com.twilio.sdk.verbs.Number;
-import com.twilio.sdk.verbs.TwiMLException;
-import com.twilio.sdk.verbs.TwiMLResponse;
-
 public class TwilioVoiceServlet extends HttpServlet
 {
-    public void service(HttpServletRequest request, HttpServletResponse response)
-	    throws IOException
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException
     {
 
 	String phoneNumber = request.getParameter("PhoneNumber");
@@ -24,23 +18,5 @@ public class TwilioVoiceServlet extends HttpServlet
 	/* Use this as the caller ID when making calls from a browser. */
 	String callerId = "+14105551234";
 
-	TwiMLResponse twiml = new TwiMLResponse();
-	Dial dial = new Dial();
-	try
-	{
-	    dial.setCallerId(callerId);
-	    dial.append(new Number(phoneNumber));
-
-	    twiml.append(dial);
-	}
-	catch (TwiMLException e)
-	{
-	    e.printStackTrace();
-	}
-
-	System.out.println(twiml.toXML());
-
-	response.setContentType("application/xml");
-	response.getWriter().print(twiml.toXML());
     }
 }
