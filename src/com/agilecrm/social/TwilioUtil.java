b937870f9dfcba35224a76d00d83da07f7dd0adc
Manohar Chapalamadugu <manohar@invox.com>
2013-04-06 11:29:14 +0530

    Removed the Jersey Client Code to connect to the remote server

--- src/com/agilecrm/social/TwilioUtil.java
+++ src/com/agilecrm/social/TwilioUtil.java
@@ -1,33 +1,11 @@
 package com.agilecrm.social;
 
-import com.twilio.sdk.client.TwilioCapability;
-import com.twilio.sdk.client.TwilioCapability.DomainException;
-
 public class TwilioUtil
 {
 
     public static String generateTwilioToken(String accountSid)
     {
-	// String authToken = "b6420aa8715bad58ad2cff61036b4640";
-	String authToken = "5e7085bb019e378fb18822f319a3ec46";
-
-	TwilioCapability capability = new TwilioCapability(accountSid,
-		authToken);
-
-	capability.allowClientOutgoing("CNf63bca035414be121d517a116066a5f8");
-
-	String token = null;
-	try
-	{
-	    token = capability.generateToken();
-	    System.out.println(token);
-	    return token;
-	}
-	catch (DomainException e)
-	{
-	    System.out.println(e.getMessage());
-	    return "";
-	}
+	return null;
     }
 
 }
--- src/com/agilecrm/social/ZendeskUtil.java
+++ src/com/agilecrm/social/ZendeskUtil.java
@@ -1,17 +1,9 @@
 package com.agilecrm.social;
 
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriBuilder;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.agilecrm.widgets.Widget;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.config.ClientConfig;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
 
 /*       
  * The <code>ZendeskUtil</code> class acts as a Client to ClickDeskPlugins
@@ -26,24 +18,6 @@ import com.sun.jersey.api.client.config.DefaultClientConfig;
 
 public class ZendeskUtil
 {
-
-    /**
-     * Plugin Server URL
-     */
-    private static final String pluginURL = "http://ec2-72-44-57-140.compute-1.amazonaws.com:8080/ClickdeskPlugins/";
-
-    /**
-     * This method is used to create a client to the ClickDeskConnector
-     * 
-     * @return resource after building the client
-     */
-
-    private static WebResource getResource()
-    {
-	ClientConfig config = new DefaultClientConfig();
-	Client client = Client.create(config);
-	return client.resource(UriBuilder.fromUri(pluginURL).build());
-    }
 
     // Run - Before Chat
     /**
@@ -60,34 +34,10 @@ public class ZendeskUtil
      *             if the response is an exception
      */
 
-    public static String getContactTickets(Widget widget, String email)
-	    throws Exception
+    public static String getContactTickets(Widget widget, String email) throws Exception
     {
 
-	JSONObject pluginPrefsJSON = buildPluginPrefsJSON(widget);
-	JSONObject contactPrefsJSON = new JSONObject().put("visitor_email",
-		email);
-
-	if (pluginPrefsJSON == null || contactPrefsJSON == null)
-	    throw new Exception("zendesk preferences null");
-
-	JSONObject prefsJSON = new JSONObject().put("pluginPrefsJSON",
-		pluginPrefsJSON).put("visitorJSON", contactPrefsJSON);
-
-	/**
-	 * Holds a {@link WebResource} object with the credentials of the
-	 * current user to authenticate user when a request is made
-	 */
-	WebResource resource = null;
-	ClientResponse response = null;
-
-	// Get ClickDesk res
-	resource = getResource();
-	response = resource.path("/core/agile/zendesk/get")
-		.type(MediaType.APPLICATION_JSON)
-		.put(ClientResponse.class, prefsJSON.toString());
-
-	return response.getEntity(String.class);
+	return null;
 
     }
 
@@ -109,37 +59,10 @@ public class ZendeskUtil
      *             if the response is an exception
      */
 
-    public static String addTicket(Widget widget, String name, String email,
-	    String subject, String description) throws Exception
+    public static String addTicket(Widget widget, String name, String email, String subject, String description) throws Exception
     {
 
-	JSONObject pluginPrefsJSON = buildPluginPrefsJSON(widget);
-	JSONObject contactPrefsJSON = new JSONObject().put("visitor_email",
-		email).put("visitor_name", name);
-	JSONObject messageJSON = new JSONObject().put("subject", subject).put(
-		"message", description);
-
-	if (pluginPrefsJSON == null || contactPrefsJSON == null)
-	    throw new Exception("zendesk preferences null");
-
-	JSONObject json = new JSONObject()
-		.put("pluginPrefsJSON", pluginPrefsJSON)
-		.put("visitorJSON", contactPrefsJSON)
-		.put("messageJSON", messageJSON);
-
-	/**
-	 * Holds a {@link WebResource} object with the credentials of the
-	 * current user to authenticate user when a request is made
-	 */
-	WebResource resource = null;
-	ClientResponse response = null;
-
-	resource = getResource();
-	response = resource.path("/core/agile/zendesk/add")
-		.type(MediaType.APPLICATION_JSON)
-		.put(ClientResponse.class, json.toString());
-
-	return response.getEntity(String.class);
+	return null;
 
     }
 
@@ -161,32 +84,10 @@ public class ZendeskUtil
      *             if the response is an exception
      */
 
-    public static String updateTicket(Widget widget, String ticketNumber,
-	    String description) throws Exception
+    public static String updateTicket(Widget widget, String ticketNumber, String description) throws Exception
     {
 
-	JSONObject pluginPrefsJSON = buildPluginPrefsJSON(widget);
-	JSONObject messageJSON = new JSONObject().put("ticketId", ticketNumber)
-		.put("message", description);
-
-	if (pluginPrefsJSON == null || messageJSON == null)
-	    throw new Exception("zendesk preferences null");
-
-	JSONObject json = new JSONObject().put("pluginPrefsJSON",
-		pluginPrefsJSON).put("messageJSON", messageJSON);
-
-	/**
-	 * Holds a {@link WebResource} object with the credentials of the
-	 * current user to authenticate user when a request is made
-	 */
-	WebResource resource = null;
-	ClientResponse response = null;
-
-	resource = getResource();
-	response = resource.path("/core/agile/zendesk/update")
-		.type(MediaType.APPLICATION_JSON)
-		.put(ClientResponse.class, json.toString());
-	return response.getEntity(String.class);
+	return null;
 
     }
 
@@ -203,12 +104,8 @@ public class ZendeskUtil
     {
 	try
 	{
-	    JSONObject pluginPrefs = new JSONObject()
-		    .put("zendesk_username",
-			    widget.getProperty("zendesk_username"))
-		    .put("zendesk_password",
-			    widget.getProperty("zendesk_password"))
-		    .put("zendesk_url", widget.getProperty("zendesk_url"));
+	    JSONObject pluginPrefs = new JSONObject().put("zendesk_username", widget.getProperty("zendesk_username"))
+		    .put("zendesk_password", widget.getProperty("zendesk_password")).put("zendesk_url", widget.getProperty("zendesk_url"));
 	    return pluginPrefs;
 	}
 	catch (JSONException e)
