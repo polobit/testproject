a68d00dc5af589baf8cf4d346546b175cd8cc6e4
Yaswanth <praveen@invox.com>
2013-06-13 17:43:41 +0530

    Spacing and line breaks in page tours

--- war/jscore/agile-tour.js
+++ war/jscore/agile-tour.js
@@ -4,24 +4,26 @@ function create_tour_steps(el) {
 	
 	AGILE_TOUR["contacts"] = [
 	                                 {
-	                                	 "element" : "#contacts-model-list",
+	                                	 "element" : "#contacts",
 	                     				"title" : "Contact & Account Management",
-	                     				"content" : "View your contacts, leads and accounts (companies) all at one place.",
-	                     				"placement" : "top",
-	                     				"el": el
+	                     				"content" : "View your contacts, leads and accounts (companies) all at one place.<br/>",
+	                     				"placement" : "bottom",
+	                     				"el": el,
+	                     				
 	                     			
 	                                 },
 	                                 {
 	                                	 "element" : "#filter-list",
 	                     				"title" : "Companies (Accounts)",
-	                     				"content" : "Accounts are stored as Companies in Agile. You can switch between contacts and companies  here.",
+	                     				"content" : "Accounts are stored as Companies in Agile.<br/><br/> You can switch between contacts and companies  here.<br/>",
 	                     				"placement" : "bottom",
-	                     				"el": el
+	                     				"el": el,
+	                     				"backdrop" : true
 	                                 },
 	                                 {
 	                                	 "element" : "#tags",
 	                     				"title" : "Tags",
-	                     				"content" : "Tags help you effectively manage your contacts and companies.<br/> For eg: you can add a lead tag to your contacts for leads.",
+	                     				"content" : "Tags help you effectively manage your contacts and companies.<br/><br/> For eg: you can add a lead tag to your contacts for leads.<br/>",
 	                     				"placement" : "right",
 	                     				"el": el,
 	                     				"backdrop" : true
@@ -33,31 +35,32 @@ function create_tour_steps(el) {
 	 * Contacts details
 	 */
 	AGILE_TOUR["contact-details"] = [
-			{
-				"element" : "#score",
-				"container": "#score",
-				"title" : "Score your leads",
-				"content" : "Assign lead scores for every contact to keep high quality leads swimming on top. Use workflows to automate the process based on user behavior.",
-				"placement" : "bottom",
-				"el": el,
-				"backdrop" : true
-			},
-			{
-				"element" : "#contact-tab-content",
-				"title" : "Facebook-Style Timeline",
-				"content" : "Notice the awesome timeline with dates, emails exchanged, social messages & site visits.",
-				"placement" : "right",
-				"el": el,
-				"backdrop" : true
-			},{
-				"element" : "#widgets",
-				"title" : "Widgets & Integrations",
-				"content" : "Get more information about the contact from social media, helpdesk tickets, chats, and from billing systems.",
-				"placement" : "left",
-				"el": el,
-				"backdrop" : true,
-			}, 
-	];
+	                             	{
+	                    				"element" : "#contact-tab-content",
+	                    				"title" : "Facebook-Style Timeline",
+	                    				"content" : "Notice the awesome timeline with dates, emails exchanged, social messages & site visits.<br/>",
+	                    				"placement" : "right",
+	                    				"el": el,
+	                    				"backdrop" : true
+								     },
+										{
+											"element" : "#score",
+											"container": "#score",
+											"title" : "Score your leads",
+											"content" : "Assign lead scores for every contact to keep high quality leads swimming on top. <br/><br/> Use workflows to automate the process based on user behavior.<br/>",
+											"placement" : "bottom",
+											"el": el,
+											"backdrop" : true
+										},
+										{
+											"element" : "#widgets",
+											"title" : "Widgets & Integrations",
+											"content" : "Get more information about the contact from social media, helpdesk tickets, chats, and from billing systems.<br/>",
+											"placement" : "left",
+											"el": el,
+											"backdrop" : true,
+										}, 
+								];
 	
 	/**
 	 * Calendar
@@ -66,7 +69,7 @@ function create_tour_steps(el) {
 	                          {
 		                        	"element" : "#calendar",
 		              				"title" : "Calendar Events",
-		              				"content" : "Events are time based such as meetings. They show up in calendar.",  
+		              				"content" : "Events are time based such as meetings.<br/> They show up in calendar.<br/>",  
 		              				"placement" : "left",
 		              			//	"el": el,
 		              				"backdrop" : true,
@@ -74,7 +77,7 @@ function create_tour_steps(el) {
 	                          {
 	                        	"element" : ".todo-block",
 	              				"title" : "To Do Tasks",
-	              				"content" : "Tasks are like to-dos. Result oriented. You can assign a category such as call, email.",  
+	              				"content" : "Tasks are like to-dos. Result oriented.<br/><br/> You can assign a category such as call, email.<br/>",  
 	              				"placement" : "right",
 	              				// "el": el,
 	              				"backdrop" : true,
@@ -82,7 +85,7 @@ function create_tour_steps(el) {
 	                          {
 		                        	"element" : "#subscribe-ical",
 		              				"title" : "Calendar Sync",
-		              				"content" : "You can sync your Agile calendar with  Outlook, Google calendar or your mobile phone.",  
+		              				"content" : "You can sync your Agile calendar with  Outlook, Google calendar or your mobile phone.<br/>",  
 		              				"placement" : "top",
 		              			//	"el": el,
 		              				"backdrop" : true,
@@ -94,7 +97,7 @@ function create_tour_steps(el) {
 	                        	   	{
 			                        	"element" : "#learn-workflows",
 			              				"title" : "Learn about Campaigns",
-			              				"content" : "love campaign workflows. You would too! <br/> Take a few mins and learn more about campaigns. ",  
+			              				"content" : "Our customers love campaign workflows. You would too!<br/><br/>  <p class='text-error'><strong>Take a few mins and learn more about campaigns.</strong></p>",  
 			              				"placement" : "left",
 			              				"el": el,
 			              				"backdrop" : true,
@@ -102,7 +105,7 @@ function create_tour_steps(el) {
 			                       	{
 			                        	"element" : "#add-trigger",
 			              				"title" : "Triggers",
-			              				"content" : "Create conditions to trigger your campaigns automatically. <br/> Eg: when a tag is added or when a contact reaches a score.",  
+			              				"content" : "Create conditions to trigger your campaigns automatically. <br/><br/> <strong>Eg:</strong> when a tag is added or when a contact reaches a score.<br/>",  
 			              				"placement" : "bottom",
 			              				"el": el,
 			              				"backdrop" : true,
@@ -110,7 +113,7 @@ function create_tour_steps(el) {
 			                     	{
 			                        	"element" : "#workflows-tour-step",
 			              				"title" : "Sample Campaigns",
-			              				"content" : "I have created few sample campaigns for you to get a hang of them.<br/><br/> Multistep Responders - simple email with an automated followup after few days <br/><br/> Multichannel Campaigns - email and engage on Twitter if they have opened your your email <br/><br/> Cart Abandonment made simple - send a coupon to someone who has added an item in the cart but hasn't finished the checkout in 3 days. <br/>",
+			              				"content" : "I have created few sample campaigns for you to get a hang of them.<br/><br/> <strong>Multistep Responders</strong> - simple email with an automated followup after few days <br/><br/> <strong>Multichannel Campaigns</strong> - email and engage on Twitter if they have opened your your email <br/><br/> <strong>Cart Abandonment made simple</strong> - send a coupon to someone who has added an item in the cart but hasn't finished the checkout in 3 days. <br/>",
 			              				"placement" : "right",
 			              				"el": el,
 			              				"backdrop" : true,
@@ -120,7 +123,7 @@ function create_tour_steps(el) {
 	                               {
 	                            	   "element" : "#workflowform",
 			              				"title" : "Visual Campaigns",
-			              				"content" : "Create your campaigns and workflows visually.<br/> Just drag and drop the nodes. Connect them to the workflow.",
+			              				"content" : "Create your campaigns and workflows visually.<br/> Just drag and drop the nodes. Connect them to the workflow.<br/>",
 			              				"placement" : "top",
 			              				"el": el,
 			              				"backdrop" : true,
@@ -158,6 +161,12 @@ function initiateTour(key, el)
 	head.js('lib/bootstrap-tour.min.js', function() {
 		tour = new Tour({
 			 name: key + "-tour",
+			  labels: {
+		            end: '',
+		            next: 'Next &raquo;',
+		            prev: '&laquo; Prev'
+		          },
+		   //   debug:true,
 			useLocalStorage : true,
 			onEnd : function(tour) {
 				
