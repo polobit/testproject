b084a565d3dbc959b52b3d294f86d3a34375fded
Yaswanth <praveen@invox.com>
2013-06-26 11:06:38 +0530

    Changed widgets functionlity so that Widget will be loaded from server only once in the session. And addition and deletion reflects in to collection

--- war/jscore/widgets/widget-loader.js
+++ war/jscore/widgets/widget-loader.js
@@ -1,6 +1,7 @@
 /**
  * Loads widgets on a contact, creates a collection view
  */
+var WIDGETS_VIEW;
 function loadWidgets(el, contact)
 {
     // Create Data JSON
@@ -10,100 +11,53 @@ function loadWidgets(el, contact)
 
     // Creates collection view, collection is sorted based on position i.e., set
     // when sorted using jquery ui sortable
-    var view = new Base_Collection_View(
+
+    if(!WIDGETS_VIEW)
     {
-        url: '/core/api/widgets',
-        restKey: "widget",
-        templateKey: "widgets",
-        individual_tag_name: 'li',
-        sortKey: 'position',
-        modelData: data
-    });
-
-    // Fetches the widget collection, on success widget scripts are loaded form
-    // the url specified in widget model attribute url.
-    view.collection.fetch(
+	   WIDGETS_VIEW = new Base_Collection_View(
+	    {
+	        url: '/core/api/widgets',
+	        restKey: "widget",
+	        templateKey: "widgets",
+	        individual_tag_name: 'li',
+	        sortKey: 'position',
+	        modelData: data,
+	        postRenderCallback : function(el) {
+	        	
+	        }
+	    });
+	
+	    // Fetches the widget collection, on success widget scripts are loaded form
+	    // the url specified in widget model attribute url.
+	    WIDGETS_VIEW.collection.fetch(
+	    {
+	        success: function ()
+	        {
+	        	set_up_widgets(el);
+	        }
+	    });
+	    
+	    	var newEl = WIDGETS_VIEW.render().el;
+	    	set_up_widgets(el);
+	    	$('#widgets', el).html(newEl);
+    }
+    else
     {
-        success: function ()
-        {
-        	console.log(view.collection.toJSON())
+    	// Have a flag, which is used to check whether widgets are already loaded. 
+    	// This avoid unnecessary 
+    	var flag = false;
+        
+        $(el).live('agile_model_loaded', function(e) {
+        	if(flag == false)
+        	{
+        		$('#widgets', el).html(WIDGETS_VIEW.render(true).el);
+        		set_up_widgets(el, WIDGETS_VIEW.el);
+        	}
+        	flag = true;
+        });
+    }
 
-            // Iterates through all the models (widgets) in the
-            // collection, and scripts are loaded from the url in the
-            // widget
-            _(view.collection.models).each(function (model)
-            {
-                // In case collection is not empty
-                var id = model.get("id");
-                var url = model.get("url");
- 
-                if(!model.get("is_minimized"))
-                	$.get(url, "script", function(data){
-                		console.log('script');
-                		console.log(model.get('name'));
-                	});
-
-                
-                // Sets the data element in the div
-                // We can retrieve this in get plugin prefs
-                $('#' + model.get('name'), el).data('model', model);
-            }, this);
-
-            // Loads jquery-ui to get sortable functionality on widgets
-            head.js(LIB_PATH + 'lib/jquery-ui.min.js', function ()
-            {
-            	
-            	console.log('sortable');
-            	$('.widget-sortable').sortable();
-        		//$('.widget-sortable').sortable("option", "containment", $('.widget-sortable'));
-        		$('.widget-sortable').sortable( "option", "handle", ".icon-move" );	
-			
-        		$('.widget-sortable').on( "sortstop", function( event, ui ) {
-        			
-    				var models = [];
-
-                    // Store the save
-                    $('.widget-sortable > li').each(function (index, element)
-                    {
-                    	var model_nam = $(element).find('.widgets').attr('id');
-                    	
-                    	console.log(model_nam);
-                    	
-                        // Get Model, model is set as data to widget element
-                        var model = $('#' + model_nam).data('model');
-                        
-                        models.push(
-                        {
-                            id: model.get("id"),
-                            position: index
-                        });
-                    });
-
-                    // Stores the positions at server
-                    $.ajax(
-                    {
-                        type: 'POST',
-                        url: '/core/api/widgets/positions',
-                        data: JSON.stringify(models),
-                        contentType: "application/json; charset=utf-8",
-                        dataType: 'json'
-                    });
-                    
-    			});
-            });
-        }
-    });
-    
-    var newEl = view.render().el;
-    
-    console.log('neew wsr')
-    console.log(newEl);
-
-    $('#widgets', el).html(newEl);
-    
-	
-	
-	$('.widget-minimize').die().live('click', function(e){
+	$('.widget-minimize').die().live('click', function(e) {
 		e.preventDefault();
 
 		var widget_name = $(this).attr('widget');
@@ -113,9 +67,10 @@ function loadWidgets(el, contact)
 		$(this).addClass('collapsed');
 		$(this).addClass('widget-maximize');
 		$(this).addClass('icon-plus');
-		var widget = view.collection.where({name: widget_name})[0]
+		var widget = WIDGETS_VIEW.collection.where({name: widget_name})[0]
 		var widgetJSON = widget.toJSON();
 		
+		widget.set({'is_minimized' : true })
 		widgetJSON['is_minimized'] = true;
 		
 		var model = new BaseModel();
@@ -129,10 +84,12 @@ function loadWidgets(el, contact)
 		e.preventDefault();
 		var widget_name = $(this).attr('widget');
 		
-		var widget = view.collection.where({name: widget_name})[0];
+		var widget = WIDGETS_VIEW.collection.where({name: widget_name})[0];
 		
 		var widgetJSON = widget.toJSON();
 		widgetJSON['is_minimized'] = false;
+		
+		widget.set({'is_minimized' : false },{silent:true})
 		
 		var model = new BaseModel();
 		model.url = "core/api/widgets";
@@ -144,19 +101,86 @@ function loadWidgets(el, contact)
 		$(this).removeClass();
 		$(this).addClass('widget-minimize');
 		$(this).addClass('icon-minus');
-		console.log(is_collapsed);
 		if(is_collapsed)
 		{
 			$("#" + widget_name).collapse('show');
 			return;
 		}
-		
 		$.get(widget.get('url'), 'script');
 
 	});
 }
 
 
+function set_up_widgets(el, widgets_el)
+{
+    // Iterates through all the models (widgets) in the
+    // collection, and scripts are loaded from the url in the
+    // widget
+    _(WIDGETS_VIEW.collection.models).each(function (model)
+    {
+        // In case collection is not empty
+        var id = model.get("id");
+        var url = model.get("url");
+
+        if(!model.get("is_minimized"))
+        	$.get(url, "script");
+
+        
+        // Sets the data element in the div
+        // We can retrieve this in get plugin prefs
+        $('#' + model.get('name'), el).data('model', model);
+    }, this);
+	
+	enableWidgetSoring(widgets_el);
+
+}
+
+function enableWidgetSoring(el)
+{
+	
+    // Loads jquery-ui to get sortable functionality on widgets
+    head.js(LIB_PATH + 'lib/jquery-ui.min.js', function ()
+    {
+    	
+    	$('.widget-sortable', el).sortable();
+		//$('.widget-sortable').sortable("option", "containment", $('.widget-sortable'));
+		$('.widget-sortable', el).sortable( "option", "handle", ".icon-move" );	
+	
+		$('.widget-sortable', el).on( "sortstop", function( event, ui ) {
+			
+			var models = [];
+
+            // Store the save
+            $('.widget-sortable > li', el).each(function (index, element)
+            {
+            	var model_name = $(element).find('.widgets').attr('id');
+            	
+            	
+                // Get Model, model is set as data to widget element
+                var model = $('#' + model_name).data('model');
+                
+                models.push(
+                {
+                    id: model.get("id"),
+                    position: index
+                });
+            });
+
+            // Stores the positions at server
+            $.ajax(
+            {
+                type: 'POST',
+                url: '/core/api/widgets/positions',
+                data: JSON.stringify(models),
+                contentType: "application/json; charset=utf-8",
+                dataType: 'json'
+            });
+            
+		});
+    });
+    
+}
 
 function queueGetRequest(queueName, url, dataType, successcallback, errorCallback)
 {
