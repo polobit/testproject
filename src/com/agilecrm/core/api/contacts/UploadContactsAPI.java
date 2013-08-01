22681cc56c37a791757dfad33711bfae602020e0
Yaswanth <praveen@invox.com>
2013-08-01 11:15:53 +0530

    Contacts CSV upload backend

---
+++ src/com/agilecrm/core/api/contacts/UploadContactsAPI.java
@@ -0,0 +1,238 @@
+package com.agilecrm.core.api.contacts;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.util.Hashtable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.agilecrm.Globals;
+import com.agilecrm.contact.util.ContactUtil;
+import com.agilecrm.session.SessionManager;
+import com.agilecrm.util.CacheUtil;
+import com.google.appengine.api.backends.BackendServiceFactory;
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreInputStream;
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskOptions;
+import com.google.appengine.api.taskqueue.TaskOptions.Method;
+import com.google.appengine.api.utils.SystemProperty;
+
+@Path("/api/upload")
+public class UploadContactsAPI
+{
+
+	/**
+	 * Handle request sent using file uploader, reads the details from the
+	 * uploaded file are returns the data which is processed and stored in to
+	 * map, so fields can be shown at the client side using the map
+	 * 
+	 * @param request
+	 *            {@link HttpServletRequest}
+	 * @return {@link String}
+	 */
+	@Path("/process")
+	@GET
+	public String post(@QueryParam("blob-key") String key)
+	{
+		System.out.println("===========================================================================");
+		try
+		{
+
+			InputStream stream = new BlobstoreInputStream(new BlobKey(key));
+
+			LineNumberReader reader = new LineNumberReader(new InputStreamReader(stream));
+
+			String contactString = IOUtils.toString(stream);
+
+			int numberOfContacts = contactString.trim().split(System.getProperty("line.separator")).length;
+
+			System.out.println(numberOfContacts);
+
+			System.out.println(new BlobstoreInputStream(new BlobKey(key)).read());
+
+			Hashtable result = ContactUtil.convertCSVToJSONArrayPartially(contactString, "");
+
+			System.out.println(result);
+
+			JSONObject success = new JSONObject();
+			success.put("success", true);
+			success.put("blob_key", key);
+			success.put("available_contacts", numberOfContacts);
+
+			// Stores results in to a map
+			JSONArray csvArray = (JSONArray) result.get("result");
+
+			// returns CSV file as a json object with key "data"
+			success.put("data", csvArray);
+			System.out.println(success);
+
+			return success.toString();
+
+			/*
+			 * System.out.println(request.getContentType()); // Reads data from
+			 * the request object InputStream file = request.getInputStream();
+			 * 
+			 * String csv = IOUtils.toString(file);
+			 * 
+			 * System.out.println(csv);
+			 * 
+			 * JSONObject success = new JSONObject(); success.put("success",
+			 * true); // success.put("blob_key", blob_key); // Stores results in
+			 * to a map
+			 * 
+			 * Hashtable result =
+			 * ContactUtil.convertCSVToJSONArrayPartially(csv, "email");
+			 * JSONArray csvArray = (JSONArray) result.get("result");
+			 * 
+			 * // returns CSV file as a json object with key "data"
+			 * success.put("data", csvArray); System.out.println(success);
+			 * 
+			 * return success.toString();
+			 */
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	@Path("/save")
+	@POST
+	public void saveContacts(@Context HttpServletRequest request)
+	{
+
+		try
+		{
+			String postURL = "https://" + Globals.BULK_ACTION_BACKENDS_URL + ".agile-crm-cloud.com";
+			// If Localhost - just return
+			if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Development)
+			{
+				postURL = BackendServiceFactory.getBackendService().getBackendAddress("b1");
+			}
+
+			InputStream stream = request.getInputStream();
+			byte[] bytes = IOUtils.toByteArray(stream);
+
+			System.out.println(bytes);
+
+			Queue queue = QueueFactory.getQueue("bulk-actions-queue");
+			TaskOptions taskOptions;
+
+			String key = request.getParameter("key");
+			if (StringUtils.isEmpty(key))
+				return;
+
+			CacheUtil.setCache(key, true);
+
+			taskOptions = TaskOptions.Builder
+					.withUrl(
+							"/core/api/bulk-actions/upload/"
+									+ String.valueOf(SessionManager.get().getDomainId() + "/"
+											+ request.getParameter("key"))).payload(bytes)
+					.header("Content-Type", request.getContentType()).header("Host", postURL).method(Method.POST);
+
+			queue.add(taskOptions);
+
+			System.out.println("completed");
+		}
+
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
+
+// /**
+// * Returns whether the key of contacts list still exists in the memcache
+// * i.e., task is not completed. This method will be called repeatedly with
+// * specified time interval from client to check whether the uploaded
+// * contacts are saved. If contacts are save then key is removed from the
+// * memcache then this method will return true if key is moved from the
+// * memcache.
+// *
+// * @param key
+// * {@link String}, key of the contact list saved in memcache
+// * @return {@link Boolean} returns true if key is removed from memcache and
+// * vice-versa
+// */
+// @Path("save/status")
+// @POST
+// @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+// public boolean contactsUploadStatus(String memcache_key)
+// {
+//
+// if (CacheUtil.isPresent(memcache_key))
+// return false;
+//
+// return true;
+// }
+//
+// @Path("contacts/save")
+// @POST
+// @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+// @Produces(MediaType.TEXT_HTML)
+// public String saveBulkContacts(String contact, @QueryParam("key") String
+// blobKey) throws Exception
+// {
+//
+// String postURL =
+// BackendServiceFactory.getBackendService().getBackendAddress("contactsbulk").trim();
+//
+// CacheUtil.put(blobKey, blobKey);
+//
+// Queue queue = QueueFactory.getDefaultQueue();
+// // Add to queue
+// queue.add(TaskOptions.Builder.withPayload(new TaskRunner(contact, postURL,
+// blobKey)));
+//
+// return blobKey;
+//
+// }
+// }
+//
+// class TaskRunner implements DeferredTask
+// {
+// String contact;
+// String blobKey;
+// String postURL;
+//
+// public TaskRunner(String contact, String postURL, String blobKey)
+// {
+// this.contact = contact;
+// this.postURL = postURL;
+// this.blobKey = blobKey;
+// }
+//
+// public void run()
+// {
+// String URL = "http://" + postURL + "/backend/contactsbulk/?key=" + blobKey;
+// try
+// {
+// Util.accessURLUsingPost(URL, contact);
+// }
+// catch (Exception e)
+// {
+// // TODO Auto-generated catch block
+// e.printStackTrace();
+// }
+// }
+// }
---
+++ war/upload-contacts.jsp
@@ -0,0 +1,112 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<%@page import="com.agilecrm.user.util.UserPrefsUtil"%>
+<%@page import="java.util.Date"%>
+<%@page import="com.google.appengine.api.blobstore.BlobstoreService"%>
+<%@page import="com.google.appengine.api.blobstore.BlobstoreServiceFactory"%>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Upload Image</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap-<%= UserPrefsUtil.getCurrentUserPrefs().template%>.min.css" />
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script type="text/javascript" src="/lib/jquery.validate.min.js"></script>
+<script type="text/javascript" src="/lib/bootstrap.min.js"></script>
+
+<script type="text/javascript">
+jQuery.validator.setDefaults({
+	debug: true,
+	success: "valid"
+});;
+</script>
+<script type="text/javascript">
+
+//Get URL
+var url = "https://s3.amazonaws.com/agilecrm/" + unescape(getUrlVars()["key"]) + "?id=" + unescape(getUrlVars()["id"]);
+
+// Get Id
+//Read a page's GET URL variables and return them as an associative array.
+function getUrlVars() {
+    var vars = [],
+        hash;
+    
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+function returnBack(key)
+{
+	
+	if(!key)
+		return;
+	
+	if (window.opener)
+	{
+		window.opener.parseCSV(key);
+	   window.close();
+	}
+	return;
+}
+
+$(function()
+{
+	// Check if this was referred back again
+	var key = getUrlVars()["key"];
+	
+	if(key != undefined)
+	{
+		returnBack(key);
+		return;
+	} 
+});
+function isValid(){
+    $("#form").validate({
+        rules: {
+        		file:{required:true,accept:"csv"}
+               }
+   		});
+    return $("#form").valid();
+    } 
+
+
+</script>
+<style>
+	label.error {
+		color:red;
+	}
+</style>
+
+</head>
+
+
+<body class='center' style="height:90%;width:90%;padding:5px">
+
+<br/>
+
+<div class="well" style="height:215px; width:390px ">
+
+<legend>Upload CSV file</legend>
+
+
+<p>upload CSV file.</i></p>
+
+<br/>
+ <form action="<%= BlobstoreServiceFactory.getBlobstoreService().createUploadUrl("/upload")  %>" method="post" enctype="multipart/form-data" onsubmit="return isValid();" id="form">
+    
+<p><input name="file" id='fileextension' type="file" /></p>
+<br/>
+<input name="submit" id="submit" value="Upload" class='submit btn btn-primary' type="submit" /> 
+</form> 
+</div>
+
+</body>
+</html>
