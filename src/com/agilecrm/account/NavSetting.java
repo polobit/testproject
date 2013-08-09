7447fcd1804df6b2a01bf5e0c9d19003f50f080d
Chandan Kumar <chandan@agilecrm.com>
2013-08-09 10:46:14 +0530

    NavbarSettings cleanup
    Comments added.

--- src/com/agilecrm/account/NavSetting.java
+++ src/com/agilecrm/account/NavSetting.java
@@ -7,30 +7,77 @@ import org.codehaus.jackson.annotate.JsonIgnore;
 
 import com.agilecrm.db.ObjectifyGenericDao;
 import com.googlecode.objectify.annotation.NotSaved;
+import com.googlecode.objectify.condition.IfDefault;
 
+/**
+ * NavSetting Entity <br/>
+ * <br/>
+ * Holds state of various tabs in Navbar.<code>true</code> implies enabled in
+ * view.
+ * 
+ * @author Chandan
+ * 
+ */
 @XmlRootElement
 public class NavSetting
 {
+	/**
+	 * Id of the entity
+	 */
 	@Id
-	public Long id=null;
-	public boolean input_cases,input_deals,input_calendar,input_campaign;  // status of various navtabs
-	
+	public Long id = null;
+
+	/**
+	 * State of Cases tab in Navbar. <code>true</code> implies visible in
+	 * Navbar.
+	 */
+	@NotSaved(IfDefault.class)
+	public boolean input_cases = true;
+
+	/**
+	 * State of Deals tab in Navbar. <code>true</code> implies visible in
+	 * Navbar.
+	 */
+	@NotSaved(IfDefault.class)
+	public boolean input_deals = true;
+
+	/**
+	 * State of Calendar tab in Navbar. <code>true</code> implies visible in
+	 * Navbar.
+	 */
+	@NotSaved(IfDefault.class)
+	public boolean input_calendar = true;
+
+	/**
+	 * State of Campaign tab in Navbar. <code>true</code> implies visible in
+	 * Navbar.
+	 */
+	@NotSaved(IfDefault.class)
+	public boolean input_campaign = true;
+
+	/**
+	 * Deafult - <br/>
+	 * Cases - not visible, Rest of the tabs - visible
+	 */
 	public void setDefault()
 	{
-		input_cases=false;
-		input_deals=input_calendar=input_campaign=true;
+		input_cases = false;
+		input_deals = input_calendar = input_campaign = true;
 	}
-	
-	public NavSetting(){}
 
-	private static ObjectifyGenericDao<NavSetting> dao=new ObjectifyGenericDao<NavSetting>(NavSetting.class);
-	
+	public NavSetting()
+	{
+	}
+
+	// dao
+	private static ObjectifyGenericDao<NavSetting> dao = new ObjectifyGenericDao<NavSetting>(NavSetting.class);
+
+	/**
+	 * Save this entity in Datastore
+	 */
 	@JsonIgnore
 	public void save()
 	{
 		dao.put(this);
 	}
-	
 }
-
-
--- src/com/agilecrm/core/api/prefs/NavbarSettingAPI.java
+++ src/com/agilecrm/core/api/prefs/NavbarSettingAPI.java
@@ -1,41 +1,68 @@
 package com.agilecrm.core.api.prefs;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.xml.bind.annotation.XmlRootElement;
 
-import com.agilecrm.account.AccountPrefs;
 import com.agilecrm.account.NavSetting;
-import com.agilecrm.account.util.AccountPrefsUtil;
 import com.agilecrm.account.util.NavSettingUtil;
-import com.googlecode.objectify.Objectify;
-import com.googlecode.objectify.ObjectifyService;
 
-
+/**
+ * NabbarSettingAPI is the rest interface to deal with {@link NavSetting}.
+ * <code>NavSetting</code> is a single object for a single user. Each of GET,
+ * PUT,POST requests correspond to single object.<code>NavSettingUtil</code> is
+ * the utility class for providing various facilities e.g. read & write, to the
+ * NavSetting object.
+ * 
+ * @author Clickdesk
+ * 
+ */
 @Path("/api/navbarsets")
 public class NavbarSettingAPI
 {
-	
+
+	/**
+	 * GET call to the path.
+	 * 
+	 * @return NavSetting object representing state of Navbar
+	 */
 	@GET
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public NavSetting getNavbarSetting()
-    {
+	public NavSetting getNavbarSetting()
+	{
 		return NavSettingUtil.getNavSetting();
-    }
-	
+	}
+
+	/**
+	 * POST & PUT are essentially same. They both update the NavSetting object
+	 * with new one from the client.
+	 * 
+	 * @param navSetting
+	 *            - the object from the client
+	 * @return NavSetting as saved on server.
+	 */
 	@POST
 	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public NavSetting setNavbarSettingPOST(NavSetting nv)
+	public NavSetting setNavbarSettingPOST(NavSetting navSetting)
 	{
-		return NavSettingUtil.setNavSetting(nv);
+		return NavSettingUtil.setNavSetting(navSetting);
 	}
-	
+
+	/**
+	 * Same as POST, update navSetting from client & return updated to client
+	 * 
+	 * @param navSetting
+	 */
 	@PUT
 	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public void setNavbarSettingPUT(NavSetting nv)
+	public void setNavbarSettingPUT(NavSetting navSetting)
 	{
-		NavSettingUtil.setNavSetting(nv);
+		NavSettingUtil.setNavSetting(navSetting);
 	}
 }
