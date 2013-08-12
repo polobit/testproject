4f60153399c9689cadedbe74967e7d18bdee31a3
karthikagile <karthik@invox.com>
2013-08-12 19:20:45 +0530

    documented

--- war/stats/js/agile_contact.js
+++ war/stats/js/agile_contact.js
@@ -1,19 +1,34 @@
+/**
+ * agile_contact.js deals with the functions used to create, get,
+ * and delete the contact.
+ * 
+ * @module stats
+ */
+
+/**
+ * Creates a contact with the following properties
+ * 
+ * @param data
+ * 				example : {tags:[tag1, tag2], properties:[{name:first_name, value: agile}, {name: last_name, value: crm}]
+ * @param callback 
+ * 				callback function for agile_createContact
+ */
 function agile_createContact(data, callback)
 {
 	var properties = [];
 
-	for ( var key in data)
+	for (var key in data)
 	{
 		if (data.hasOwnProperty(key) && key != "tags")
 		{
-			// alert(key + " -> " + p[key]);
+			//add tags to properties array
 			properties.push(agile_propertyJSON(key, data[key]));
 		}
 	}
 
 	var original_ref = "original_ref";
 	properties.push(agile_propertyJSON(original_ref, agile_read_cookie(agile_guid.cookie_original_ref)));
-
+	//creating json object and appending properties 
 	var model = {};
 	model.properties = properties;
 	console.log(model);
@@ -22,35 +37,48 @@ function agile_createContact(data, callback)
 		var tags = data["tags"];
 		var tags_string = tags.trim().replace("/ /g", " ");
 
-		// Replace ,space with ,
+		//Replace ,space with ,
 		tags_string = tags.replace("/, /g", ",");
-
+		//Splitting tags string at ,
 		model.tags = tags_string.split(",");
 	}
 
-	// var params = "contact={0}&tags={1}".format(encodeURIComponent(data),
-	// encodeURIComponent(JSON.stringify(tags)));
-	// Get
+	//var params = "contact={0}&tags={1}".format(encodeURIComponent(data), encodeURIComponent(JSON.stringify(tags)));
+	//Get
 	var agile_url = agile_id.getURL() + "/contacts?callback=?&id=" + agile_id.get() + "&contact=" + encodeURIComponent(JSON.stringify(model));
-
+	//callback
 	agile_json(agile_url, callback, data);
 }
 
+/**
+ * Delete a contact based on the email
+ * @param email
+ * 				email of the contact
+ * @param callback
+ * 				callback for agile_deleteContact
+ */			
 function agile_deleteContact(email, callback)
 {
 	var agile_url = agile_id.getURL() + "/contact/delete?callback=?&id=" + agile_id.get() + "&email=" + encodeURIComponent(email);
-
+	//callback
 	agile_json(agile_url, callback);
 }
 
+/**
+ * Get contact based on the email
+ * @param email
+ * 				email of the contact
+ * @param callback
+ * 				callback of the agile_getContact
+ */
 function agile_getContact(email, callback)
 {
 	var params = "email={0}".format(encodeURIComponent(email));
 	console.log(agile_id);
 	console.log(agile_id.getURL());
 	console.log(agile_id.get());
-	// Get
+	//Get
 	var agile_url = agile_id.getURL() + "/contact/email?callback=?&id=" + agile_id.get() + "&" + params;
-
+	//callback
 	agile_json(agile_url, callback);
 }
--- war/stats/js/agile_deals.js
+++ war/stats/js/agile_deals.js
@@ -1,5 +1,18 @@
+/**
+ * agile_deals.js deals with function to add a deal to a contact 
+ * based on the email
+ * @param data
+ * 				example : {"name": "Deal sales", "description": "brief description on deal", 
+ * 						  "expected_value": "100", "milestone":"won", "close_date": data as epoch time}
+ * @param callback
+ * 				callback function for agile_addDeal
+ * @param email
+ * 				email of the contact
+ */
+
 function agile_addDeal(data, callback, email)
 {
+	//if email is not passed get it from cookie
 	if (!email)
 	{
 		if (!agile_guid.get_email())
@@ -12,8 +25,8 @@ function agile_addDeal(data, callback, email)
 
 	var params = "opportunity={0}&email={1}".format(encodeURIComponent(JSON.stringify(data)), encodeURIComponent(email));
 
-	// Get
+	//Get
 	var agile_url = agile_id.getURL() + "/opportunity?callback=?&id=" + agile_id.get() + "&" + params;
-
+	//callback
 	agile_json(agile_url, callback, data);
 }
