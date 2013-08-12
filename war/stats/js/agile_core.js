f44407b44589390530f673ba57051aacc2523ac5
karthikagile <karthik@invox.com>
2013-08-12 10:45:58 +0530

    callback chnged

--- war/stats/js/agile_core.js
+++ war/stats/js/agile_core.js
@@ -12,6 +12,24 @@ function agile_propertyJSON(name, id, type)
 	return json;
 }
 
+function agile_json(URL, callback, data)
+{
+	var ud = 'json' + (Math.random() * 100).toString().replace(/\./g, '');
+	window[ud] = function(data)
+	{
+		if (callback && typeof(callback) === "function"){
+			callback && callback(data);
+		}
+	};
+	document.getElementsByTagName('body')[0].appendChild((function()
+	{
+		var s = document.createElement('script');
+		s.type = 'text/javascript';
+		s.src = URL.replace('callback=?', 'callback=' + ud);
+		return s;
+	})());
+}
+
 String.prototype.format = function()
 {
 	var args = arguments;
