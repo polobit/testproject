906d50199aedba4ac136be1253eb553c301c59a8
karthikagile <karthik@invox.com>
2013-08-12 16:59:32 +0530

    documented api fn

--- war/stats/js/agile_score.js
+++ war/stats/js/agile_score.js
@@ -1,8 +1,19 @@
+/**
+ * agile_score.js deals with functions to add or substract score
+ * from the contact fetched based on email 
+ * @param score
+ * 				score to be added or subtracted from contact
+ * @param callback
+ * 				callback function for addSore or subtractScore
+ * @param email
+ * 				email of the contact
+ */
 function agile_addScore(score, callback, email)
 {
+	//if score is not passed return
 	if (!score)
 		return;
-
+	//check if email is passed else get it from cookie
 	if (!email)
 	{
 		if (!agile_guid.get_email())
@@ -15,14 +26,16 @@ function agile_addScore(score, callback, email)
 
 	// Post
 	var agile_url = agile_id.getURL() + "/contacts/add-score?callback=?&id=" + agile_id.get() + "&score=" + score + "&email=" + encodeURIComponent(email);
-
+	//callback
 	agile_json(agile_url, callback);
 }
 
 function agile_subtractScore(score, callback, email)
 {
+	//if score is not passed return
 	if (!score)
 		return;
+	//check if email is passed else get it from cookie
 	if (!email)
 	{
 		if (!agile_guid.get_email())
@@ -34,6 +47,6 @@ function agile_subtractScore(score, callback, email)
 	}
 	// Post
 	var agile_url = agile_id.getURL() + "/contacts/subtract-score?callback=?&id=" + agile_id.get() + "&score=" + score + "&email=" + encodeURIComponent(email);
-
+	//callback
 	agile_json(agile_url, callback);
 }
