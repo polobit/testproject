1b598bca45e035cc18a08db7af591d1e2fa4c4c1
Yaswanth <praveen@invox.com>
2013-08-14 18:13:04 +0530

    path /api is required to developer api to connect

--- src/com/agilecrm/core/api/API.java
+++ src/com/agilecrm/core/api/API.java
@@ -10,13 +10,28 @@ import com.agilecrm.account.APIKey;
 @Path("/api")
 public class API
 {
-	// API Key
-	// This method is called if TEXT_PLAIN is request
-	@Path("api-key")
-	@GET
-	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public APIKey getAPIKey()
-	{
-		return APIKey.getAPIKey();
-	}
+
+    // This method is called if TEXT_PLAIN is request
+    /**
+     * This method is required to collect to developers API, requests are sent
+     * to <domain>.agilecrm.com/dev/api
+     * 
+     * @return
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayPlainTextHello()
+    {
+	return "Connected";
+    }
+
+    // API Key
+    // This method is called if TEXT_PLAIN is request
+    @Path("api-key")
+    @GET
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public APIKey getAPIKey()
+    {
+	return APIKey.getAPIKey();
+    }
 }
