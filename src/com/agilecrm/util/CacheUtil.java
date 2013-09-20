99eb1dfd4072d902520228ee9cc9cf1cf8fe399b
Yaswanth <praveen@invox.com>
2013-09-20 15:25:46 +0530

    Added methods in Cache util to take custom expiration date

--- src/com/agilecrm/util/CacheUtil.java
+++ src/com/agilecrm/util/CacheUtil.java
@@ -1,6 +1,7 @@
 package com.agilecrm.util;
 
 import com.google.appengine.api.NamespaceManager;
+import com.google.appengine.api.memcache.Expiration;
 import com.google.appengine.api.memcache.MemcacheService;
 import com.google.appengine.api.memcache.MemcacheServiceFactory;
 
@@ -32,6 +33,39 @@ public class CacheUtil
 	syncCache.put(key, value);
 
 	NamespaceManager.set(oldNamespace);
+    }
+
+    /**
+     * Adds the key and value pair to the Memcache with expiry milliseconds.
+     * 
+     * @param key
+     *            String to store as map key
+     * @param value
+     *            Object represents the value to map
+     * @param timeInMilliSeconds
+     *            Number of milliseconds to set expiry
+     */
+    public static void setCache(String key, Object value, int timeInMilliSeconds)
+    {
+	String oldNamespace = NamespaceManager.get();
+	NamespaceManager.set("");
+
+	MemcacheService syncCache = MemcacheServiceFactory.getMemcacheService();
+	syncCache.put(key, value, Expiration.byDeltaMillis(timeInMilliSeconds));
+
+	NamespaceManager.set(oldNamespace);
+    }
+
+    /**
+     * Adds the key and value pair to the Memcache for number of days set.
+     * 
+     * @param key
+     * @param value
+     * @param numberOfDays
+     */
+    public static void setCacheForNumberOfDays(String key, Object value, int numberOfDays)
+    {
+	setCache(key, value, numberOfDays * 24 * 60 * 60 * 1000);
     }
 
     /**
