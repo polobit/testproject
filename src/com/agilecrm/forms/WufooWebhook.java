0e4de00db5347fa06d0d11836efcb5d6a6be3fa8
karthikagile <karthik@invox.com>
2013-11-08 18:57:17 +0530

    correctd error with update contact

--- src/com/agilecrm/forms/WufooWebhook.java
+++ src/com/agilecrm/forms/WufooWebhook.java
@@ -27,164 +27,142 @@ import com.googlecode.objectify.Key;
 @SuppressWarnings("serial")
 public class WufooWebhook extends HttpServlet
 {
-    protected void service(HttpServletRequest req, HttpServletResponse res)
-    {
-	try
+	protected void service(HttpServletRequest req, HttpServletResponse res)
 	{
-	    // Read hand shake key (agile API key) to authenticate data
-		String tagString = req.getParameter("HandshakeKey");
-		System.out.println("handshake key "+ tagString);
-	   
-		Contact contact = null;
-	    List<ContactField> properties = new ArrayList<ContactField>();
-
-	    // Get fields structure from form data and iterate JSON
-	    JSONObject obj = new JSONObject(req.getParameter("FieldStructure"));
-	    
-	    System.out.println("field structure "+ obj.toString());
-	    
-	    JSONArray arr = obj.getJSONArray("Fields");
-	    for (int i = 0; i < arr.length(); i++)
-	    {
-	    	
-		JSONObject json = arr.getJSONObject(i);
-		
-		// Check if data contains email, search contact based on email
-		if(json.getString("Title").toLowerCase().contains("email")){
-			contact = ContactUtil.searchContactByEmail(req.getParameter(json.getString("ID")));
-			System.out.println("contact is "+ contact);
-		
-		// If contact is not found create new contact
-			if(contact==null)
-				contact = new Contact();
-		}
-		// If data does not contain email create new contact
-		else contact = new Contact();
-
-		// Add properties to list of properties
-		properties.add(buildProperty(json.getString("Title"), req.getParameter(json.getString("ID"))));
-		Iterator<?> keys = json.keys();
-		while (keys.hasNext())
+		try
 		{
-		    // Iterate subfields
-		    String key = (String) keys.next();
-		    if (key.equals("SubFields"))
-		    {
-			JSONArray subArr = json.getJSONArray("SubFields");
-			for (int j = 0; j < subArr.length(); j++)
+			// Get API key and tags
+			String tagString = req.getParameter("HandshakeKey");
+			Contact contact = null;
+			List<ContactField> properties = new ArrayList<ContactField>();
+
+			// Get fields from field structure and iterate
+			JSONObject obj = new JSONObject(req.getParameter("FieldStructure"));
+			JSONArray arr = obj.getJSONArray("Fields");
+			for (int i = 0; i < arr.length(); i++)
 			{
-			    JSONObject subObj = subArr.getJSONObject(j);
+				JSONObject json = arr.getJSONObject(i);
 
-			    // Add properties to list of properties
-			    properties.add(buildProperty(subObj.getString("Label"),
-				    req.getParameter(subObj.getString("ID"))));
+				// Check if data contains email, search contact based on email
+				if (json.getString("Title").toLowerCase().contains("email"))
+					contact = ContactUtil.searchContactByEmail(req.getParameter(json.getString("ID")));
+
+				// If contact is not found create new contact
+				if (contact == null)
+					contact = new Contact();
 			}
-		    }
+			for (int i = 0; i < arr.length(); i++)
+			{
+				JSONObject json = arr.getJSONObject(i);
+
+				// Add properties to list of properties
+				properties.add(buildProperty(json.getString("Title"), req.getParameter(json.getString("ID")), contact));
+				Iterator<?> keys = json.keys();
+				while (keys.hasNext())
+				{
+					// Iterate subfields
+					String key = (String) keys.next();
+					if (key.equals("SubFields"))
+					{
+						JSONArray subArr = json.getJSONArray("SubFields");
+						for (int j = 0; j < subArr.length(); j++)
+						{
+							JSONObject subObj = subArr.getJSONObject(j);
+
+							// Add properties to list of properties
+							properties.add(buildProperty(subObj.getString("Label"),
+									req.getParameter(subObj.getString("ID")), contact));
+						}
+					}
+				}
+			}
+			// Format tagString and split into tagsWithKey array
+			tagString = tagString.trim();
+			tagString = tagString.replace("/, /g", ",");
+			String[] tagsWithKey = tagString.split(",");
+
+			// Remove API key from tagsWithKey array and set contact owner
+			String[] tags = Arrays.copyOfRange(tagsWithKey, 1, tagsWithKey.length);
+			Key<DomainUser> owner = APIKey.getDomainUserKeyRelatedToAPIKey(tagsWithKey[0]);
+			if (owner != null)
+			{
+				contact.setContactOwner(owner);
+				contact.properties = properties;
+				contact.addTags(tags);
+				contact.save();
+			}
 		}
-	    }
-	    // Format tagString and split into tagsWithKey array
-	    String[] tagsWithKey = new String[0];
-	    tagString = tagString.trim();
-		tagString = tagString.replace("/, /g", ",");
-		tagsWithKey = tagString.split(",");
-		System.out.println("length of tags with key " + tagsWithKey.length);
-		
-		// Remove API key from tagsWithKey array
-		String[] tags = Arrays.copyOfRange(tagsWithKey, 1, tagsWithKey.length);
-		System.out.println("length of tags " + tags.length);
-		
-		System.out.println("contact is "+contact);
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return;
+		}
+	}
 
-	    try{
-	    System.out.println("properties "+ properties);
-	    // Add properties to contact and set contact owner
-	    contact.properties = properties;
-	    contact.addTags(tags);
-	    }
-	    catch(Exception e){
-	    	e.printStackTrace();
-	    	return;
-	    }
-	    
-	    System.out.println("api key is "+ tagsWithKey[0]);
-	    
-	    Key<DomainUser> owner = APIKey.getDomainUserKeyRelatedToAPIKey(tagsWithKey[0]);
-	    System.out.println("owner id is "+ owner.getId());
-	    
-	    if (owner != null)
-	    {
-		contact.setContactOwner(owner);
-		contact.save();
-	    }
-	}
-	catch (Exception e)
+	public static ContactField buildProperty(String name, String value, Contact contact)
 	{
-	    e.printStackTrace();
-	    System.out.println("exception occured");
-	    return;
-	}
-    }
+		name = name.toLowerCase();
 
-    public static ContactField buildProperty(String name, String value)
-    {
-	name = name.toLowerCase();
-	ContactField field = new ContactField();
+		// Get contact field of contact, based on its name
+		ContactField field = contact.getContactFieldByName(name);
+		if (field == null)
+			field = new ContactField();
 
-	// Set field type to SYSTEM for name, email, company, title, phone, all
-	// other fields save as CUSTOM.
-	if (name.equals("name") || name.equals("first") || name.equalsIgnoreCase("first name"))
-	{
-	    field.name = Contact.FIRST_NAME;
-	    field.type = FieldType.SYSTEM;
-	    field.value = value;
+		// Set field type to SYSTEM for name, email, company, title, phone, all
+		// other fields save as CUSTOM.
+		if (name.equals("name") || name.equals("first") || name.equalsIgnoreCase("first name"))
+		{
+			field.name = Contact.FIRST_NAME;
+			field.type = FieldType.SYSTEM;
+			field.value = value;
+		}
+		else if (name.equals("last") || name.equalsIgnoreCase("last name"))
+		{
+			field.name = Contact.LAST_NAME;
+			field.type = FieldType.SYSTEM;
+			field.value = value;
+		}
+		else if (name.contains("organisation") || name.contains("organization") || name.equals(Contact.COMPANY))
+		{
+			field.name = Contact.COMPANY;
+			field.value = value;
+			field.type = FieldType.SYSTEM;
+		}
+		else if (name.contains("designation") || name.equals(Contact.TITLE))
+		{
+			field.name = Contact.TITLE;
+			field.value = value;
+			field.type = FieldType.SYSTEM;
+		}
+		else if (name.contains("phone"))
+		{
+			field.name = "phone";
+			field.value = value;
+			field.type = FieldType.SYSTEM;
+			field.subtype = "work";
+		}
+		else if (name.contains("mobile"))
+		{
+			field.name = "phone";
+			field.value = value;
+			field.type = FieldType.SYSTEM;
+			field.subtype = "home";
+		}
+		else if (name.contains("email"))
+		{
+			if (ContactUtil.isValidEmail(value))
+			{
+				field.name = Contact.EMAIL;
+				field.value = value;
+				field.type = FieldType.SYSTEM;
+			}
+		}
+		else
+		{
+			field.name = name;
+			field.value = value;
+			field.type = FieldType.CUSTOM;
+		}
+		return field;
 	}
-	else if (name.equals("last") || name.equalsIgnoreCase("last name"))
-	{
-	    field.name = Contact.LAST_NAME;
-	    field.type = FieldType.SYSTEM;
-	    field.value = value;
-	}
-	else if (name.contains("organisation") || name.contains("organization") || name.equals(Contact.COMPANY))
-	{
-	    field.name = Contact.COMPANY;
-	    field.value = value;
-	    field.type = FieldType.SYSTEM;
-	}
-	else if (name.contains("designation") || name.equals(Contact.TITLE))
-	{
-	    field.name = Contact.TITLE;
-	    field.value = value;
-	    field.type = FieldType.SYSTEM;
-	}
-	else if (name.contains("phone"))
-	{
-	    field.name = "phone";
-	    field.value = value;
-	    field.type = FieldType.SYSTEM;
-	    field.subtype = "work";
-	}
-	else if (name.contains("mobile"))
-	{
-	    field.name = "phone";
-	    field.value = value;
-	    field.type = FieldType.SYSTEM;
-	    field.subtype = "home";
-	}
-	else if (name.contains("email"))
-	{
-	    if (ContactUtil.isValidEmail(value))
-	    {
-	    	field.name = Contact.EMAIL;
-	    	field.value = value;
-	    	field.type = FieldType.SYSTEM;
-	    }
-	}
-	else
-	{
-	    field.name = name;
-	    field.value = value;
-	    field.type = FieldType.CUSTOM;
-	}
-	return field;
-    }
-	}
+}
