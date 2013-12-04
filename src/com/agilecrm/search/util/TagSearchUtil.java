4ee8682a29f201aa41037aab467c3217ee07f57c
Yaswanth <praveen@invox.com>
2013-12-04 20:17:34 +0530

    Tags graph ratio fix

--- src/com/agilecrm/search/util/TagSearchUtil.java
+++ src/com/agilecrm/search/util/TagSearchUtil.java
@@ -197,16 +197,15 @@ public class TagSearchUtil
 	    int tag1Count = getTagCount(contactFilter, tag1, startTimeMilli + "", startCalendar.getTimeInMillis() + "");
 	    int tag2Count = getTagCount(contactFilter, tag2, startTimeMilli + "", startCalendar.getTimeInMillis() + "");
 
-	    System.out.println("tag1 : " + tag1 + " - " + tag1Count + "tag2 : "
-		    + tag2 + " - " + tag2Count);
 
 	    // Get Tag Ratio
 	    float tagRatio = 0;
-	    if (tag2Count != 0)
+	    if (tag2Count != 0 && tag1Count != 0)
 		tagRatio = ((float) tag2Count / tag1Count) * 100f;
 
-	    tagsCount.put("Conversion", round(tagRatio, 2));
+	    Float tagRatio1 = round(tagRatio, 2);
 
+	    tagsCount.put("Conversion", tagRatio1);
 	    // Put time and tags array
 	    tagsCountJSONObject.put(startTimeMilli / 1000 + "", tagsCount);
 
