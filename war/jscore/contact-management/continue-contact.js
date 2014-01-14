fe19dd2c71bb6e0f504a922f1650003cbf44088d
Yaswanth <praveen@invox.com>
2014-01-14 11:37:46 +0530

    Website filling fix

--- war/jscore/contact-management/continue-contact.js
+++ war/jscore/contact-management/continue-contact.js
@@ -450,7 +450,7 @@ function deserialize_contact(contact, template) {
 	}
 	
 	// If contact is added from social suite, need to add website.
-	socialsuite_add_website();
+	//socialsuite_add_website();
 }
 
 /**
