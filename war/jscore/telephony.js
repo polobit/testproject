0bfb3db0056f334916b6202d94904dea61bf1461
farahnaj <farahinamdar@gmail.com>
2014-01-17 14:00:08 +0530

    twitter4j updated.

--- war/jscore/telephony.js
+++ war/jscore/telephony.js
@@ -3,16 +3,52 @@
         <script src="build/candybar.js"></script>
 */
 var cb;
+//var dialer;
 
 $(function()
   {			
 	console.log("In telephony.js");
 	
-	head.js(LIB_PATH + 'lib/candybar.js', function()
+	head.js(LIB_PATH + 'lib/candybar.js',LIB_PATH + 'lib/dialpad.js',LIB_PATH + 'lib/att.phonenumber.js', function()
 		{
 		   cb = new CandyBar();
-		   cb.render();		     		   
-		});	    
+		   cb.render();
+		   
+		   
+		   
+		   window.dialer = new Dialpad({
+		        onPress: function (key) {
+		            console.log('a key was pressed', key);
+		        },
+		        onCallableNumber: function (number) {
+		            console.log('we have a number that seems callable', number);
+		        },
+		        onHide: function () {
+		            console.log('removed it');
+		        },
+		        onCall: function (number) {
+		            console.log('The call button was pressed', number);
+		        }
+		    });
+
+		    document.addEventListener('DOMContentLoaded', function () {
+		      document.body.insertBefore(dialer.render(), document.body.firstChild);
+		    }, false);
+
+		});	   
+	
+	
+	
+	// calling render returns the rendered dialer
+	// as a DOM element with all the right handlers
+	//window.dialer.render();
+
+	// OPTIONALLY: you can also add event listeners later if you prefer.
+	/*dialer.on('call', function (number) {
+	    // assuming you've created a phone object..
+	    phone.dial(number);
+	});*/
+	      
     
     $(".contact-make-call").die().live("click", function(e)
      {
@@ -27,15 +63,17 @@ $(function()
         {
     	  alert(name +"'s contact number not added."); 
     	  return;
-    	}
-    	    	
-		  if(!setUser(name, phone, image)) 
+    	}  	
+    	    	    	
+		  /*if(!setUser(name, phone, image)) 
 			  {
     	        callUser();
     	        
     	        // After call receive.
     	        setStateActive()
-			  }
+			  }*/
+    	
+    	window.dialer.render();
      });    
     
     $(".call_end").die().live("click", function(e)
