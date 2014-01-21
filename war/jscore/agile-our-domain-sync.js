176f1cdf23df48bbd2ae9184f1bfebd5eb0ae8a2
Yaswanth <praveen@invox.com>
2014-01-21 18:50:29 +0530

    Parsing date epoch into int in javascript

--- war/jscore/agile-our-domain-sync.js
+++ war/jscore/agile-our-domain-sync.js
@@ -44,6 +44,7 @@ function add_current_loggedin_time()
 	var current_date_object = new Date();
 	var current_date_string = current_date_object.getUTCMonth() + 1 + "/" + current_date_object.getUTCDate() + "/" + current_date_object.getUTCFullYear();
 
+	console.log(parseInt(current_date_object.getTime()/1000));
 	
 	// Gets logged in time property.
 	var loggedin_time_property = getProperty(Agile_Contact.properties, 'Last login');
@@ -61,7 +62,7 @@ function add_current_loggedin_time()
 	if (existing_date_string && existing_date_string == current_date_string)
 		return;
 
-	loggedin_time_property = create_contact_custom_field("Last login", current_date_object.getTime()/1000, 'CUSTOM');
+	loggedin_time_property = create_contact_custom_field("Last login", parseInt(current_date_object.getTime()/1000), 'CUSTOM');
 
 	_agile.add_property(loggedin_time_property);
 }
