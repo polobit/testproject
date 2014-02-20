7d2d4f44348e112c214f30bcb300b526cff8169b
karthikagile <karthik@invox.com>
2014-02-20 13:28:57 +0530

    added templates

---
+++ war/misc/modal-templates/modal_templates_structure.js
@@ -0,0 +1,106 @@
+{
+    "templates": [
+        {
+            "label": "Information Popup",
+            "category": "information",
+            "themes": [
+                {
+                    "label": "Sleak",
+                    "title": "sleak-information",
+                    "theme_preview": "/misc/modal-templates/information/sleak/images/sleak.png",
+                    "layouts": [
+                        {
+                            "title":"sleak-information",
+                            "href": "/misc/modal-templates/information/sleak/images/sleak.png",
+                            "link": "/misc/modal-templates/information/sleak/sleak.html"
+                        }
+                    ]
+                },
+                {
+                    "label": "Radiant",
+                    "title": "radiant-information",
+                    "theme_preview": "/misc/modal-templates/information/radiant/images/radiant.png",
+                    "layouts": [
+                        {
+                            "title": "radiant-information",
+                            "href": "/misc/modal-templates/information/radiant/images/radiant.png",
+                            "link": "/misc/modal-templates/information/radiant/radiant.html"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "label": "Confirmation Popup",
+            "category": "confirmation",
+            "themes": [
+                {
+                    "label": "Elegant",
+                    "title": "elegant-confirmation",
+                    "theme_preview": "/misc/modal-templates/confirmation/elegant/images/elegant.png",
+                    "layouts": [
+                        {
+                            "title": "elegant-confirmation",
+                            "href": "/misc/modal-templates/confirmation/elegant/images/elegant.png",
+                            "link": "/misc/modal-templates/confirmation/elegant/elegant.html"
+                        }
+                    ]
+                },
+                {
+                    "label": "Fullscreen",
+                    "title": "fullscreen-confirmation",
+                    "theme_preview": "/misc/modal-templates/confirmation/fullscreen/images/fullscreen.png",
+                    "layouts": [
+                        {
+                            "title": "fullscreen-confirmation",
+                            "href": "/misc/modal-templates/confirmation/fullscreen/images/fullscreen.png",
+                            "link": "/misc/modal-templates/confirmation/fullscreen/fullscreen.html"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "label": "Signup Form Popup",
+            "category": "form",
+            "themes": [
+                {
+                    "label": "Standout",
+                    "title": "standout-form",
+                    "theme_preview": "/misc/modal-templates/form/standout/images/standout.png",
+                    "layouts": [
+                        {
+                            "title": "standout-form",
+                            "href": "/misc/modal-templates/form/standout/images/standout.png",
+                            "link": "/misc/modal-templates/form/standout/standout.html"
+                        }
+                    ]
+                },
+                {
+                    "label": "Elegant",
+                    "title": "elegant-form",
+                    "theme_preview": "/misc/modal-templates/form/elegant/images/elegant.png",
+                    "layouts": [
+                        {
+                            "title": "elegant-form",
+                            "href": "/misc/modal-templates/form/elegant/images/elegant.png",
+                            "link": "/misc/modal-templates/form/elegant/elegant.html"
+                        }
+                    ]
+                },
+                {
+                    "label": "Newsletter",
+                    "title": "newsletter-form",
+                    "theme_preview": "/misc/modal-templates/form/newsletter/images/newsletter.png",
+                    "layouts": [
+                        {
+                            "title": "newsletter-form",
+                            "href": "/misc/modal-templates/form/newsletter/images/newsletter.png",
+                            "link": "/misc/modal-templates/form/newsletter/newsletter.html"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
