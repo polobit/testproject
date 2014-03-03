47366fb3e5afa190a594eb8326eb96974616c91b
agilecrm <manohar@Manohars-MacBook-Pro.local>
2014-03-03 17:37:06 +0530

    Removed head js dependency for web rules. It's downloaded only when required.

--- war/stats/js/agile_webrules.js
+++ war/stats/js/agile_webrules.js
@@ -16,11 +16,7 @@ function agile_webRules(callback)
 function _agile_execute_web_rules()
 {
 	// Download web rules and call _agile_webrules
-	_agile_require_js("https://d2l6lw2yloivu1.cloudfront.net/web-grabbers/agile-webrules.js", 
-			function(){
-				_agile_require_js("https://d2l6lw2yloivu1.cloudfront.net/web-grabbers/lib/head.load.min.js", _agile_webrules);
-			});
-	);
+	_agile_require_js("https://d2l6lw2yloivu1.cloudfront.net/web-grabbers/agile-webrules.js", _agile_webrules);
 }
 
 /*
