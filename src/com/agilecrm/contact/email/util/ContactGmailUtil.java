d6a784835a5736d132a37ddab40b2c4510288830
agilecrm <manohar@Manohars-MacBook-Pro.local>
2014-03-12 20:35:58 +0530

    Added oauth2 support to send gmail prefs correctly to the IMAP/Gmail Server

--- src/com/agilecrm/contact/email/util/ContactGmailUtil.java
+++ src/com/agilecrm/contact/email/util/ContactGmailUtil.java
@@ -2,6 +2,8 @@ package com.agilecrm.contact.email.util;
 
 import java.net.URLEncoder;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.agilecrm.user.AgileUser;
 import com.agilecrm.user.SocialPrefs;
 import com.agilecrm.user.SocialPrefs.Type;
@@ -24,15 +26,15 @@ public class ContactGmailUtil
     @SuppressWarnings("deprecation")
     public static String getGmailURL(String searchEmail, String offset, String count)
     {
-    	// Get Gmail Social Prefs
-    	Type socialPrefsTypeEnum = SocialPrefs.Type.GMAIL;
-    	SocialPrefs gmailPrefs = SocialPrefsUtil.getPrefs(AgileUser.getCurrentAgileUser(), socialPrefsTypeEnum);
-    
-    	if (gmailPrefs == null)
-    		return null;
-    
-    	return ContactGmailUtil.getGmailURLForPrefs(gmailPrefs, searchEmail, offset, count);
-    
+	// Get Gmail Social Prefs
+	Type socialPrefsTypeEnum = SocialPrefs.Type.GMAIL;
+	SocialPrefs gmailPrefs = SocialPrefsUtil.getPrefs(AgileUser.getCurrentAgileUser(), socialPrefsTypeEnum);
+
+	if (gmailPrefs == null)
+	    return null;
+
+	return ContactGmailUtil.getGmailURLForPrefs(gmailPrefs, searchEmail, offset, count);
+
     }
 
     /**
@@ -50,22 +52,29 @@ public class ContactGmailUtil
      */
     public static String getGmailURLForPrefs(SocialPrefs gmailPrefs, String searchEmail, String offset, String count)
     {
-    	String userName = gmailPrefs.email;
-    	String host = "imap.gmail.com";
-    	String port = "993";
-    	String consumerKey = "anonymous";
-    	String consumerSecret = "anonymous";
-    
-    	String oauth_key = gmailPrefs.token;
-    	String oauth_secret = gmailPrefs.secret;
-    
-    	String url = "https://agile-imap.appspot.com/imap?command=oauth_email&user_name=" + URLEncoder.encode(userName)
-    			+ "&search_email=" + searchEmail + "&host=" + URLEncoder.encode(host) + "&port="
-    			+ URLEncoder.encode(port) + "&offset=" + offset + "&count=" + count + "&consumer_key="
-    			+ URLEncoder.encode(consumerKey) + "&consumer_secret=" + URLEncoder.encode(consumerSecret)
-    			+ "&oauth_key=" + URLEncoder.encode(oauth_key) + "&oauth_secret=" + URLEncoder.encode(oauth_secret);
-    
-    	return url;
-    }
+	String userName = gmailPrefs.email;
+	String host = "imap.gmail.com";
+	String port = "993";
+	String consumerKey = "anonymous";
+	String consumerSecret = "anonymous";
 
+	String oauth_key = gmailPrefs.token;
+	String oauth_secret = gmailPrefs.secret;
+
+	String command = "oauth_email";
+
+	// Gmail Prefs has been updated from oauth1 (deprecated) to oauth2. For
+	// oauth2, we store secret as v2
+	if (StringUtils.equalsIgnoreCase(gmailPrefs.secret, "v2"))
+	{
+	    return "https://agile-imap.appspot.com/imap?command=oauth_email2&user_name=" + URLEncoder.encode(userName) + "&search_email=" + searchEmail
+		    + "&host=" + URLEncoder.encode(host) + "&port=" + URLEncoder.encode(port) + "&offset=" + offset + "&count=" + count + "&oauth_key="
+		    + URLEncoder.encode(oauth_key);
+	}
+
+	return "https://agile-imap.appspot.com/imap?command=oauth_email&user_name=" + URLEncoder.encode(userName) + "&search_email=" + searchEmail + "&host="
+		+ URLEncoder.encode(host) + "&port=" + URLEncoder.encode(port) + "&offset=" + offset + "&count=" + count + "&consumer_key="
+		+ URLEncoder.encode(consumerKey) + "&consumer_secret=" + URLEncoder.encode(consumerSecret) + "&oauth_key=" + URLEncoder.encode(oauth_key)
+		+ "&oauth_secret=" + URLEncoder.encode(oauth_secret);
+    }
 }
