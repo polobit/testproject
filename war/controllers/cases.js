c6f49d3e9ac0eb0496bfc0224b3ff46ebe2e864a
sukanya <sukanya@invox.com>
2014-05-18 00:56:53 +0530

    updation time in cases

--- war/controllers/cases.js
+++ war/controllers/cases.js
@@ -11,8 +11,14 @@ var CasesRouter = Backbone.Router.extend({
 	 */
 	listCases : function()
 	{
-		this.casesCollectionView = new Base_Collection_View({ url : 'core/api/cases', restKey : "case", templateKey : "cases", cursor : true, page_size : 25,
-			individual_tag_name : 'tr' });
+		this.casesCollectionView = new Base_Collection_View({ url : 'core/api/cases', sort_collection : false, restKey : "case", templateKey : "cases", cursor : true, page_size : 25,
+			individual_tag_name : 'tr',	postRenderCallback : function(el) {
+				includeTimeAgo(el);
+			},
+			appendItemCallback : function(el)
+			{
+				includeTimeAgo(el);
+			} });
 
 		this.casesCollectionView.collection.fetch();
 
