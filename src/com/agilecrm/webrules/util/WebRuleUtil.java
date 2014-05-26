e6705f750ba0c877017c2fe82d008b20720f7b37
agilecrm <manohar@Manohars-MacBook-Pro.local>
2014-05-26 11:30:16 +0530

    Added code to send country along with each web rule at the server side

--- src/com/agilecrm/webrules/util/WebRuleUtil.java
+++ src/com/agilecrm/webrules/util/WebRuleUtil.java
@@ -2,6 +2,8 @@ package com.agilecrm.webrules.util;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import com.agilecrm.webrules.WebRule;
 
 public class WebRuleUtil
@@ -9,5 +11,17 @@ public class WebRuleUtil
     public static List<WebRule> getAllWebRules()
     {
 	return WebRule.dao.fetchAll();
+    }
+
+    // Fill Countries
+    public static List<WebRule> fillCountry(List<WebRule> webRules, HttpServletRequest request)
+    {
+	for (WebRule webRule : webRules)
+	{
+	    // Fill the country
+	    webRule.country = request.getHeader("X-AppEngine-Country");
+	}
+
+	return webRules;
     }
 }
