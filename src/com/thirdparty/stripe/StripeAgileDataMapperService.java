a04161cd89430419a71dcb4b71991f565064e7c0
jitendra <jitendra@jitendra>
2014-06-12 16:06:06 +0530

    Implemented Stripe Data Import Implemented Stripe widget configuration and syn data from stripe

--- src/com/thirdparty/stripe/StripeAgileDataMapperService.java
+++ src/com/thirdparty/stripe/StripeAgileDataMapperService.java
@@ -13,6 +13,10 @@ import org.codehaus.jettison.json.JSONObject;
 import com.agilecrm.contact.Contact;
 import com.agilecrm.contact.Contact.Type;
 import com.agilecrm.contact.ContactField;
+import com.agilecrm.contact.ContactField.FieldType;
+import com.agilecrm.user.util.DomainUserUtil;
+import com.agilecrm.widgets.Widget;
+import com.agilecrm.widgets.util.WidgetUtil;
 import com.stripe.model.Card;
 import com.stripe.model.Customer;
 
@@ -29,9 +33,17 @@ public class StripeAgileDataMapperService {
 		fields.add(new ContactField(Contact.EMAIL, customer.getEmail(), "work"));
 		Card card = customer.getActiveCard();
 		fields.add(new ContactField(Contact.FIRST_NAME, card.getName(), null));
-
-		JSONObject address = new JSONObject();
+		Widget widget = WidgetUtil.getWidget("Stripe");
 		try {
+		if(widget != null){
+			JSONObject prefs = new JSONObject(widget.prefs);
+			if(prefs.has("stripe_field_name"))
+				fields.add(new ContactField(prefs.get("stripe_field_name").toString(),customer.getId(),null));
+			
+		}
+        
+		JSONObject address = new JSONObject();
+	
 			address.put("Street",
 					card.getAddressLine1() + " " + card.getAddressLine2());
 			address.put("City", card.getAddressCity());
@@ -40,6 +52,12 @@ public class StripeAgileDataMapperService {
 			address.put("Zip", card.getAddressZip());
 			fields.add(new ContactField(Contact.ADDRESS, address.toString(),"Work"));
 			ctx.properties = fields;
+			List<ContactField> l = ctx.getProperties();
+			for(ContactField  f:l){
+			  if(f.type == FieldType.CUSTOM){
+				  System.out.println(f.name);
+			  }
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 
