ccceeff56125f313affe70699ca7f181c6b91eed
karthikagile <karthik@invox.com>
2014-06-25 17:40:29 +0530

    added allowed domains

---
+++ war/stats/js/agile_allowed_domains.js
@@ -0,0 +1,11 @@
+/**
+ * Function to get allowed domains string
+ */
+function agile_allowedDomains(callback){
+	
+	// GET
+	var agile_url = agile_id.getURL() + "/allowed-domains?callback=?&id=" + agile_id.get();
+	
+	// Callback
+	agile_json(agile_url,callback);
+}
