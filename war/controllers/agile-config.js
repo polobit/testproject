35f3eb0e1bc916398a13ed2cbe2ee9f59a190390
RajithaArelli <rajitha@invox.com>
2014-06-25 14:48:17 +0530

    css changes onboarding saas page

--- war/controllers/agile-config.js
+++ war/controllers/agile-config.js
@@ -11,7 +11,39 @@ var AgileConfigRouter = Backbone.Router.extend({
 	
 	onBoarding : function(link)
 	{
-		$("#content").html(getTemplate("onboarding-"+ link), {});
-	},
+		if (!CURRENT_DOMAIN_USER.is_admin)
+		{
+			$('#content').html("You have no Admin Privileges");
+			return;
+		}
+		$("#content").html(getTemplate("admin-settings"), {});
+		head.js(LIB_PATH + 'lib/prettify-min.js','../lib/zeroclipboard/ZeroClipboard.js', function()
+		{
+			var view = new Base_Model_View({ url : '/core/api/api-key', template : "admin-settings-api-key-model", postRenderCallback : function(el)
+			{
+				prettyPrint();
+				console.log(link+"link is");
+				if(link)
+				{
+					$(el).find('#APITab a[href="#'+ link +'"]').trigger('click');
+				}
+				
+				initZeroClipboard("saas_api_track_code_icon", "saas_api_track_code");
+				
+
+			} });
+			$('#content').find('#admin-prefs-tabs-content').html(view.render().el);
+			$('#content').find('#AdminPrefsTab .active').removeClass('active');
+			$('#content').find('.analytics-code-tab').addClass('active');
+			// $('#content').html(view.el);
+		});
+		
+		$.getJSON("core/api/api-key", function(data)
+			    {
+					console.log(data);
+					$("#content").html(getTemplate("onboarding-"+ link, data));
+			    });
+		
+	}
 	
 });
