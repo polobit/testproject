870e1c39f76726d5e1d7d7ca95521b8dd1dbdf8a
jitendra <jitendra@jitendra>
2014-07-03 19:18:05 +0530

    Implemented Stripe Customer import service

--- src/com/thirdparty/stripe/StripeDataService.java
+++ src/com/thirdparty/stripe/StripeDataService.java
@@ -3,19 +3,36 @@
  */
 package com.thirdparty.stripe;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+import org.json.JSONObject;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Verb;
+
+import com.agilecrm.Globals;
 import com.google.appengine.api.taskqueue.TaskQueuePb.TaskQueueService;
 import com.thirdparty.google.ContactPrefs;
+import com.thirdparty.google.ContactPrefs.Duration;
 import com.thirdparty.google.ContactPrefs.Type;
 import com.thirdparty.google.ContactsImportUtil;
 import com.thirdparty.google.utl.ContactPrefsUtil;
@@ -32,66 +49,58 @@ public class StripeDataService
 {
 
     /**
-     * This will save user auth preferences and validate
+     * Return User's Save stripe contactPref
      * 
-     * @param username
-     * @param password
-     * @param apiKey
      * @return
      */
-    @Path("/savePrefs")
-    @POST
-    @Consumes({ MediaType.APPLICATION_FORM_URLENCODED })
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public ContactPrefs saveCredential(@FormParam("username") String username, @FormParam("password") String password,
-	    @FormParam("apiKey") String apiKey)
+    @GET
+    @Path("/importSetting")
+    public ContactPrefs getPref()
     {
-	ContactPrefs ctx = new ContactPrefs();
-	ctx.userName = username;
-	ctx.password = password;
-	ctx.apiKey = apiKey;
-	ctx.type = Type.STRIPE;
-	try
-	{
-	    ctx.save();
-
-	}
-	catch (Exception e)
-	{
-	    throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage())
-		    .build());
-	}
-
-	return ctx;
+	return ContactPrefsUtil.getPrefsByType(Type.STRIPE);
     }
 
     /**
-     * This code will retrieve data from Stripe API and initialize back end
-     * import task that will add task in queue and run in Background
-     * {@link TaskQueueService} and through exception customer not found
+     * update Users contactPrefs if token will be there in contactPref then it
+     * will initialize import data from stripe
      * 
-     * @param customer
-     * @throws Exception
+     * @param prefs
      */
-    @Path("/importData")
-    @POST
-    @Consumes({ MediaType.APPLICATION_FORM_URLENCODED })
-    @Produces({ MediaType.APPLICATION_JSON })
-    public void fetchDataFromStripe(@FormParam("customer") boolean customer) throws Exception
+    @PUT
+    @Path("/importSetting")
+    public void saveImportPref(ContactPrefs prefs)
     {
-	ContactPrefs ctxPrefs = ContactPrefsUtil.getPrefsByType(Type.STRIPE);
-	if (customer)
-	{
-	    ArrayList<String> options = new ArrayList<String>();
-	    options.add("customer");
-	    ctxPrefs.thirdPartyField = options;
-	    ContactsImportUtil.initilaizeImportBackend(ctxPrefs);
 
-	}
-	else
-	{
-	    throw new Exception("Invalid Option. Please try again");
-	}
+	ContactPrefs pref = ContactPrefsUtil.get(prefs.id);
+	pref.save();
+
+	if (!pref.token.isEmpty() && pref != null)
+	    doImport(pref);
+
+    }
+
+    /**
+     * delete ContactPref
+     */
+    @DELETE
+    @Path("/importSetting")
+    public void deletePref()
+    {
+	ContactPrefsUtil.delete(Type.STRIPE);
+
+    }
+
+    /**
+     * initialize import data for given configuration preferences
+     * 
+     * @param pref
+     */
+    private void doImport(ContactPrefs pref)
+    {
+	ArrayList<String> options = new ArrayList<String>();
+	options.add("customer");
+	pref.thirdPartyField = options;
+	ContactsImportUtil.initilaizeImportBackend(pref);
 
     }
 
