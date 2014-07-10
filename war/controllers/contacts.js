0f51bfc1ff69701b119281a9391dde57b56440f2
Yaswanth <praveen@invox.com>
2014-07-10 20:14:00 +0530

    Webstats error message fix which is shown in multiple places other than webstats tab

--- war/controllers/contacts.js
+++ war/controllers/contacts.js
@@ -490,7 +490,7 @@ var ContactsRouter = Backbone.Router.extend({
 		}
 
 		// Contact Duplicate
-		var contact = this.contactsListView.collection.get(this.contactDetailView.model.id);
+		var contact = this.contactDetailView.model
 		var json = contact.toJSON();
 
 		// Delete email as well as it has to be unique
