1f73c1f1001f22e024d67d44334ddbe1da04ec3d
jitendra <jeetu345@gmail.com>
2014-08-08 18:13:28 +0530

    Quick book find customer

--- src/com/thirdparty/quickbook/QuickBookSyncImpl.java
+++ src/com/thirdparty/quickbook/QuickBookSyncImpl.java
@@ -25,7 +25,6 @@ import com.intuit.ipp.services.QueryResult;
  */
 public class QuickBookSyncImpl extends OneWaySyncService
 {
-	private static String BASE_URL = "https://quickbooks.api.intuit.com/v3/company/1248774125/query?query=SELECT%20FROM%20Customer";
 
 	@Override
 	public Class<? extends WrapperService> getWrapperService()
@@ -72,36 +71,5 @@ public class QuickBookSyncImpl extends OneWaySyncService
 		return list;
 	}
 
-	public static void main(String[] args)
-	{
-		String id = "1248774125";
-		String apToken = "qyprdx6AjL6Mf4ExWADoW1qCr9PP5ewOzK0imq8sVnWr91Bn";
-		String query = "SELECT * FROM Customer";
-		Map<String, String> param = new HashMap<String, String>();
-		param.put("companyID", id);
-		// param.put("query", query);
-		try
-		{
-
-			OAuthAuthorizer oauth = new OAuthAuthorizer(Globals.QUICKBOOKS_CONSUMER_KEY,
-					Globals.QUICKBOOKS_CONSUMER_SECRET, "qyprdPTMnT9LFQgJkB47FmiHROl7njYXtOKhL12I7rpauzco",
-					"ZbJssQY1qRlSKu9HTSyncRW6utNq7gTnehE0Zfo1");
-			Context context = new Context(oauth, Globals.QUICKBOOKS_APP_KEY, ServiceType.QBO, id);
-			DataService service = new DataService(context);
-			QueryResult q = service.executeQuery("SELECT * FROM Customer");
-			List<Customer> customers = (List<Customer>) q.getEntities();
-			for (Customer c : customers)
-			{
-				System.out.println(c.getDisplayName());
-			}
-
-			// Response response = req.send();
-			// System.out.println(response.getCode());
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
-
+	
 }
