a647c2b5b8d347e021225467d87af3ed6715c339
saikiran-k <saikiran@invox.com>
2014-08-22 10:51:18 +0530

    Backward compatibility for search all.
    Backward compatibility for search all in search API.

--- src/com/agilecrm/core/api/search/SearchAPI.java
+++ src/com/agilecrm/core/api/search/SearchAPI.java
@@ -46,7 +46,7 @@ public class SearchAPI
 	return new AppengineSearch<Contact>(Contact.class).getSimpleSearchResults(keyword, Integer.parseInt(count),
 		cursor, type);
     }
-    
+
     @Path("{keyword}")
     @GET
     @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
@@ -56,7 +56,6 @@ public class SearchAPI
 	return new AppengineSearch<Contact>(Contact.class).getSimpleSearchResults(keyword, Integer.parseInt(count),
 		cursor, type);
     }
-
 
     /**
      * It initializes AppengineSearch, which is used to build query based on the
@@ -74,6 +73,32 @@ public class SearchAPI
     @GET
     @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     public Collection searchAll(@QueryParam("q") String keyword, @QueryParam("page_size") String count,
+	    @QueryParam("cursor") String cursor, @QueryParam("type") String type)
+    {
+
+	if (StringUtils.isEmpty(type))
+	    return new QueryDocument(new Document().index, null).simpleSearch(keyword, Integer.parseInt(count), cursor);
+
+	return new QueryDocument(new Document().index, null).simpleSearchWithType(keyword, Integer.parseInt(count),
+		cursor, type);
+    }
+
+    /**
+     * It initializes AppengineSearch, which is used to build query based on the
+     * search keyword. AppengineSearch calls {@link QueryDocument} to perform
+     * search, based on the keyword and cursor sent. It searchs on all the
+     * entities (contacts, deals, cases)
+     * 
+     * @param keyword
+     * @param count
+     * @param cursor
+     * @param type
+     * @return
+     */
+    @Path("/all/{keyword}")
+    @GET
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public Collection searchAllOld(@PathParam("keyword") String keyword, @QueryParam("page_size") String count,
 	    @QueryParam("cursor") String cursor, @QueryParam("type") String type)
     {
 
