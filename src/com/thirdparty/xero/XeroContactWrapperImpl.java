c64b67c6bdbe0bf4aea40c1915957444cb99dd22
jitendra <jeetu345@gmail.com>
2014-08-26 11:18:26 +0530

    implemented Xero ContactWrapper

---
+++ src/com/thirdparty/xero/XeroContactWrapperImpl.java
@@ -0,0 +1,92 @@
+/**
+ * 
+ */
+package com.thirdparty.xero;
+
+import java.util.List;
+
+import com.agilecrm.contact.Contact;
+import com.agilecrm.contact.ContactField;
+import com.agilecrm.contact.Note;
+import com.agilecrm.contact.sync.wrapper.ContactWrapper;
+import com.agilecrm.contact.sync.wrapper.WrapperService;
+
+/**
+ * @author jitendra
+ *
+ */
+public class XeroContactWrapperImpl extends ContactWrapper
+{
+
+    @Override
+    public void wrapContact()
+    {
+	// TODO Auto-generated method stub
+	
+    }
+
+    @Override
+    public ContactField getFirstName()
+    {
+	return null;
+    }
+
+    @Override
+    public ContactField getLastName()
+    {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public ContactField getEmail()
+    {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public ContactField getPhoneNumber()
+    {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public ContactField getOrganization()
+    {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public String getDescription()
+    {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public List<String> getTags()
+    {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public ContactField getAddress()
+    {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public List<Note> getNotes()
+    {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+   
+
+}
