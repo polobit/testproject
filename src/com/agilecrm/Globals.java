df807a8628c48326402dc9febcf698994c277fcc
jitendra <jeetu345@gmail.com>
2014-09-01 17:56:49 +0530

    Quickbook change key from dev to production key

--- src/com/agilecrm/Globals.java
+++ src/com/agilecrm/Globals.java
@@ -70,7 +70,8 @@ public class Globals
     public static final String PAYPAL = "Paypal";
 
     // development keys
-  //  public static final String DEV_STRIPE_API_KEY = "sk_test_qxs4FCoEJ3o5aED4d1rIWiCE";
+    // public static final String DEV_STRIPE_API_KEY =
+    // "sk_test_qxs4FCoEJ3o5aED4d1rIWiCE";
 
     // Stripe live keys
     // public static final String STRIPE_API_KEY =
@@ -89,21 +90,20 @@ public class Globals
     public static final String STRIPE_API_KEY = STRIPE_LIVE_API_KEY;
     public static final String STRIPE_CLIENT_ID = STRIPE_LIVE_CLIENT_ID;
 
-
     // Xero live keys
     public static final String XERO_API_KEY = "DLVO983KIWJ7AOZDYGVLNSC5VORU1F";
     public static final String XERO_CLIENT_ID = "EGL5WPETBTSYYQ6E8QI4JSZSOW3DZW";
     // develop key
-    public static final String QUICKBOOKS_APP_KEY = "f0fca6d6b0541b470bb9711b713000493994";
-    public static final String QUICKBOOKS_CONSUMER_KEY = "qyprdnkO01AkcMJjNmabqS1PBDsmFp";
-    public static final String QUICKBOOKS_CONSUMER_SECRET = "CmBH6OWTlDmemY3uHOBIjFE87MZ6O8FRcsLqtYZF";
-    // production key
     // public static final String QUICKBOOKS_APP_KEY =
-    // "08945c87bae1fb46bab9241ba0e81dc19bbf";
+    // "f0fca6d6b0541b470bb9711b713000493994";
     // public static final String QUICKBOOKS_CONSUMER_KEY =
-    // "qyprdCLvyUgaMn6K9lrd3eyPDn1DuL";
+    // "qyprdnkO01AkcMJjNmabqS1PBDsmFp";
     // public static final String QUICKBOOKS_CONSUMER_SECRET =
-    // "o7hOCLRSBidVE9gkGQPBaFy0IIcyvtHuXPpL8kmw";
+    // "CmBH6OWTlDmemY3uHOBIjFE87MZ6O8FRcsLqtYZF";
+    // production key
+    public static final String QUICKBOOKS_APP_KEY = "08945c87bae1fb46bab9241ba0e81dc19bbf";
+    public static final String QUICKBOOKS_CONSUMER_KEY = "qyprdCLvyUgaMn6K9lrd3eyPDn1DuL";
+    public static final String QUICKBOOKS_CONSUMER_SECRET = "o7hOCLRSBidVE9gkGQPBaFy0IIcyvtHuXPpL8kmw";
 
     /** test facebook app details */
     // public static final String FACEBOOK_APP_ID = "1472962409608031";
