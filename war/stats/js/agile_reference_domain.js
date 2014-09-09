44918946ac8891aa661566278a5d770564461222
jagadeesh <jagadeesh@invox.com>
2014-09-09 14:07:47 +0530

    refereltracking files

---
+++ war/stats/js/agile_reference_domain.js
@@ -0,0 +1,24 @@
+/**
+ * agile_refernce_domain.js is used to read the reference domain from query
+ * string when user visited from referral link
+ */
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+
+if(getParameterByName('utm_source')=="affiliates"){
+	var reference_domain=getParameterByName("utm_campaign");
+	if(reference_domain){
+
+		agile_create_cookie("Agile_Reference_Domain", reference_domain, 90);
+		console.log("cookie created "+reference_domain);
+	}
+	else{
+		reference_domain="";
+	}
+}	
