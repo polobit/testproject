c3d8b291c2ffcadc8e3e83d03d1589d2c7c51a38
jagadeesh <jagadeesh@invox.com>
2014-09-19 16:16:00 +0530

    cookie condition changed

--- war/stats/js/agile_reference_domain.js
+++ war/stats/js/agile_reference_domain.js
@@ -3,8 +3,6 @@
  * string when user visited from referral link
  */
 
-
-
 function getParameterByName(name)
 {
 	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
@@ -14,14 +12,19 @@ function getParameterByName(name)
 
 function agile_create_reference_domain_cookie()
 {
-	var _utm_source=getParameterByName('utm_source');
-	var _utm_medium=getParameterByName('utm_medium');
-	var _utm_campaign=getParameterByName('utm_campaign');
-	
-	agile_createCookieInAllAgileSubdomains("_agile_utm_source", _utm_source, 90);
-	agile_createCookieInAllAgileSubdomains("_agile_utm_medium", _utm_medium, 90);
-	agile_createCookieInAllAgileSubdomains("_agile_utm_campaign", _utm_campaign, 90);
-	
+	var _utm_source = getParameterByName('utm_source');
+	var _utm_medium = getParameterByName('utm_medium');
+	var _utm_campaign = getParameterByName('utm_campaign');
+
+	console.log("_utm_source ::" + _utm_source + "_utm_medium::" + _utm_medium + "_utm_campaign::" + _utm_campaign);
+
+	if (_utm_source && _utm_medium && _utm_campaign)
+	{
+
+		agile_createCookieInAllAgileSubdomains("_agile_utm_source", _utm_source, 90);
+		agile_createCookieInAllAgileSubdomains("_agile_utm_medium", _utm_medium, 90);
+		agile_createCookieInAllAgileSubdomains("_agile_utm_campaign", _utm_campaign, 90);
+	}
 	if (_utm_source == "affiliates")
 	{
 		var reference_domain = _utm_campaign;
@@ -37,7 +40,3 @@ function agile_create_reference_domain_cookie()
 		}
 	}
 }
-
-
-
-
