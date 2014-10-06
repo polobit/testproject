f31ba5034cfff45e5409d80f3cc1ec91302945a5
yaswanthagile <praveen@invox.com>
2014-10-06 16:53:38 +0530

    Added nagger

--- war/jscore/noty.js
+++ war/jscore/noty.js
@@ -1,77 +1,18 @@
 /** The function is commented inorder to implement later. It shows Upgrade message to free users**/
-//$(function(){
-//
-//	var accountprefs = Backbone.Model.extend({
-//		url:'core/api/account-prefs'
-//	});
-//	
-//	var accountPrefs = new accountprefs();
-//	accountPrefs.fetch({
-//		success: function(data)
-//		{
-//			var json = data.toJSON();
-//			
-//			console.log(json);
-//			
-//			// Allow only for free users
-//			try{
-//				
-//				// if json consists of plan, then return
-//				if(json.plan)
-//					return;	
-//				
-//			}
-//			catch(err){}	
-//	
-//	// Show the first one after 3 secs
-//	setTimeout(function(){
-//		showNoty("warning", get_random_message(), "bottom");
-//	}, 3000);
-//	
-////	// Set the periodically
-////	setInterval(function(){
-////		showNoty("warning", get_random_message(), "bottom");
-////	}, 30000);
-//	
-//		}
-//	});
-//	
-//});
 
-//function showNotyP(type, message, position)
-//{
-//	// Download the lib
-//	head.js(LIB_PATH + 'lib/noty/jquery.noty.js', LIB_PATH + 'lib/noty/layouts/bottom.js', LIB_PATH + 'lib/noty/layouts/top.js', LIB_PATH + 'lib/noty/themes/default.js',
-//       		function()
-//       		{
-//		
-//				// Close all
-//				$.noty.closeAll()
-//		
-//				var n = noty({
-//		    			 text: message,
-//		    			 layout: position,
-//		    			 type: type
-//		    	});
-//				
-//			        // Set the handler for click
-//				     $('.noty_bar').die().live('click', function(){
-//					
-//					// Close all
-//					$.noty.closeAll();
-//					
-//					if(n.options.type == "warning"){
-//					
-//						// Send to upgrade page
-//					Backbone.history.navigate('subscribe', {
-//						trigger : true
-//					});
-//					
-//					}
-//				});
-//	   });
-//	 
-//}
+function showUpgradeNoty()
+{
+
+	if(_billing_restriction.currentLimits.freePlan)
+		return;
+	
+		// Show the first one after 3 secs
+		showNoty("warning", get_random_message(), "bottom", 3000, function(){
+			Backbone.history.navigate('subscribe', {
+				 trigger : true
+				 });
+		});
+}
 
 var CONTACTS_HARD_RELOAD = false;
 
@@ -174,12 +115,12 @@ function notySetup(type, message, position, noty_timeout) {
 		});
 	}
 
-/*function get_random_message() {
+function get_random_message() {
 
-	var messages = [ "Thanks for trying Agile CRM.", "You can upgrade here." ];
+	var messages = ["Thanks for trying Agile CRM. You can upgrade here."];
 
 	var random = Math.floor((Math.random() * messages.length));
 	// console.log(random + messages[random]);
 
 	return messages[random];
-}*/
+}
