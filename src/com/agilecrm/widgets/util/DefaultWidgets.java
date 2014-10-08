440b20a909c5e369c9a57db0f9e323785e14d444
jitendra <jeetu345@gmail.com>
2014-10-08 14:25:37 +0530

    Fixed shopify widget small logo Conflicts: 	src/com/agilecrm/widgets/util/DefaultWidgets.java 	war/stats/min/agile-min.js

--- src/com/agilecrm/widgets/util/DefaultWidgets.java
+++ src/com/agilecrm/widgets/util/DefaultWidgets.java
@@ -77,26 +77,20 @@ public class DefaultWidgets
 	widgets.add(new Widget(
 		"Xero",
 		"Xero is an online accounting software for small business - Explore invoicing, reconciliation anytime, anywhere.",
-		"/widgets/xero.js", "/widgets/xero.png", "/widgets/xero24x24.png", null, WidgetType.BILLING)); 
+		"/widgets/xero.js", "/widgets/xero.png", "/widgets/xero24x24.png", null, WidgetType.BILLING));
 	widgets.add(new Widget(
 		"QuickBooks",
 		"Run your whole business better with QuickBooks wherever you are. Automate your invoicing, billing and reporting and simplify your business for life!",
 		"/widgets/quickBooks.js", "/widgets/intuit-quickbooks.png", "/widgets/quickbooks_small.jpg", null,
 		WidgetType.BILLING));
 	widgets.add(new Widget(
+
 		"Facebook",
 		"Facebook is a social utility that connects people with friends and others who work, study and live around them",
-		"/widgets/facebook.js", "/widgets/facebook.png", "/widgets/Fb24x24icon.png", null,
-		WidgetType.SOCIAL));
-	/**widgets.add(new Widget(
-		"Shopify",
+		"/widgets/facebook.js", "/widgets/facebook.png", "/widgets/Fb24x24icon.png", null, WidgetType.SOCIAL));
+	widgets.add(new Widget("Shopify",
 		"Shopify is a powerful ecommerce solution that includes everything you need to create an online store",
-<<<<<<< HEAD
-		"/widgets/shopify.js", "/widgets/shopify.png","/widgets/shopify-small.png", null,
-=======
-		"/widgets/shopify.js", "/widgets/shopify.png","/widgets/shopify-small.jpg", null,
->>>>>>> 036434a4df85a0fbb506fda4d249966f02978f88
-		WidgetType.ECOMMERCE));*/
+		"/widgets/shopify.js", "/widgets/shopify.png", "/widgets/shopifysm.png", null, WidgetType.ECOMMERCE));
 
 	System.out.println("Default widgets ");
 	System.out.println(widgets);
