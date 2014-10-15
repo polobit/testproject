979975d9761444d0688365e535b8c6e8e53cb097
karthikagile <karthik@invox.com>
2014-10-15 14:49:18 +0530

    check if shop is already linked with domain

--- src/com/thirdparty/shopify/ShopifyAppServlet.java
+++ src/com/thirdparty/shopify/ShopifyAppServlet.java
@@ -1,6 +1,7 @@
 package com.thirdparty.shopify;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,18 +18,35 @@ public class ShopifyAppServlet extends HttpServlet
     public void service(HttpServletRequest request, HttpServletResponse response) throws IOException
     {
 	String shopUrl = request.getParameter("shop");
+	String domainFromURL = request.getParameter("agile_domain");
 	String agileDomain = ShopifyAppUtil.getDomainByShop(shopUrl);
 	String redirectURL;
 
-	if (StringUtils.isBlank(agileDomain))
-	    redirectURL = new String("https://widgets.agilecrm.com/shopify/index.php?" + request.getQueryString());
+	String fromIndex = request.getParameter("i");
+	if (StringUtils.equals(fromIndex, "yes"))
+	{
+	    String shopFromDb = ShopifyAppUtil.getShopFromDomain(domainFromURL);
+	    if(StringUtils.isBlank(shopFromDb))
+	    {
+		redirectURL = new String("https://widgets.agilecrm.com/shopify/index.php?" + request.getQueryString());
+	    }
+	    else
+	    {
+		redirectURL = new String("https://widgets.agilecrm.com/shopify/index.php?" + request.getQueryString() + "&error=" + URLEncoder.encode("true", "UTF-8"));
+	    }
+	}
 	else
 	{
-	    NamespaceManager.set(agileDomain);
-	    redirectURL = new String("https://" + agileDomain + ".agilecrm.com/#shopify/" + shopUrl);
+	    if (StringUtils.isBlank(agileDomain))
+		redirectURL = new String("https://widgets.agilecrm.com/shopify/index.php?" + request.getQueryString());
+	    else
+	    {
+		NamespaceManager.set(agileDomain);
+		redirectURL = new String("https://" + agileDomain + ".agilecrm.com/#shopify/" + shopUrl);
+	    }
 	}
 	response.setContentType("text/html");
-	response.setStatus(response.SC_MOVED_TEMPORARILY);
+	response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
 	response.sendRedirect(redirectURL);
 	return;
     }
