7b2bd43630e66d44b5f021de9bba91e54a21172e
yaswanthagile <praveen@invox.com>
2014-11-05 14:58:59 +0530

    Cron for sync framework

--- src/com/thirdparty/sync/ContactSyncDeferredTask.java
+++ src/com/thirdparty/sync/ContactSyncDeferredTask.java
@@ -1,6 +1,5 @@
 package com.thirdparty.sync;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
@@ -10,6 +9,7 @@ import com.google.appengine.api.NamespaceManager;
 import com.google.appengine.api.taskqueue.DeferredTask;
 import com.thirdparty.google.ContactPrefs;
 import com.thirdparty.google.ContactsImportUtil;
+import com.thirdparty.google.utl.ContactPrefsUtil;
 
 /**
  * Deferred task to get all prefs based on frequency and initialize sync on it
@@ -43,7 +43,7 @@ public class ContactSyncDeferredTask implements DeferredTask
 	{
 	    NamespaceManager.set(namespace);
 
-	    List<ContactPrefs> prefs = new ArrayList<ContactPrefs>();
+	    List<ContactPrefs> prefs = ContactPrefsUtil.getprefs(frequency);
 	    if (prefs == null || prefs.size() == 0)
 		return;
 
