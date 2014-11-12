2bb866e7631a0f90eccc00670810260a02aa8ef3
nareshagile <naresh@faxdesk.com>
2014-11-12 12:19:18 +0530

    Fixed Reply bug in contact mails tab.

--- war/jscore/contact-management/contact-details-tabs.js
+++ war/jscore/contact-management/contact-details-tabs.js
@@ -407,30 +407,48 @@ $(function(){
 		var $parent_element = $(this).parent().parent();
 		
 		var to_emails = $parent_element.find('.to-emails').data('to');
+		var cc_emails = $parent_element.find('.cc-emails').data('cc');
+		var bcc_emails = $parent_element.find('.bcc-emails').data('bcc');
 		
-		if(to_emails){
-			
-			var to_array = to_emails.split(',');
+		var email_sync_configured = contact_details_tab.email_sync_configured;
+		var configured_email;
 		
-			to_emails = "";
+		if(email_sync_configured)
+		{
+			if(email_sync_configured["type"])
+				configured_email = email_sync_configured["email"];
+			else
+				configured_email= email_sync_configured["user_name"];
+		}
+		
+		if(configured_email && to_emails){
 			
-			for(var i=0, len = to_array.length; i < len; i++)
-			{
+			to_emails = get_emails_to_reply(to_emails, from, configured_email);
+		}
+		
+		if(configured_email && cc_emails){
+			
+			cc_emails = get_emails_to_reply(cc_emails, from, configured_email);
+		}
 
-				to_emails += to_array[i].match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi)[0];
-				
-				// Append comma without trailing
-				if(i < len-1)
-					to_emails += ', ';
-			
-			}
+		if(configured_email && bcc_emails){
+	
+			bcc_emails = get_emails_to_reply(bcc_emails, from, configured_email);
 		}
 		
 		// Change url only without triggerring function
 		App_Contacts.navigate('send-email');
 		
+		var reply_email = from;
+		
+		if(email_sync_configured)
+			reply_email = from + ', ' + to_emails;
+		
+		// Removes leading and trailing commas
+		reply_email = reply_email.replace(/(, $)/g, "");
+		
 		// Trigger route callback
-		App_Contacts.sendEmail(to_emails, "Re: " + $parent_element.find('.email-subject').text(), '<p></p><blockquote style="margin:0 0 0 .8ex;border-left:1px #ccc solid;padding-left:1ex;">'+ $parent_element.find('.email-body').html()+'</blockquote>');
+		App_Contacts.sendEmail(reply_email, "Re: " + $parent_element.find('.email-subject').text(), '<p></p><blockquote style="margin:0 0 0 .8ex;border-left:1px #ccc solid;padding-left:1ex;">'+ $parent_element.find('.email-body').html()+'</blockquote>', cc_emails, bcc_emails);
 		
 	});
 	
@@ -647,5 +665,30 @@ function load_contact_tab(el, contactJSON)
 		contact_details_tab["load_" + position]();
 	}
 		
+}
+
+function get_emails_to_reply(emails, from, configured_email)
+{
+	var emails_array = emails.split(',');
+	
+	emails = "";
+	
+	for(var i=0, len = emails_array.length; i < len; i++)
+	{
+
+		var email = emails_array[i].match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi)[0];
+		
+		if(configured_email && (email == configured_email || email == from))
+			continue;
+		
+		emails += email;
+		
+		// Append comma without trailing
+		if(i < len-1)
+			emails += ', ';
+	
+	}
+	
+	return emails;
 }
 
