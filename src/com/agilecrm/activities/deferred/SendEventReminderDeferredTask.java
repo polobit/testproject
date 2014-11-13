bd5d2dad39f3816975fc4713fcaed3cccf43f648
jagadeesh <jagadeesh@invox.com>
2014-11-13 20:34:13 +0530

    changed subject line

--- src/com/agilecrm/activities/deferred/SendEventReminderDeferredTask.java
+++ src/com/agilecrm/activities/deferred/SendEventReminderDeferredTask.java
@@ -155,7 +155,7 @@ public class SendEventReminderDeferredTask implements DeferredTask
 		    map.put("events", eventListMap);
 
 		    // Sends mail to the domain user.
-		    SendMail.sendMail(domainuser.email, SendMail.START_EVENT_REMINDER_SUBJECT,
+		    SendMail.sendMail(domainuser.email, "Event Reminder :" + event.title + "@" + event.date,
 			    SendMail.START_EVENT_REMINDER, map);
 		}
 	    }
