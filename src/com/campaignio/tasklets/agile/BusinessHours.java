7de8f41f8bdd692a384e8fdb36c36c854008d088
Bhasuri <bhasuri@invox.com>
2014-11-28 08:23:14 +0530

    Business Node - commit 2
    Removed sysouts and made few changes for date type

--- src/com/campaignio/tasklets/agile/BusinessHours.java
+++ src/com/campaignio/tasklets/agile/BusinessHours.java
@@ -1,24 +1,20 @@
 package com.campaignio.tasklets.agile;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+import java.util.Vector;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.campaignio.cron.Cron;
-import com.campaignio.cron.util.CronUtil;
-import com.campaignio.logger.Log.LogType;
-import com.campaignio.logger.util.LogUtil;
 import com.campaignio.tasklets.TaskletAdapter;
-import com.campaignio.tasklets.agile.util.AgileTaskletUtil;
 import com.campaignio.tasklets.util.TaskletUtil;
 
 /**
- * <code>Wait</code> class represents wait node in workflow. Wait is to set
- * duration period in the workflow. It consists of duration period and duration
- * type. Wait class uses {@link Cron} to set for that duration period.
+ * <code>Time</code> class looks at the current timing rules and looks at the
+ * values given out in the grid and gives Yes or No.
  * 
  * @author Manohar
  * 
@@ -28,12 +24,12 @@ public class BusinessHours extends TaskletAdapter
 	/**
 	 * Duration period
 	 */
-	public static String DURATION = "duration";
+	public static String TIME_GRID = "timegrid";
 
 	/**
 	 * Duration type such as Days,Hours and Minutes
 	 */
-	public static String DURATION_TYPE = "duration_type";
+	public static String TIME_ZONE = "timezone";
 
 	/*
 	 * (non-Javadoc)
@@ -44,65 +40,219 @@ public class BusinessHours extends TaskletAdapter
 	public void run(JSONObject campaignJSON, JSONObject subscriberJSON, JSONObject data, JSONObject nodeJSON)
 			throws Exception
 	{
-		// Get Duration, Type
-		String duration = getStringValue(nodeJSON, subscriberJSON, data, "timegrid");
+		String timeGrid = getStringValue(nodeJSON, subscriberJSON, data, TIME_GRID);
 
-		JSONArray arrayDuration = new JSONArray(duration);
-		List<String> campaignIDs = new ArrayList<String>();
-		JSONArray jsonArray;
+		String zoneValueInJSON = getStringValue(nodeJSON, subscriberJSON, data, TIME_ZONE);
+
+		boolean isMatched = false;
+
+		isMatched = inBusinessHour(timeGrid, zoneValueInJSON);
+
+		if (isMatched)
+			TaskletUtil.executeTasklet(campaignJSON, subscriberJSON, data, nodeJSON, "YES");
+		else
+			TaskletUtil.executeTasklet(campaignJSON, subscriberJSON, data, nodeJSON, "NO");
+
+	}
+
+	private boolean inBusinessHour(String timeGrid, String zoneValueInJSON)
+	{
+
 		try
 		{
-			jsonArray = nodeJSON.getJSONArray(JSON_VALUES);
-			int jsonArrayLength = jsonArray.length();
+			JSONArray arrayDuration = new JSONArray(timeGrid);
+			int jsonArrayLength = arrayDuration.length();
 
-			// Iterate through name/value pairs. First one is the select option
-			// ID and multi select ID
-
-			for (int i = 1; i < jsonArrayLength; i++)
+			for (int i = 0; i < jsonArrayLength; i++)
 			{
-				// Get the each JSON data
 
-				JSONObject json = jsonArray.getJSONObject(i);
+				JSONObject json = arrayDuration.getJSONObject(i);
+				Vector dayOfWeek = new Vector();
 
-				// For Serialized data from ui - you will get name, value
-				// pairs
-				if (json.has("name"))
-					campaignIDs.add((String) json.get("value"));
+				/*
+				 * "Mon-Fri": "Mon-Fri", "Mon-Sat": "Mon-Sat", "Mon-Sun":
+				 * "Mon-Sun", "Sat-Sun": "Sat-Sun", "Mon": "Mon", "Tue": "Tue",
+				 * "Wed": "Wed", "Thu": "Thu", "Fri": "Fri", "Sat": "Sat",
+				 * "Sun": "Sun"
+				 */
 
+				/*
+				 * { "Day": "Mon-Fri", "From": "9:00 AM", "To": "6:00 PM" }, {
+				 */
+
+				// Check day
+				String uiDay = json.getString("Day");
+				if (uiDay.equalsIgnoreCase("Mon-Fri"))
+				{
+					dayOfWeek.add(Calendar.MONDAY);
+					dayOfWeek.add(Calendar.TUESDAY);
+					dayOfWeek.add(Calendar.WEDNESDAY);
+					dayOfWeek.add(Calendar.THURSDAY);
+					dayOfWeek.add(Calendar.FRIDAY);
+				}
+
+				if (uiDay.equalsIgnoreCase("Mon-Sat"))
+				{
+					dayOfWeek.add(Calendar.MONDAY);
+					dayOfWeek.add(Calendar.TUESDAY);
+					dayOfWeek.add(Calendar.WEDNESDAY);
+					dayOfWeek.add(Calendar.THURSDAY);
+					dayOfWeek.add(Calendar.FRIDAY);
+					dayOfWeek.add(Calendar.SATURDAY);
+				}
+				if (uiDay.equalsIgnoreCase("Mon-Sun"))
+				{
+					dayOfWeek.add(Calendar.MONDAY);
+					dayOfWeek.add(Calendar.TUESDAY);
+					dayOfWeek.add(Calendar.WEDNESDAY);
+					dayOfWeek.add(Calendar.THURSDAY);
+					dayOfWeek.add(Calendar.FRIDAY);
+					dayOfWeek.add(Calendar.SATURDAY);
+					dayOfWeek.add(Calendar.SUNDAY);
+				}
+
+				if (uiDay.equalsIgnoreCase("Sat-Sun"))
+				{
+					dayOfWeek.add(Calendar.SATURDAY);
+					dayOfWeek.add(Calendar.SUNDAY);
+				}
+
+				if (uiDay.equalsIgnoreCase("Mon"))
+					dayOfWeek.add(Calendar.MONDAY);
+
+				if (uiDay.equalsIgnoreCase("Tue"))
+					dayOfWeek.add(Calendar.TUESDAY);
+
+				if (uiDay.equalsIgnoreCase("Wed"))
+					dayOfWeek.add(Calendar.WEDNESDAY);
+
+				if (uiDay.equalsIgnoreCase("Thu"))
+					dayOfWeek.add(Calendar.THURSDAY);
+
+				if (uiDay.equalsIgnoreCase("Fri"))
+					dayOfWeek.add(Calendar.FRIDAY);
+
+				if (uiDay.equalsIgnoreCase("Sat"))
+					dayOfWeek.add(Calendar.SATURDAY);
+
+				if (uiDay.equalsIgnoreCase("Sun"))
+					dayOfWeek.add(Calendar.SUNDAY);
+
+				// Get the current time in Hong Kong
+				/*
+				 * { "name": "timezone", "value":
+				 * "(GMT-12:00 ) International Date Line West" }
+				 */
+
+				// Start time
+				// "From": "9:00 AM",
+				// "To": "6:00 PM"
+				String startTimeUI = json.getString("From");
+				String startTimeHour = startTimeUI.split(":")[0];
+				String startTimeMins = startTimeUI.split(":")[1].split(" ")[0];
+				String startTimeAM = startTimeUI.split(":")[1].split(" ")[1];
+
+				if (startTimeAM.equalsIgnoreCase("PM"))
+				{
+					int hour = Integer.parseInt(startTimeHour);
+
+					if (hour != 12)
+						startTimeHour = "" + (hour + 12);
+
+				}
+
+				if (startTimeAM.equalsIgnoreCase("AM"))
+				{
+					int hour = Integer.parseInt(startTimeHour);
+
+					if (hour == 12)
+						startTimeHour = "00";
+
+				}
+
+				// Add 0
+				if (startTimeHour.length() <= 1)
+					startTimeHour = "0" + startTimeHour;
+
+				String startTime = startTimeHour + startTimeMins;
+
+				// End time
+				String endTimeUI = json.getString("To");
+				String endTimeHour = endTimeUI.split(":")[0];
+				String endTimeMins = endTimeUI.split(":")[1].split(" ")[0];
+				String endTimeAM = endTimeUI.split(":")[1].split(" ")[1];
+
+				if (endTimeAM.equalsIgnoreCase("AM"))
+				{
+					int hour = Integer.parseInt(endTimeHour);
+
+					if (hour == 12)
+						endTimeHour = "00";
+
+				}
+
+				if (endTimeAM.equalsIgnoreCase("PM"))
+				{
+					int hour = Integer.parseInt(endTimeHour);
+					if (hour != 12)
+						endTimeHour = "" + (hour + 12);
+				}
+				if (endTimeHour.length() <= 1)
+					endTimeHour = "0" + endTimeHour;
+
+				String endTime = endTimeHour + endTimeMins;
+
+				// Zone
+				String timeZone = zoneValueInJSON.substring(zoneValueInJSON.indexOf("GMT"));
+
+				// Get in quotes (GMT-12:00)
+				String tz = timeZone.substring(0, timeZone.indexOf(")"));
+
+				Calendar calendar = new GregorianCalendar(TimeZone.getTimeZone(tz.trim()));
+				// Calendar calendar = new
+				// GregorianCalendar(TimeZone.getTimeZone(gmt));
+
+				// if(!dayOfWeek.contains(calendar.get(Calendar.DAY_OF_WEEK)))
+				// continue;
+
+				// Check time
+				String currentTime = "" + calendar.get(Calendar.HOUR_OF_DAY) + "" + calendar.get(Calendar.MINUTE);
+				if (currentTime.length() < 4)
+				{
+					currentTime = "0" + currentTime;
+				}
+
+				/*
+				 * If the start time time and time are same then it means that
+				 * the working hours are 24hrs so all the 24 hrs are working
+				 * hours. added by Jaffar on 24 Sep 2010
+				 */
+
+				if (startTime.compareTo(endTime) == 0)
+					return true;
+
+				/*
+				 * when the end time is night 12 O clock then this block will be
+				 * executed added by Jaffar on 1st oct 2010
+				 */
+				if ((endTime.trim()).equalsIgnoreCase("0000"))
+				{
+					endTime = "2400";
+					if (currentTime.compareTo(startTime) > 0 && currentTime.compareTo(endTime) < 0)
+						return true;
+				}
+				if (currentTime.compareTo(startTime) > 0 && currentTime.compareTo(endTime) < 0)
+					return true;
 			}
 		}
 		catch (JSONException e)
 		{
 			// TODO Auto-generated catch block
+			System.err.println("Inside isBusinessHour node method");
 			e.printStackTrace();
+			return false;
 		}
-
-		String durationType = getStringValue(nodeJSON, subscriberJSON, data, DURATION_TYPE);
-
-		System.out.println("Waiting for " + duration + " " + durationType);
-
-		// Creates log for sending email
-		LogUtil.addLogToSQL(AgileTaskletUtil.getId(campaignJSON), AgileTaskletUtil.getId(subscriberJSON),
-				"Wait duration - " + duration + " " + durationType, LogType.WAIT.toString());
-
-		// Add ourselves to Cron Queue
-		long timeout = CronUtil.getTimer(duration, durationType);
-		CronUtil.enqueueTask(campaignJSON, subscriberJSON, data, nodeJSON, timeout, null, null, null);
+		return false;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.campaignio.tasklets.TaskletAdapter#timeOutComplete(org.json.JSONObject
-	 * , org.json.JSONObject, org.json.JSONObject, org.json.JSONObject)
-	 */
-	public void timeOutComplete(JSONObject campaignJSON, JSONObject subscriberJSON, JSONObject data, JSONObject nodeJSON)
-			throws Exception
-	{
-		System.out.println("Wake up from wait. Executing next one.");
-
-		// Execute Next One in Loop
-		TaskletUtil.executeTasklet(campaignJSON, subscriberJSON, data, nodeJSON, null);
-	}
 }
