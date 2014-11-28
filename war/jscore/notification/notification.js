f81109a79d58ce731a699949173b787543d0b0de
jagadeesh <jagadeesh@invox.com>
2014-11-28 12:21:06 +0530

    added return statement

--- war/jscore/notification/notification.js
+++ war/jscore/notification/notification.js
@@ -418,10 +418,11 @@ function showNoty(type, message, position, notification_type, onCloseCallback)
 			return;
 		}
 		
-		if (notification_type != "CALENDER_REMINDER")
+		if (notification_type != "CALENDER_REMINDER"){
 		show_desktop_notification(getImageUrl(message,notification_type), getNotificationType(notification_type), getTextMessage(message), getId(message), getId(message).split(
 				'/')[1] + '-' + notification_type);
 		return;
+		}
 	}
 
 	// Download the lib
