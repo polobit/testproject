181acabfbdcdb5085ca4e985cc3a7030e94ff74c
Bhasuri <bhasuri@invox.com>
2014-12-02 21:41:19 +0530

    Ui fix - singlee quotes message

--- war/json/nodes/common/Unsubscribe.js
+++ war/json/nodes/common/Unsubscribe.js
@@ -2,7 +2,7 @@
     "name": "Unsubscribe",
     "thumbnail": "json/nodes/images/common/unsubscribe.png",
     "icon": "json/nodes/icons/common/unsubscribe.png",
-    "info": "Unsubscribe the contact from selected campaign",
+    "info": "Unsubscribe the contact from selected campaign.",
     "help": "Unsubscribe the contact from selected campaigns.You can unsubscribe the contact from either a specific campaign or all the campaigns the contact is currently in.",
     "author": "Bhasuri",
     "company": "Agile",
@@ -24,7 +24,8 @@
             "style":
             	{
             	"margin-top": "10px",
-            	"height": "100px"
+            	"height": "100px",
+            	"width":"50%"
             	}
         },
         {
