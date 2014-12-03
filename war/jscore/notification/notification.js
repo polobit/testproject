a683f3edd5157ed023f8775ded6963cea68ca387
jagadeesh <jagadeesh@invox.com>
2014-12-03 17:27:23 +0530

    removed desktop notification

--- war/jscore/notification/notification.js
+++ war/jscore/notification/notification.js
@@ -422,9 +422,11 @@ function showNoty(type, message, position, notification_type, onCloseCallback)
 			return;
 		}
 		
-		show_desktop_notification(getImageUrl(message,notification_type), getNotificationType(notification_type), getTextMessage(message), getId(message), getId(message).split(
-				'/')[1] + '-' + notification_type);
-		return;
+		if(notification_type!="CALENDER_REMINDER"){
+			show_desktop_notification(getImageUrl(message,notification_type), getNotificationType(notification_type), getTextMessage(message), getId(message), getId(message).split(
+					'/')[1] + '-' + notification_type);
+			return;
+			}
 		
 	}
 
@@ -559,8 +561,6 @@ function getImageUrl(message, notification_type)
 		
 		return $('span:eq(0)', message).attr('id');
 		}
-	if(notification_type=="CALENDER_REMINDER"){
-		return '/img/phone16.png';
 	}
 
 	return '/img/deal.png';
