16d0afc4ae7a248896c8d691f6daa162a3da9c9f
karthikagile <karthik@invox.com>
2014-12-16 12:51:37 +0530

    add util file

---
+++ src/com/agilecrm/forms/util/FormUtil.java
@@ -0,0 +1,28 @@
+package com.agilecrm.forms.util;
+
+import java.util.List;
+
+import com.agilecrm.db.ObjectifyGenericDao;
+import com.agilecrm.forms.Form;
+import com.googlecode.objectify.Query;
+
+public class FormUtil
+{
+    private static ObjectifyGenericDao<Form> dao = new ObjectifyGenericDao<Form>(Form.class);
+
+    public static List<Form> getAllForms()
+    {
+	return Form.dao.fetchAll();
+    }
+
+    public static Form getFormById(Long formId)
+    {
+	Query<Form> query = dao.ofy().query(Form.class);
+	query.filter("id", formId);
+	Form form = query.get();
+	if (form == null)
+	    return null;
+	else
+	    return form;
+    }
+}
