93cbdf71e8c9bc77033a85ae5eb860714b8b4f05
Bhasuri <bhasuri@invox.com>
2015-01-05 20:32:49 +0530

    New image - Close task

--- war/json/nodes/crm/close_task.js
+++ war/json/nodes/crm/close_task.js
@@ -1,7 +1,7 @@
 {
     "name": "Close Task",
-    "thumbnail": "json/nodes/images/crm/closed_task.png",
-    "icon": "json/nodes/icons/crm/closed_task.png",
+    "thumbnail": "json/nodes/images/crm/close_task.png",
+    "icon": "json/nodes/icons/crm/close_task.png",
     "info": "Mark the tasks related to the contact as completed.",
     "help": "Mark the tasks related to the contact as completed. You can optionally choose to complete only a particular user’s tasks.",
     "author": "Bhasuri",
