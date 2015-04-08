e89852fd0f9bb80d1881c4b1991dc4227ba93d11
saikiran-k <saikiran@invox.com>
2015-04-08 15:32:36 +0530

    Revert "Paging for notes in the contact - bug"
    This reverts commit 6f76810475ed0a6c0eefec2b71c73ba51ec89a8f.

--- agile-core-classes/src/com/agilecrm/contact/Note.java
+++ agile-core-classes/src/com/agilecrm/contact/Note.java
@@ -1,14 +1,29 @@
 package com.agilecrm.contact;
 
-import src.com.agilecrm.cursor.Cursor;
-import src.com.agilecrm.db.ObjectifyGenericDao;
-import src.com.agilecrm.session.SessionManager;
-import src.com.agilecrm.session.UserInfo;
-import src.com.agilecrm.user.AgileUser;
-import src.com.agilecrm.user.DomainUser;
-import src.com.agilecrm.user.UserPrefs;
-import src.com.agilecrm.user.util.DomainUserUtil;
-import src.com.agilecrm.user.util.UserPrefsUtil;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+import com.agilecrm.db.ObjectifyGenericDao;
+import com.agilecrm.session.SessionManager;
+import com.agilecrm.session.UserInfo;
+import com.agilecrm.user.AgileUser;
+import com.agilecrm.user.DomainUser;
+import com.agilecrm.user.UserPrefs;
+import com.agilecrm.user.util.DomainUserUtil;
+import com.agilecrm.user.util.UserPrefsUtil;
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.annotation.Cached;
+import com.googlecode.objectify.annotation.NotSaved;
+import com.googlecode.objectify.condition.IfDefault;
 
 /**
  * <code>Note</code> class stores notes with their related contacts. A contact
@@ -24,7 +39,7 @@ import src.com.agilecrm.user.util.UserPrefsUtil;
  */
 @XmlRootElement
 @Cached
-public class Note extends Cursor
+public class Note
 {
     // Key
     @Id
