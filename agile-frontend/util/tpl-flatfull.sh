8a6fe614a92387a12d64bb8bdef964d449a9b076
yaswanthagile <praveen@invox.com>
2015-04-10 19:38:03 +0530

    Controller for flat full

---
+++ agile-frontend/util/tpl-flatfull.sh
@@ -0,0 +1,20 @@
+cd ../WebContent
+cat flatfull/tpl/*.html > flatfull/tpl/min/tpl.js
+cat flatfull/tpl/settings/*.html >  flatfull/tpl/min/settings.js
+cat flatfull/tpl/continue/*.html > flatfull/tpl/min/continue.js
+cat flatfull/tpl/bulk-actions/*.html > flatfull/tpl/min/bulk-actions.js
+cat flatfull/tpl/gmap/*.html > flatfull/tpl/min/gmap.js
+cat flatfull/tpl/report/*.html > flatfull/tpl/min/report.js
+cat flatfull/tpl/workflow/*.html > flatfull/tpl/min/workflow.js
+cat flatfull/tpl/billing/*.html > flatfull/tpl/min/billing.js
+cat flatfull/tpl/web-rules/*.html > flatfull/tpl/min/web-rules.js
+cat flatfull/tpl/widget/*.html > flatfull/tpl/min/widget.js
+cat flatfull/tpl/socialsuite/*.html > flatfull/tpl/min/socialsuite.js
+cat flatfull/tpl/admin-settings/*.html > flatfull/tpl/min/admin-settings.js
+cat flatfull/tpl/contact-detail/*.html > flatfull/tpl/min/contact-detail.js
+cat flatfull/tpl/contact-view/*.html > flatfull/tpl/min/contact-view.js
+cat flatfull/tpl/contact-filter/*.html > flatfull/tpl/min/contact-filter.js
+cat flatfull/tpl/case/*.html > flatfull/tpl/min/case.js
+cat flatfull/tpl/document/*.html > flatfull/tpl/min/document.js
+cat flatfull/tpl/portlets/*.html > flatfull/tpl/min/portlets.js
+cd ../util
