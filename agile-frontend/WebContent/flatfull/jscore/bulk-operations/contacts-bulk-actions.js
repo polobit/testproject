97609d16af1bde13f0becf59d5e85ee6ad7a81f5
Bhasuri Kona <bhasuri@invox.com>
2015-04-21 19:14:34 +0530

    New UI changes - Bulk operations

--- agile-frontend/WebContent/flatfull/jscore/bulk-operations/contacts-bulk-actions.js
+++ agile-frontend/WebContent/flatfull/jscore/bulk-operations/contacts-bulk-actions.js
@@ -885,7 +885,7 @@ $(function()
 
 						$('body')
 								.find('#bulk-select')
-								//.css('display', 'block')
+								.css('display', 'block')
 								.html(
 										"Selected " + App_Contacts.contactsListView.collection.length + " contacts. <a href='#'  id='select-all-available-contacts' >Select all " + getAvailableContacts() + " contacts</a>");
 					});
@@ -961,11 +961,14 @@ function toggle_contacts_bulk_actions_dropdown(clicked_ele, isBulk, isCampaign)
 		$('body').find('#bulk-actions').css('display', 'inline-block');
 
 		if (isBulk && total_available_contacts != App_Contacts.contactsListView.collection.length)
+			{
 			$('body')
 					.find('#bulk-select')
-					//.css('display', 'block')
+					.css('display', 'block')
 					.html(
 							"Selected " + App_Contacts.contactsListView.collection.length + " contacts. <a id='select-all-available-contacts' href='#'>Select all " + total_available_contacts + " contacts</a>");
+			 $('#bulk-select').css("display","inline");
+			}
 	}
 	else
 	{
