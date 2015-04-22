fa6e1b1078637d79d7c908cbce8051d699dfe889
saikiran-k <saikiran@invox.com>
2015-04-22 19:06:59 +0530

    Gmail gadget updates.

--- agile-frontend/WebContent/misc/gmail/gadget-js-all/js/email.js
+++ agile-frontend/WebContent/misc/gmail/gadget-js-all/js/email.js
@@ -11,7 +11,8 @@ function agile_get_emails()
 				{ name_from : "Devika Jakkannagari" },
 				{ email_to : "abhi@gashok.mygbiz.com;rahul@gashok.mygbiz.com;dheeraj@gashok.mygbiz.com;chandan@gashok.mygbiz.com;abhiranjan@gashok.mygbiz.com" },
 				{ name_to : "Abhi;;D j p;;" }, { email_cc : "devikatest1@gmail.com;devikatest@gmail.com;teju@gmail.com" }, { name_cc : "Dev T1;;Teju" },
-				{ email : "devikatest@gmail.com" }, { email : "test1@gmail.com" }, { email : "test1@gmail.com" }, { email : "pbx.kumar@gmail.com" }
+				{ email : "devikatest@gmail.com" }, { email : "test1@gmail.com" }, { email : "test1@gmail.com" }, { email : "pbx.kumar@gmail.com" },
+				{email_body : "Testing local emails body extractor."},{subject : "Testing local subject extractor."}
 		];
 
 		// emails = [{email:"devikatest@gmail.com"}];
@@ -38,6 +39,13 @@ function validateEmails(emails){
 function parse_emails(emails)
 {
 	return $.merge(collate_emails(emails, "from"), collate_emails(emails, "to")).concat(collate_emails(emails, "cc")).concat(agile_grep(emails, "email"));
+}
+
+function getSubBody(){
+	var tempMsg = {};
+	tempMsg.subject = agile_grep(agile_get_emails(),'subject');
+	tempMsg.body = agile_grep(agile_get_emails(),'email_body');
+	return tempMsg;
 }
 
 // Finds email_key and then finds name_key and collates them
--- agile-frontend/WebContent/misc/gmail/gadget-js-all/js/event-handler/agile-action-events.js
+++ agile-frontend/WebContent/misc/gmail/gadget-js-all/js/event-handler/agile-action-events.js
@@ -30,6 +30,27 @@ $(function()
 			agile_gadget_adjust_height();
 		});
 	});
+	
+	$('.action-add-email-note').die().live('click', function(e)
+			{
+				// ------ Prevent default functionality. ------
+				e.preventDefault();
+				var email = getSubBody();
+				// ------ Set context (HTML container where event is triggered). ------
+				var el = $(this).closest("div.gadget-contact-details-tab").find("div.show-form");
+				$('.gadget-notes-tab-list', el).hide();
+				// ------ Build notes tab UI to add note. ------
+				agile_build_form_template($(this), "gadget-note", ".gadget-notes-tab-list", function()
+				{
+					$('#subject',el).val(email.subject);
+					$('#subject',el).val(email.body);
+					// ------ Show notes tab. ------
+					$('.gadget-notes-tab a', el).tab('show');
+					$('.gadget-notes-tab-list', el).show();
+					// ------ Adjust gadget height. ------
+					agile_gadget_adjust_height();
+				});
+			});
 
 	// ------------------------------------------------- Click event for Action
 	// Menu (add task) -----------------------------------
@@ -44,16 +65,28 @@ $(function()
 		// ------ Build tasks tab UI to add task. ------
 		agile_build_form_template($(this), "gadget-task", ".gadget-tasks-tab-list", function()
 		{
-			/*
-			 * ------ Load and apply Bootstrap date picker on text box in Task
-			 * form. ------
-			 */
-			agile_load_datepicker($('.task-calender', el), function()
-			{
-				$('.gadget-tasks-tab a', el).tab('show');
-				$('.gadget-tasks-tab-list', el).show();
-				// ------ Adjust gadget height. ------
-				agile_gadget_adjust_height();
+			var agile_url = agile_id.getURL() + "/users?callback=?&id=" + agile_id.get();
+			agile_json(agile_url, function(usersList){
+				
+				if(usersList != null && usersList.length != 0){
+					var html = '';
+					$.each(usersList,function(index,value){
+						html += '<option value="'+value.id+'">'+value.name+'</option>';
+					});
+					//console.log(html);
+					$('#ownerId',el).html(html);
+				}
+				/*
+				 * ------ Load and apply Bootstrap date picker on text box in Task
+				 * form. ------
+				 */
+				agile_load_datepicker($('.task-calender', el), function()
+				{
+					$('.gadget-tasks-tab a', el).tab('show');
+					$('.gadget-tasks-tab-list', el).show();
+					// ------ Adjust gadget height. ------
+					agile_gadget_adjust_height();
+				});
 			});
 		});
 	});
@@ -69,6 +102,28 @@ $(function()
 		var el = $(this).closest("div.gadget-contact-details-tab").find("div.show-form");
 		var That = $(this);
 		$('.gadget-deals-tab-list', el).hide();
+		var domain = agile_id.getNamespace();
+		var owner_url = agile_id.getNamespace()+'';
+		
+		var agile_url = agile_id.getURL() + "/users?callback=?&id=" + agile_id.get();
+		agile_json(agile_url, function(usersList){
+			// ------ Take contact data from global object variable. ------
+			var Json = Contacts_Json[el.closest(".show-form").data("content")];
+			
+			// ------ Compile template and generate UI. ------
+			var Handlebars_Template = getTemplate("gadget-deal", Json, 'no');
+			
+			var elAll = $(Handlebars_Template);
+			
+			if(usersList != null && usersList.length != 0){
+				Json.ownersList = usersList;
+				var html = '';
+				$.each(usersList,function(index,value){
+					html += '<option value="'+value.id+'">'+value.name+'</option>';
+				});
+				//console.log(html);
+				$('#ownerId',elAll).html(html);
+			}
 		
 		// ------ Get campaign work-flow data. ------
 		_agile.get_pipelines({ success : function(Response)
@@ -77,14 +132,37 @@ $(function()
 			for ( var Loop in Milestone_Array)
 				Milestone_Array.splice(Loop, 1, Milestone_Array[Loop].trim());*/
 
-			// ------ Take contact data from global object variable. ------
-			var Json = Contacts_Json[el.closest(".show-form").data("content")];
 			Json.pipelines = Response;
+			console.log('-----------',Json);
+			html = '';
+			// If there is only one pipeline, select the option by default and hide the field.
+			if(Response.length==1){
+				var mile = Response[0];
+				$.each(mile.milestones.split(","), function(index,milestone){
+						html+='<option value="'+mile.id+'_'+milestone+'">'+milestone+'</option>';
+				});
+				$('#pipeline_milestone',elAll).closest('.control-group').find('label b').text('Milestone');
+			}
+			else {
+				$.each(Response,function(index,mile){
+					console.log(mile.milestones);
+					var array = [];
+					html+='<optgroup label="'+mile.name+'">';
+					$.each(mile.milestones.split(","), function(index,milestone){
+						array.push($.trim(this));
+							html+='<option value="'+mile.id+'_'+milestone+'">'+mile.name+' - '+milestone+'</option>';
+					});
+					html+='</optgroup>';
+					
+				});
+				$('#pipeline_milestone',elAll).closest('.control-group').find('label b').text('Track & Milestone');
+			}
+			$('#pipeline_milestone',elAll).html(html);
+			console.log('adding');
 
-			// ------ Compile template and generate UI. ------
-			var Handlebars_Template = getTemplate("gadget-deal", Json, 'no');
+			
 			// ------ Insert template to container in HTML. ------
-			That.closest(".gadget-contact-details-tab").find(".gadget-deals-tab-list").html($(Handlebars_Template));
+			That.closest(".gadget-contact-details-tab").find(".gadget-deals-tab-list").html(elAll);
 			$('.gadget-deals-tab a', el).tab('show');
 			$('.gadget-deals-tab-list', el).show();
 			/*
@@ -102,42 +180,26 @@ $(function()
 			agile_gadget_adjust_height();
 			
 			console.log(Response);
-			if(Response.length==1){
 				console.log('auto select track');
+				$('#milestone',el).val(Response[0].milestones.split(',')[0]);
 				$('#pipeline').val(Response[0].id);
-				$('#pipeline').trigger('change');
-				$('#pipeline').closest('.control-group').hide();
-			}
+				$('#pipeline_milestone').trigger('change');
 
 		}, error : function(Response)
 		{
 
 		} });
+	});
 
 	});
 	
-	$('#pipeline').die().live('change',function(e){
-		var pipeline_id = $(this).val();
-		console.log('-----',pipeline_id);
-		if(pipeline_id.length >0)
-		_agile.get_milestones_by_pipeline(pipeline_id,{ success : function(Response)
-			{
-				Milestone_Array = Response.milestones.split(",");
-				for ( var Loop in Milestone_Array)
-					Milestone_Array.splice(Loop, 1, Milestone_Array[Loop].trim());
-				
-				var html = '';
-				for(var mile in Milestone_Array)
-					html+='<option value="'+Milestone_Array[mile]+'">'+Milestone_Array[mile]+'</option>';
-				$('#milestone').html(html);
-
-			}, error : function(Response)
-			{
-				console.log('Error in getting milestones',Response);
-				$('#milestone').html("");
-			} });
-		else
-			$('#milestone').html("");
+	$('#pipeline_milestone').die().live('change',function(e){
+		var temp = $(this).val();
+		var track = temp.substring(0,temp.indexOf('_'));
+		var milestone = temp.substring(temp.indexOf('_')+1,temp.length+1);
+		$(this).closest('form').find('#pipeline').val(track);
+		$(this).closest('form').find('#milestone').val(milestone);
+		console.log(track,'-----------',milestone);
 	});
 
 	// ------------------------------------------------- Click event for Action
