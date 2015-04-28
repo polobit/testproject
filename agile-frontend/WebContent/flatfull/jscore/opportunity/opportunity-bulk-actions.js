f579e7bdeca19961e8383f868cd3259549f412c9
saikiran-k <saikiran@invox.com>
2015-04-28 14:51:13 +0530

    New UI bug fixes.

--- agile-frontend/WebContent/flatfull/jscore/opportunity/opportunity-bulk-actions.js
+++ agile-frontend/WebContent/flatfull/jscore/opportunity/opportunity-bulk-actions.js
@@ -43,8 +43,8 @@
 				callback(data);
 
 			// On save back to deals list
-			//Backbone.history.navigate("deals", { trigger : true });  
-			App_Deals.deals();
+			// Backbone.history.navigate("deals", { trigger : true });  
+			//App_Deals.deals();
 			
 			// If no_noty is given as error message, neglect noty
 			if(error_message === "no_noty")
@@ -169,7 +169,7 @@
 			bulkDeleteDeals();
 		});
 		
-		$("#deal_bulk_delete_modal,#deal_owner_change_modal,#deal_mile_change_modal,#deal_bulk_archive_modal,#deal_bulk_restore_modal").on('show',function(){
+		$("#deal_bulk_delete_modal,#deal_owner_change_modal,#deal_mile_change_modal,#deal_bulk_archive_modal,#deal_bulk_restore_modal").on('show.bs.modal',function(){
 			if(deal_bulk_actions.SELECT_ALL_DEALS)
 				$(this).find('span.count').text(numberWithCommas(getAvailableDeals()));
 			else
@@ -199,6 +199,11 @@
 			e.preventDefault();
 			deal_bulk_actions.SELECT_ALL_DEALS = false;
 			$('body').find('#bulk-select').html("Selected " + numberWithCommas(App_Deals.opportunityCollectionView.collection.length) + " deals. <a id='select-all-available-deals' href='#'>Select all " + numberWithCommas(getAvailableDeals()) + " deals</a>");
+		});
+		
+		$("#deal_bulk_delete_modal,#deal_owner_change_modal,#deal_mile_change_modal,#deal_bulk_archive_modal,#deal_bulk_restore_modal").on('hidden.bs.modal', function (e) {
+			  // do something...
+			App_Deals.deals();
 		});
 		
 		
