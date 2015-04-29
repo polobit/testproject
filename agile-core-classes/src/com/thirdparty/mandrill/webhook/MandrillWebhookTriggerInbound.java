f46ec5ca7b86255c056c8c42ec0f7ad65a940f4f
karthikagile <karthik@invox.com>
2015-04-29 17:36:12 +0530

    handle save exception

--- agile-core-classes/src/com/thirdparty/mandrill/webhook/MandrillWebhookTriggerInbound.java
+++ agile-core-classes/src/com/thirdparty/mandrill/webhook/MandrillWebhookTriggerInbound.java
@@ -29,278 +29,283 @@ import com.googlecode.objectify.Key;
 @SuppressWarnings("serial")
 public class MandrillWebhookTriggerInbound extends HttpServlet
 {
-	public enum AgileDetail
+    public enum AgileDetail
+    {
+	API_KEY, DOMAIN;
+    }
+
+    public static final String INBOUND_MAIL_EVENT = "INBOUND_MAIL_EVENT";
+
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+	String oldNamespace = NamespaceManager.get();
+	try
 	{
-		API_KEY, DOMAIN;
-	}
-
-	public static final String INBOUND_MAIL_EVENT = "INBOUND_MAIL_EVENT";
-
-	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException
-	{
-		String oldNamespace = NamespaceManager.get();
-		try
-		{
-			String mandrillEvents = request.getParameter("mandrill_events");
-			// System.out.println("mandrill events parameter is " +
-			// mandrillEvents);
-			if (StringUtils.isBlank(mandrillEvents))
-			{
-				// response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-				return;
-			}
-
-			JSONArray events = new JSONArray(mandrillEvents);
-			for (int i = 0; i < events.length(); i++)
-			{
-				JSONObject event = events.getJSONObject(i);
-				// System.out.println("event json is " + event);
-
-				if (event.has("event") && StringUtils.equals(event.getString("event"), "inbound"))
-				{
-					JSONObject message = event.getJSONObject("msg");
-					// System.out.println("email message is " + message);
-					if (message == null)
-					{
-						// response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-						continue;
-					}
-					String agileEmail = getAgileEmail(message);
-					String apiKey = getAgileDetail(agileEmail, AgileDetail.API_KEY);
-					String agileDomain = getAgileDetail(agileEmail, AgileDetail.DOMAIN);
-
-					System.out.println("agile api key is " + apiKey);
-					System.out.println("agile special email is " + agileEmail);
-					System.out.println("agile domain is " + agileDomain);
-					if (agileEmail == null || agileDomain == null || apiKey == null)
-					{
-						// response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-						continue;
-					}
-
-					NamespaceManager.set(agileDomain);
-
-					Key<DomainUser> owner = APIKey.getDomainUserKeyRelatedToAPIKey(apiKey);
-					System.out.println("owner is " + owner);
-					if (owner == null)
-					{
-						// response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-						continue;
-					}
-					System.out.println("CURRENT namespace is " + NamespaceManager.get());
-
-					if (isConfirmationEmail(message))
-					{
-						// response.setStatus(HttpServletRequest.SC_BAD_REQUEST);
-						sendConfirmationEmail(apiKey, message);
-						continue;
-					}
-
-					String fromEmail = message.getString("from_email");
-
-					String fromName = null;
-					if (message.has("from_name"))
-						fromName = message.getString("from_name");
-
-					System.out.println("from email is " + fromEmail);
-					System.out.println("from name is " + fromName);
-
-					Boolean newContact = isNewContact(fromEmail);
-					Contact contact = buildContact(fromName, fromEmail, newContact);
-
-					if (contact == null)
-					{
-						// response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-						continue;
-					}
-
-					System.out.println("saving contact");
-					contact.setContactOwner(owner);
-
-					try
-					{
-					    contact.save();
-					}
-					catch(PlanRestrictedException e)
-					{
-					    continue;
-					}
-					List<Trigger> triggers = TriggerUtil
-							.getTriggersByCondition(com.agilecrm.workflows.triggers.Trigger.Type.INBOUND_MAIL_EVENT);
-					for (Trigger trigger : triggers)
-					{
-						System.out.println("trigger id is " + trigger.id);
-						if (getTriggerRunResult(newContact, trigger))
-						{
-							System.out.println("assigning campaign to contact");
-
-							WorkflowSubscribeUtil.subscribeDeferred(contact, trigger.campaign_id,
-									new JSONObject().put("message", message));
-						}
-					}
-				}
-			}
-		}
-		catch (JSONException e)
-		{
-			System.out.println("exception occured parsing mandrill events JSON " + e.getMessage());
-			// response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-			return;
-		}
-
-		NamespaceManager.set(oldNamespace);
-		// response.setStatus(HttpServletResponse.SC_OK);
+	    String mandrillEvents = request.getParameter("mandrill_events");
+	    // System.out.println("mandrill events parameter is " +
+	    // mandrillEvents);
+	    if (StringUtils.isBlank(mandrillEvents))
+	    {
+		// response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 		return;
-	}
+	    }
 
-	public JSONObject getSenderNames(String fromName, String fromEmail) throws JSONException
-	{
-		JSONObject from = new JSONObject();
-		if (!(StringUtils.isBlank(fromName) || StringUtils.equals(fromName, "null")))
+	    JSONArray events = new JSONArray(mandrillEvents);
+	    for (int i = 0; i < events.length(); i++)
+	    {
+		JSONObject event = events.getJSONObject(i);
+		// System.out.println("event json is " + event);
+
+		if (event.has("event") && StringUtils.equals(event.getString("event"), "inbound"))
 		{
-			if (StringUtils.contains(fromName, " "))
+		    JSONObject message = event.getJSONObject("msg");
+		    // System.out.println("email message is " + message);
+		    if (message == null)
+		    {
+			// response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			continue;
+		    }
+		    String agileEmail = getAgileEmail(message);
+		    String apiKey = getAgileDetail(agileEmail, AgileDetail.API_KEY);
+		    String agileDomain = getAgileDetail(agileEmail, AgileDetail.DOMAIN);
+
+		    System.out.println("agile api key is " + apiKey);
+		    System.out.println("agile special email is " + agileEmail);
+		    System.out.println("agile domain is " + agileDomain);
+		    if (agileEmail == null || agileDomain == null || apiKey == null)
+		    {
+			// response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			continue;
+		    }
+
+		    NamespaceManager.set(agileDomain);
+
+		    Key<DomainUser> owner = APIKey.getDomainUserKeyRelatedToAPIKey(apiKey);
+		    System.out.println("owner is " + owner);
+		    if (owner == null)
+		    {
+			// response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			continue;
+		    }
+		    System.out.println("CURRENT namespace is " + NamespaceManager.get());
+
+		    if (isConfirmationEmail(message))
+		    {
+			// response.setStatus(HttpServletRequest.SC_BAD_REQUEST);
+			sendConfirmationEmail(apiKey, message);
+			continue;
+		    }
+
+		    String fromEmail = message.getString("from_email");
+
+		    String fromName = null;
+		    if (message.has("from_name"))
+			fromName = message.getString("from_name");
+
+		    System.out.println("from email is " + fromEmail);
+		    System.out.println("from name is " + fromName);
+
+		    Boolean newContact = isNewContact(fromEmail);
+		    Contact contact = buildContact(fromName, fromEmail, newContact);
+
+		    if (contact == null)
+		    {
+			// response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			continue;
+		    }
+
+		    System.out.println("saving contact");
+		    contact.setContactOwner(owner);
+
+		    try
+		    {
+			contact.save();
+		    }
+		    catch (PlanRestrictedException e)
+		    {
+			continue;
+		    }
+		    catch (Exception e)
+		    {
+			continue;
+		    }
+
+		    List<Trigger> triggers = TriggerUtil
+			    .getTriggersByCondition(com.agilecrm.workflows.triggers.Trigger.Type.INBOUND_MAIL_EVENT);
+		    for (Trigger trigger : triggers)
+		    {
+			System.out.println("trigger id is " + trigger.id);
+			if (getTriggerRunResult(newContact, trigger))
 			{
-				from.put(Contact.FIRST_NAME, fromName.split(" ")[0]);
-				from.put(Contact.LAST_NAME, fromName.split(" ")[1]);
+			    System.out.println("assigning campaign to contact");
+
+			    WorkflowSubscribeUtil.subscribeDeferred(contact, trigger.campaign_id,
+				    new JSONObject().put("message", message));
 			}
-			else
-				from.put(Contact.FIRST_NAME, fromName);
+		    }
 		}
-		else
-			from.put(Contact.FIRST_NAME, fromEmail.split("@")[0]);
-		return from;
+	    }
 	}
-
-	public Boolean isNotAgileEmail(String recepientEmail)
+	catch (JSONException e)
 	{
-		return !StringUtils.contains(recepientEmail, "@agle.cc");
+	    System.out.println("exception occured parsing mandrill events JSON " + e.getMessage());
+	    // response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+	    return;
 	}
 
-	public Contact buildContact(String fromName, String fromEmail, Boolean isNewContact)
+	NamespaceManager.set(oldNamespace);
+	// response.setStatus(HttpServletResponse.SC_OK);
+	return;
+    }
+
+    public JSONObject getSenderNames(String fromName, String fromEmail) throws JSONException
+    {
+	JSONObject from = new JSONObject();
+	if (!(StringUtils.isBlank(fromName) || StringUtils.equals(fromName, "null")))
 	{
-		if (StringUtils.isBlank(fromEmail) || StringUtils.equals(fromEmail, "null"))
-			return null;
-
-		Contact contact = null;
-
-		if (isNewContact)
-			contact = new Contact();
-		else
-			contact = ContactUtil.searchContactByEmail(fromEmail);
-
-		contact.addpropertyWithoutSaving(new ContactField(Contact.EMAIL, fromEmail, null));
-		try
-		{
-			JSONObject from = getSenderNames(fromName, fromEmail);
-			if (from.has(Contact.FIRST_NAME))
-				contact.addpropertyWithoutSaving(new ContactField(Contact.FIRST_NAME, from
-						.getString(Contact.FIRST_NAME), null));
-			if (from.has(Contact.LAST_NAME))
-				contact.addpropertyWithoutSaving(new ContactField(Contact.LAST_NAME, from.getString(Contact.LAST_NAME),
-						null));
-
-			return contact;
-		}
-		catch (JSONException e)
-		{
-			return null;
-		}
+	    if (StringUtils.contains(fromName, " "))
+	    {
+		from.put(Contact.FIRST_NAME, fromName.split(" ")[0]);
+		from.put(Contact.LAST_NAME, fromName.split(" ")[1]);
+	    }
+	    else
+		from.put(Contact.FIRST_NAME, fromName);
 	}
+	else
+	    from.put(Contact.FIRST_NAME, fromEmail.split("@")[0]);
+	return from;
+    }
 
-	public String getAgileEmail(JSONObject message)
+    public Boolean isNotAgileEmail(String recepientEmail)
+    {
+	return !StringUtils.contains(recepientEmail, "@agle.cc");
+    }
+
+    public Contact buildContact(String fromName, String fromEmail, Boolean isNewContact)
+    {
+	if (StringUtils.isBlank(fromEmail) || StringUtils.equals(fromEmail, "null"))
+	    return null;
+
+	Contact contact = null;
+
+	if (isNewContact)
+	    contact = new Contact();
+	else
+	    contact = ContactUtil.searchContactByEmail(fromEmail);
+
+	contact.addpropertyWithoutSaving(new ContactField(Contact.EMAIL, fromEmail, null));
+	try
 	{
-		try
-		{
-			JSONArray allRecepients = new JSONArray();
-			if (message.has("to"))
-				allRecepients.put(message.getJSONArray("to"));
-			if (message.has("cc"))
-				allRecepients.put(message.getJSONArray("cc"));
+	    JSONObject from = getSenderNames(fromName, fromEmail);
+	    if (from.has(Contact.FIRST_NAME))
+		contact.addpropertyWithoutSaving(new ContactField(Contact.FIRST_NAME, from
+		        .getString(Contact.FIRST_NAME), null));
+	    if (from.has(Contact.LAST_NAME))
+		contact.addpropertyWithoutSaving(new ContactField(Contact.LAST_NAME, from.getString(Contact.LAST_NAME),
+		        null));
 
-			for (int i = 0; i < allRecepients.length(); i++)
-			{
-				System.out.println(allRecepients.getJSONArray(i));
-				JSONArray recepients = allRecepients.getJSONArray(i);
-				for (int j = 0; j < recepients.length(); j++)
-				{
-					System.out.println(recepients.getJSONArray(j));
-					JSONArray recepient = recepients.getJSONArray(j);
-					if (!isNotAgileEmail(recepient.getString(0)))
-						return recepient.getString(0);
-				}
-			}
-			if (!StringUtils.isBlank(message.getString("email")) && !isNotAgileEmail(message.getString("email")))
-				return message.getString("email");
-
-			return null;
-		}
-		catch (JSONException e)
-		{
-			return null;
-		}
+	    return contact;
 	}
-
-	public String getAgileDetail(String agileEmail, AgileDetail detail)
+	catch (JSONException e)
 	{
-		try
-		{
-			if (detail == AgileDetail.API_KEY)
-				return agileEmail.split("@")[0].split("-")[1];
-			else if (detail == AgileDetail.DOMAIN)
-				return agileEmail.split("@")[0].split("-")[0];
-			return null;
-		}
-		catch (Exception e)
-		{
-			return null;
-		}
+	    return null;
 	}
+    }
 
-	public Boolean isNewContact(String fromEmail)
+    public String getAgileEmail(JSONObject message)
+    {
+	try
 	{
-		return !ContactUtil.isExists(fromEmail);
-	}
+	    JSONArray allRecepients = new JSONArray();
+	    if (message.has("to"))
+		allRecepients.put(message.getJSONArray("to"));
+	    if (message.has("cc"))
+		allRecepients.put(message.getJSONArray("cc"));
 
-	public Boolean getTriggerRunResult(Boolean newContact, Trigger trigger)
+	    for (int i = 0; i < allRecepients.length(); i++)
+	    {
+		System.out.println(allRecepients.getJSONArray(i));
+		JSONArray recepients = allRecepients.getJSONArray(i);
+		for (int j = 0; j < recepients.length(); j++)
+		{
+		    System.out.println(recepients.getJSONArray(j));
+		    JSONArray recepient = recepients.getJSONArray(j);
+		    if (!isNotAgileEmail(recepient.getString(0)))
+			return recepient.getString(0);
+		}
+	    }
+	    if (!StringUtils.isBlank(message.getString("email")) && !isNotAgileEmail(message.getString("email")))
+		return message.getString("email");
+
+	    return null;
+	}
+	catch (JSONException e)
 	{
-		return (newContact || !TriggerUtil.getTriggerRunStatus(trigger));
+	    return null;
 	}
+    }
 
-	public Boolean isConfirmationEmail(JSONObject message)
+    public String getAgileDetail(String agileEmail, AgileDetail detail)
+    {
+	try
 	{
-		Boolean isConfirmationEmail = false;
-		try
-		{
-			String messageSubject = message.getString("subject");
-			String[] emailTitleWords = new String[] { "Forwarding", "Confirmation" };
-			for (int i = 0; i < emailTitleWords.length; i++)
-			{
-				isConfirmationEmail = StringUtils.contains(messageSubject, emailTitleWords[i]);
-				if (!isConfirmationEmail)
-					break;
-			}
-		}
-		catch (JSONException e)
-		{
-			isConfirmationEmail = false;
-		}
-		return isConfirmationEmail;
+	    if (detail == AgileDetail.API_KEY)
+		return agileEmail.split("@")[0].split("-")[1];
+	    else if (detail == AgileDetail.DOMAIN)
+		return agileEmail.split("@")[0].split("-")[0];
+	    return null;
 	}
-
-	public void sendConfirmationEmail(String apiKey, JSONObject message)
+	catch (Exception e)
 	{
-		try
-		{
-			String messageSubject = message.getString("subject");
-			System.out.println("forwarding confirmation email with subject " + messageSubject);
-
-			DomainUser user = APIKey.getDomainUserRelatedToAPIKey(apiKey);
-			EmailUtil.sendEmailUsingAPI("noreply@agilecrm.com", "Agile CRM", user.email, null, null, messageSubject,
-					null, null, message.getString("text"), null);
-		}
-		catch (Exception e)
-		{
-			System.out.println("exception occured in sending confirmation email " + e.getMessage());
-		}
+	    return null;
 	}
+    }
+
+    public Boolean isNewContact(String fromEmail)
+    {
+	return !ContactUtil.isExists(fromEmail);
+    }
+
+    public Boolean getTriggerRunResult(Boolean newContact, Trigger trigger)
+    {
+	return (newContact || !TriggerUtil.getTriggerRunStatus(trigger));
+    }
+
+    public Boolean isConfirmationEmail(JSONObject message)
+    {
+	Boolean isConfirmationEmail = false;
+	try
+	{
+	    String messageSubject = message.getString("subject");
+	    String[] emailTitleWords = new String[] { "Forwarding", "Confirmation" };
+	    for (int i = 0; i < emailTitleWords.length; i++)
+	    {
+		isConfirmationEmail = StringUtils.contains(messageSubject, emailTitleWords[i]);
+		if (!isConfirmationEmail)
+		    break;
+	    }
+	}
+	catch (JSONException e)
+	{
+	    isConfirmationEmail = false;
+	}
+	return isConfirmationEmail;
+    }
+
+    public void sendConfirmationEmail(String apiKey, JSONObject message)
+    {
+	try
+	{
+	    String messageSubject = message.getString("subject");
+	    System.out.println("forwarding confirmation email with subject " + messageSubject);
+
+	    DomainUser user = APIKey.getDomainUserRelatedToAPIKey(apiKey);
+	    EmailUtil.sendEmailUsingAPI("noreply@agilecrm.com", "Agile CRM", user.email, null, null, messageSubject,
+		    null, null, message.getString("text"), null);
+	}
+	catch (Exception e)
+	{
+	    System.out.println("exception occured in sending confirmation email " + e.getMessage());
+	}
+    }
 }
