f5cd1e3c10f3ca2ee2408e4e97913f0705ae45cd
saikiran-k <saikiran@invox.com>
2015-04-30 15:21:19 +0530

    Task details tab position cookie name.

--- agile-frontend/WebContent/jscore/handlebars/handlebars-helpers.js
+++ agile-frontend/WebContent/jscore/handlebars/handlebars-helpers.js
@@ -3798,7 +3798,7 @@ $(function()
 												// Avoid comma appending to last element
 												if (i < j - 1)
 												{
-																ret = ret + ",";
+																ret = ret + ", ";
 												}
 												;
 								}
