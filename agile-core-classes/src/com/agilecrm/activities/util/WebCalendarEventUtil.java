e3c75551e8e4d48b83b43c766f9fe2fa3e68c7a2
jagadeeshAgilecrm <jagadeesh@invox.com>
2015-05-01 12:21:34 +0530

    added null condition

--- agile-core-classes/src/com/agilecrm/activities/util/WebCalendarEventUtil.java
+++ agile-core-classes/src/com/agilecrm/activities/util/WebCalendarEventUtil.java
@@ -783,7 +783,8 @@ public class WebCalendarEventUtil
 				client_event.created_time = System.currentTimeMillis() / 1000;
 				newEvnt.color = "#36C";
 				newEvnt.type = EventType.WEB_APPOINTMENT;
-				newEvnt.description = StringUtils.isNotEmpty(wce.phoneNumber) ? wce.phoneNumber + " - " + wce.notes
+				newEvnt.description = StringUtils.isNotEmpty(wce.phoneNumber) || wce.phoneNumber != "" ? wce.phoneNumber
+						+ " - " + wce.notes
 						: wce.notes;
 
 				epoch_start_date = newEvnt.start;
@@ -839,7 +840,8 @@ public class WebCalendarEventUtil
 				client_event.created_time = System.currentTimeMillis() / 1000;
 				newEvnt.color = "#36C";
 				newEvnt.type = EventType.WEB_APPOINTMENT;
-				newEvnt.description = StringUtils.isNotEmpty(wce.phoneNumber) ? wce.phoneNumber + " - " + wce.notes
+				newEvnt.description = StringUtils.isNotEmpty(wce.phoneNumber) || wce.phoneNumber != "" ? wce.phoneNumber
+						+ " - " + wce.notes
 						: wce.notes;
 
 				epoch_start_date = newEvnt.start;
