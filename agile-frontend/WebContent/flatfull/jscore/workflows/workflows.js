7d93547bfc155a317e23280c6adc9fc9d3bdd181
premnathtammina <premtammina@gmail.com>
2015-05-11 19:19:13 +0530

    UI Bug fixes

--- agile-frontend/WebContent/flatfull/jscore/workflows/workflows.js
+++ agile-frontend/WebContent/flatfull/jscore/workflows/workflows.js
@@ -204,7 +204,7 @@ $(function(){
 
 		// Stops video on modal hide
 		$(workflow_help_modal).on("hide.bs.modal", function(){
-			$(this).children('div.modal-body').find("iframe").removeAttr("src");
+			$(this).find("iframe").removeAttr("src");
 		});
 	});
 	
