69e989fd1b146ab0e9e56f1f7da30b73d898e035
Subrahmanyam <subrahmanyam@invox.com>
2015-05-12 12:31:54 +0530

    task name click issue in today tasks portlet

--- agile-frontend/WebContent/flatfull/controllers/portlets.js
+++ agile-frontend/WebContent/flatfull/controllers/portlets.js
@@ -298,7 +298,7 @@ $('#portlets-tasks-model-list > tr').live('click', function(e) {
 	// Add notes in task modal
 	showNoteOnForm("updateTaskForm", value.notes);*/
 	var hrefFlag = false;
-	if(e.target.attributes!=undefined && e.target.attributes!=null && e.target.attributes.length==0 && e.srcElement.tagName=="A")
+	if(e.target.attributes!=undefined && e.target.attributes!=null && e.target.attributes.length==0 && e.target.tagName.toLowerCase()=="a")
 		hrefFlag = true;
 	$.each(e.target.attributes,function(){
 		if(this.name=="href")
