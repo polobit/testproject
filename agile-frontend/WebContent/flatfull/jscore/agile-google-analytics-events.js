5ed2455c43adf0136ac55230b661be300233bbd0
yaswanth <yaswanth@agilecrm.com>
2015-05-18 12:38:50 +0530

    Tracking payment events

--- agile-frontend/WebContent/flatfull/jscore/agile-google-analytics-events.js
+++ agile-frontend/WebContent/flatfull/jscore/agile-google-analytics-events.js
@@ -2,12 +2,42 @@
 
  function push_signup_plan(plan)
  {
-	_gaq.push(['_trackEvent', 'Dashboard', 'Signup', _plan_on_signup["plan_type"], _plan_on_signup["quantity"]]);
+ 	if(!window.ga)
+ 	{
+ 		setTimeout(function(){
+ 			push_signup_plan(plan);
+ 		}, 2000);
+ 	}
+
+ 	try
+ 	{
+ 		ga('send', 'event', 'Dashboard', 'Signup', _plan_on_signup["plan_type"], _plan_on_signup["quantity"]]);	
+ 	}
+ 	catch(err)
+ 	{
+ 		console.error(err);
+ 	}
+	
+	
  }
 
  function push_actual_plan(plan)
  {
-	_gaq.push(['_trackEvent', 'Dashboard', 'Paid', plan["plan_type"], plan["quantity"]]);
+ 	if(!window.ga)
+ 	{
+ 		setTimeout(function(){
+ 			push_actual_plan(plan);
+ 		}, 2000);
+ 	}
+	
+	try
+	{
+		ga('send', 'event', 'Dashboard', 'Paid', plan["plan_type"], plan["quantity"]]);
+	}
+ 	catch(err)
+ 	{
+ 		console.error(err);
+ 	}
  }
 
  $(function(){
