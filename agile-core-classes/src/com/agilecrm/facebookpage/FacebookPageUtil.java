0a9dcc2220dad969d483134e358c506807cae909
yaswanth <yaswanth@agilecrm.com>
2015-06-05 12:42:16 +0530

    Facebook fix - Purushotham

--- agile-core-classes/src/com/agilecrm/facebookpage/FacebookPageUtil.java
+++ agile-core-classes/src/com/agilecrm/facebookpage/FacebookPageUtil.java
@@ -20,8 +20,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import src.com.agilecrm.Globals;
-
+import com.agilecrm.Globals;
 import com.agilecrm.db.ObjectifyGenericDao;
 import com.google.appengine.api.NamespaceManager;
 import com.google.appengine.api.utils.SystemProperty;
@@ -38,7 +37,7 @@ public class FacebookPageUtil
     // is used for local test)
     private static String redirect_uri = "https://my.agilecrm.com/backend/googleservlet";
     private static String state = "http://localhost:8888/fbpagecallback";
-    
+
     // / set this to the list of extended permissions you want
     private static final String[] scopes = new String[] { "public_profile", "email", "manage_pages" };
 
@@ -55,22 +54,24 @@ public class FacebookPageUtil
     public static String getLoginRedirectURL()
     {
 	setProductionCallbackURL();
-	return FB_END_POINT + "/oauth/authorize?client_id=" + APP_ID + "&display=page&scope=" + StringUtils.join(scopes, ',') + "&redirect_uri=" + redirect_uri + "&state=" + state;
+	return FB_END_POINT + "/oauth/authorize?client_id=" + APP_ID + "&display=page&scope="
+		+ StringUtils.join(scopes, ',') + "&redirect_uri=" + redirect_uri + "&state=" + state;
     }
 
     public static String getAuthURL(String authCode)
     {
 	setProductionCallbackURL();
-	return FB_END_POINT + "/oauth/access_token?client_id=" + APP_ID + "&redirect_uri=" + redirect_uri + "&client_secret=" + APP_SECRET + "&code="
-		+ authCode;
+	return FB_END_POINT + "/oauth/access_token?client_id=" + APP_ID + "&redirect_uri=" + redirect_uri
+		+ "&client_secret=" + APP_SECRET + "&code=" + authCode;
     }
 
     public static void setProductionCallbackURL()
     {
 	if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production)
 	{
-		//state = "https://" + NamespaceManager.get() +  ".agilecrm.com/fbpagecallback";		
-		state = "https://" + NamespaceManager.get() + "-dot-sandbox-dot-agilecrmbeta.appspot.com/fbpagecallback";
+	    // state = "https://" + NamespaceManager.get() +
+	    // ".agilecrm.com/fbpagecallback";
+	    state = "https://" + NamespaceManager.get() + "-dot-sandbox-dot-agilecrmbeta.appspot.com/fbpagecallback";
 	}
     }
 
@@ -82,7 +83,8 @@ public class FacebookPageUtil
 	for (String pair : pairs)
 	{
 	    int idx = pair.indexOf("=");
-	    queryPairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"), URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
+	    queryPairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"),
+		    URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
 	}
 
 	return queryPairs.get("access_token").trim();
@@ -114,23 +116,26 @@ public class FacebookPageUtil
 	return listOfpages;
     }
 
-    public static boolean linkOurFacebookTab(String pageID, String accessToken, String formName) throws JSONException, UnsupportedEncodingException
+    public static boolean linkOurFacebookTab(String pageID, String accessToken, String formName) throws JSONException,
+	    UnsupportedEncodingException
     {
-	String response = HttpRequest(FB_END_POINT + "/" + pageID + "/tabs?app_id=" + APP_ID + "&method=post&access_token=" + accessToken);
+	String response = HttpRequest(FB_END_POINT + "/" + pageID + "/tabs?app_id=" + APP_ID
+		+ "&method=post&access_token=" + accessToken);
 	JSONObject responseObj = new JSONObject(response);
 	boolean status = responseObj.getBoolean("success");
 	if (status)
 	{
 	    formName = formName.replaceAll("[()?:!.,;@/\\{}]+", "");
-	    HttpRequest(FB_END_POINT + "/" + pageID + "/tabs/app_" + APP_ID + "?custom_name=" + URLEncoder.encode(formName, "UTF-8")
-		    + "&method=post&access_token=" + accessToken);
+	    HttpRequest(FB_END_POINT + "/" + pageID + "/tabs/app_" + APP_ID + "?custom_name="
+		    + URLEncoder.encode(formName, "UTF-8") + "&method=post&access_token=" + accessToken);
 	}
 	return status;
     }
-    
+
     public static boolean deleteOurFacebookTab(String pageID, String accessToken) throws JSONException
     {
-	String response = HttpRequest(FB_END_POINT + "/" + pageID + "/tabs/app_" + APP_ID + "?method=delete&access_token=" + accessToken);
+	String response = HttpRequest(FB_END_POINT + "/" + pageID + "/tabs/app_" + APP_ID
+		+ "?method=delete&access_token=" + accessToken);
 	JSONObject responseObj = new JSONObject(response);
 	boolean status = responseObj.getBoolean("success");
 	return status;
@@ -174,62 +179,63 @@ public class FacebookPageUtil
     {
 	if (StringUtils.isBlank(pageID))
 	    return null;
-	
+
 	Query<FacebookPage> q = null;
 	String oldNameSpace = NamespaceManager.get();
 	NamespaceManager.set("");
-	
+
 	try
 	{
-		ObjectifyGenericDao<FacebookPage> dao = new ObjectifyGenericDao<FacebookPage>(FacebookPage.class);
-		q = dao.ofy().query(FacebookPage.class);
-		q.filter("page_id", pageID);
+	    ObjectifyGenericDao<FacebookPage> dao = new ObjectifyGenericDao<FacebookPage>(FacebookPage.class);
+	    q = dao.ofy().query(FacebookPage.class);
+	    q.filter("page_id", pageID);
 	}
 	finally
 	{
-		NamespaceManager.set(oldNameSpace);
+	    NamespaceManager.set(oldNameSpace);
 	}
-	
+
 	return q.get();
     }
 
     public static List<Map<String, String>> getLinkedFacebookPages(String domain)
     {
-    if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production)
-    	if (StringUtils.isBlank(domain))
-    	    return null;
-	
+	if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production)
+	    if (StringUtils.isBlank(domain))
+		return null;
+
 	List<FacebookPage> facebookPages = null;
-	
+
 	try
 	{
-		NamespaceManager.set("");
-		ObjectifyGenericDao<FacebookPage> dao = new ObjectifyGenericDao<FacebookPage>(FacebookPage.class);
-		Query<FacebookPage> q = dao.ofy().query(FacebookPage.class);
-		if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production)
-		{
-			q.filter("domain", domain);
-		}
-		facebookPages = q.list();
+	    NamespaceManager.set("");
+	    ObjectifyGenericDao<FacebookPage> dao = new ObjectifyGenericDao<FacebookPage>(FacebookPage.class);
+	    Query<FacebookPage> q = dao.ofy().query(FacebookPage.class);
+	    if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production)
+	    {
+		q.filter("domain", domain);
+	    }
+	    facebookPages = q.list();
 	}
 	finally
 	{
-		NamespaceManager.set(domain);
+	    NamespaceManager.set(domain);
 	}
 
 	List<Map<String, String>> listOfpages = new ArrayList<Map<String, String>>();
-	
-	if(facebookPages != null) {
-		for (Iterator<FacebookPage> iterator = facebookPages.iterator(); iterator.hasNext();)
-		{
-		    FacebookPage facebookPage = (FacebookPage) iterator.next();
-		    Map<String, String> pagesInfo = new LinkedHashMap<String, String>();
-		    pagesInfo.put("form_id", facebookPage.form_id);
-		    pagesInfo.put("form_name", facebookPage.form_name);
-		    pagesInfo.put("page_id", facebookPage.page_id);
-		    pagesInfo.put("page_name", facebookPage.page_name);
-		    listOfpages.add(pagesInfo);
-		}
+
+	if (facebookPages != null)
+	{
+	    for (Iterator<FacebookPage> iterator = facebookPages.iterator(); iterator.hasNext();)
+	    {
+		FacebookPage facebookPage = (FacebookPage) iterator.next();
+		Map<String, String> pagesInfo = new LinkedHashMap<String, String>();
+		pagesInfo.put("form_id", facebookPage.form_id);
+		pagesInfo.put("form_name", facebookPage.form_name);
+		pagesInfo.put("page_id", facebookPage.page_id);
+		pagesInfo.put("page_name", facebookPage.page_name);
+		listOfpages.add(pagesInfo);
+	    }
 	}
 
 	return listOfpages;
