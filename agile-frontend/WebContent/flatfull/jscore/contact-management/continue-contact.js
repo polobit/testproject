d2c411a519ee5bf28e49bdfed1ed2f620349d039
saikiran-k <saikiran@invox.com>
2015-06-09 14:54:21 +0530

    Tags for companies.

--- agile-frontend/WebContent/flatfull/jscore/contact-management/continue-contact.js
+++ agile-frontend/WebContent/flatfull/jscore/contact-management/continue-contact.js
@@ -303,6 +303,64 @@ function serialize_and_save_continue_contact(e, form_id, modal_id, continueConta
 
 								if (isValidField(form_id + ' #company_url'))
 												properties.push(property_JSON('url', form_id + ' #company_url'));
+								
+								if (tagsSourceId === undefined || !tagsSourceId || tagsSourceId.length <= 0)
+									tagsSourceId = form_id;
+
+								var tags = get_tags(tagsSourceId);
+								if (tags != undefined && tags.length != 0)
+								{
+												obj.tags = [];
+
+												var tags_valid = true;
+												if (!obj['tagsWithTime'] || obj['tagsWithTime'].length == 0)
+												{
+																$.each(tags[0].value, function(index, value)
+																{
+																	if(!isValidTag(value, false)) {
+																		tags_valid = false;
+																		return false;
+																	}
+																});
+																obj['tagsWithTime'] = [];
+																$.each(tags[0].value, function(index, value)
+																{
+																				obj.tagsWithTime.push({ "tag" : value });
+																});
+												}
+												else
+												{
+																var tag_objects_temp = [];
+																$.each(tags[0].value, function(index, value)
+																{
+																				var is_new = true;
+																				$.each(obj['tagsWithTime'], function(index, tagObject)
+																				{
+																								if (value == tagObject.tag)
+																								{
+																												tag_objects_temp.push(tagObject);
+																												is_new = false
+																												return false;
+																								}
+																				});
+
+																				if (is_new) {
+																					tag_objects_temp.push({ "tag" : value });
+																					//check if tags are valid if they are newly adding to the contact.
+																					if(!isValidTag(value, false)) {
+																						tags_valid = false;
+																						return false;
+																					}
+																				}
+																});
+																obj['tagsWithTime'] = tag_objects_temp;
+												}
+												if(!tags_valid) {
+													$('.invalid-tags-person').show().delay(6000).hide(1);
+													enable_save_button($(saveBtn));
+													return false;
+												}
+								}
 				}
 
 				/*
@@ -725,7 +783,7 @@ $(function()
 				// Update button click event in continue-company
 				$("#company-update").die().live('click', function(e)
 				{
-								serialize_and_save_continue_contact(e, 'continueCompanyForm', 'companyModal', false, false, this);
+								serialize_and_save_continue_contact(e, 'continueCompanyForm', 'companyModal', false, false, this,'tags_source_continue_company');
 				});
 });
 
