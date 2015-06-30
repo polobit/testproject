a5a2984a31c24b60c6ce82b0347a0380f6be32ca
saikiran-k <saikiran@invox.com>
2015-06-30 18:32:53 +0530

    Deals related contacts bulk actions.

--- agile-core-classes/src/com/agilecrm/core/api/deals/DealsBulkActionsAPI.java
+++ agile-core-classes/src/com/agilecrm/core/api/deals/DealsBulkActionsAPI.java
@@ -1,7 +1,10 @@
 package com.agilecrm.core.api.deals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
@@ -13,12 +16,17 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.commons.lang.StringUtils;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.agilecrm.AgileQueues;
 import com.agilecrm.activities.Activity.EntityType;
 import com.agilecrm.activities.util.ActivitySave;
+import com.agilecrm.bulkaction.deferred.CampaignSubscriberDeferredTask;
+import com.agilecrm.contact.Contact;
+import com.agilecrm.contact.util.ContactUtil;
 import com.agilecrm.contact.util.bulk.BulkActionNotifications;
 import com.agilecrm.deals.Opportunity;
 import com.agilecrm.deals.util.OpportunityUtil;
@@ -28,9 +36,16 @@ import com.agilecrm.session.SessionManager;
 import com.agilecrm.session.UserInfo;
 import com.agilecrm.user.DomainUser;
 import com.agilecrm.user.util.DomainUserUtil;
+import com.agilecrm.workflows.Workflow;
 import com.agilecrm.workflows.triggers.Trigger;
 import com.agilecrm.workflows.triggers.util.DealTriggerUtil;
 import com.agilecrm.workflows.triggers.util.TriggerUtil;
+import com.agilecrm.workflows.util.WorkflowUtil;
+import com.google.appengine.api.NamespaceManager;
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskOptions;
+import com.googlecode.objectify.Key;
 
 /**
  * <code>DealsAPI</code> includes REST calls to interact with
@@ -437,6 +452,149 @@ public class DealsBulkActionsAPI
 	{
 	    System.out.println("Exception in export deal in backend code. - " + e.getMessage());
 	    e.printStackTrace();
+	}
+    }
+
+    /**
+     * Call backends for adding tags to the related contacts of the selected
+     * deals.
+     */
+    @Path("/contacts/add-tag/{current_user_id}")
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public void addTagToDealRelatedContacts(@PathParam("current_user_id") Long currentUserId,
+	    @FormParam("ids") String ids, @FormParam("filter") String filters, @FormParam("form") String form)
+    {
+	// Set the session manager to get the user preferences and the other
+	// details required.
+	if (SessionManager.get() != null)
+	{
+	    SessionManager.get().setDomainId(currentUserId);
+	}
+	else
+	{
+	    DomainUser user = DomainUserUtil.getDomainUser(currentUserId);
+	    SessionManager.set(new UserInfo(null, user.email, user.name));
+	    SessionManager.get().setDomainId(user.id);
+	}
+
+	if (StringUtils.isEmpty(form))
+	    return;
+
+	try
+	{
+	    JSONObject formJSON = new JSONObject(form);
+	    System.out.println("------------" + formJSON.toString());
+
+	    if (!formJSON.has("tags"))
+		return;
+
+	    JSONArray tagsJSONArray = formJSON.getJSONArray("tags");
+
+	    String[] tagsArray = null;
+	    try
+	    {
+		tagsArray = new ObjectMapper().readValue(tagsJSONArray.toString(), String[].class);
+	    }
+	    catch (Exception e)
+	    {
+		e.printStackTrace();
+	    }
+
+	    if (tagsArray == null)
+		return;
+
+	    List<Opportunity> deals = OpportunityUtil.getOpportunitiesForBulkActions(ids, filters, 100);
+	    System.out.println("total deals -----" + deals.size());
+	    Set<Contact> contacts = new HashSet<Contact>();
+	    for (Opportunity deal : deals)
+	    {
+		for (Contact contact : deal.getContacts())
+		{
+		    contacts.add(contact);
+		}
+	    }
+
+	    List<Contact> contactsList = new ArrayList<Contact>();
+	    contactsList.addAll(contacts);
+
+	    // ContactUtil.deleteContactsbyListSupressNotification(fetcher.nextSet());
+	    ContactUtil.addTagsToContactsBulk(contactsList, tagsArray);
+
+	    BulkActionNotifications.publishNotification(Arrays.asList(tagsArray).toString() + " Tag(s) are added to "
+		    + contacts.size() + " Contacts.");
+
+	    ActivitySave.createBulkActionActivity(contacts.size(), "ADD_TAG", tagsJSONArray.toString(), "contacts", "");
+
+	}
+	catch (Exception je)
+	{
+	    je.printStackTrace();
+	}
+    }
+
+    /**
+     * Call backends for adding the related contacts of deals to the selected
+     * campaign.
+     */
+    @Path("/contacts/add-campaign/{workflow_id}/{current_user_id}")
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public void addDealRelatedContactToCampaigns(@PathParam("current_user_id") Long currentUserId,
+	    @PathParam("workflow_id") Long workflowId, @FormParam("ids") String ids, @FormParam("filter") String filters)
+    {
+
+	DomainUser user = null;
+	// Set the session manager to get the user preferences and the other
+	// details required.
+	if (SessionManager.get() != null)
+	{
+	    SessionManager.get().setDomainId(currentUserId);
+	}
+	else
+	{
+	    user = DomainUserUtil.getDomainUser(currentUserId);
+	    SessionManager.set(new UserInfo(null, user.email, user.name));
+	    SessionManager.get().setDomainId(user.id);
+	}
+
+	try
+	{
+	    if (user == null)
+		return;
+
+	    List<Opportunity> deals = OpportunityUtil.getOpportunitiesForBulkActions(ids, filters, 100);
+	    System.out.println("total deals -----" + deals.size());
+	    Set<Key<Contact>> contacts = new HashSet<Key<Contact>>();
+	    for (Opportunity deal : deals)
+	    {
+		for (String contactId : deal.getContact_ids())
+		{
+		    contacts.add(new Key<Contact>(Contact.class, Long.parseLong(contactId)));
+		}
+	    }
+
+	    CampaignSubscriberDeferredTask task = new CampaignSubscriberDeferredTask(currentUserId, workflowId,
+		    NamespaceManager.get(), contacts, new UserInfo(user));
+
+	    // Add to queue
+	    Queue queue = QueueFactory.getQueue(AgileQueues.CAMPAIGN_SUBSCRIBE_SUBTASK_QUEUE);
+	    queue.add(TaskOptions.Builder.withPayload(task));
+
+	    Workflow workflow = WorkflowUtil.getWorkflow(workflowId);
+
+	    String message = contacts.size() + " Contacts are added to " + workflow.name + " Campaign.";
+	    if (contacts.size() == 1)
+		message = " 1 Contact is added to " + workflow.name + " Campaign.";
+
+	    BulkActionNotifications.publishNotification(message);
+
+	    ActivitySave.createBulkActionActivity(contacts.size(), "ASIGN_WORKFLOW", workflow.name, "contacts", "");
+
+	}
+	catch (Exception je)
+	{
+	    je.printStackTrace();
 	}
     }
 
