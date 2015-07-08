5a427fe4c06fc728424dd0f9e329d6390b653335
Santhosh Reddy Mogulla <mogulla@invox.com>
2015-07-08 18:52:15 +0530

    update tasks count when task deleted

--- agile-frontend/WebContent/flatfull/controllers/task-details.js
+++ agile-frontend/WebContent/flatfull/controllers/task-details.js
@@ -225,6 +225,11 @@ $(function()
 		$.ajax({ url : 'core/api/tasks/' + id, type : 'DELETE', success : function(response)
 		{
 			document.location.href = document.location.origin + "#/tasks";
+			var due_task_count = getDueTasksCount();
+			if(due_task_count !=0)
+				$('#due_tasks_count').html(due_task_count);
+			else
+				$('#due_tasks_count').html("");
 		} })
 	})
 
