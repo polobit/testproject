5de008cb0b481655a534c822dec4dba06db55705
Subrahmanyam <subrahmanyam@invox.com>
2015-07-13 16:05:13 +0530

    timezone issue fixes in reports

--- agile-frontend/WebContent/flatfull/jscore/email-charts.js
+++ agile-frontend/WebContent/flatfull/jscore/email-charts.js
@@ -101,7 +101,7 @@ function getOptions()
 	//Get the GMT end time
 	var end_time = getGMTEpochFromDate(new Date(end_value));
 
-	end_time += (23*60*60)+(59*60)+59;
+	end_time += (((23*60*60)+(59*60)+59)*1000);
 
 	// Adds start_time, end_time and timezone offset to params.
 	options += ("start_time=" + start_time + "&end_time=" + end_time);
