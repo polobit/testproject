a007e4bb3b57128d94d3748d40b3ad73086f097b
karthikagile <karthik@invox.com>
2015-07-27 12:04:45 +0530

    form redirect changes included

--- agile-core-classes/src/com/thirdparty/forms/AgileFormRedirect.java
+++ agile-core-classes/src/com/thirdparty/forms/AgileFormRedirect.java
@@ -1,19 +1,15 @@
 package com.thirdparty.forms;
 
-import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -40,11 +36,8 @@ public class AgileFormRedirect extends HttpServlet
 	String urlParameters = sb.toString();
 	URL url = new URL(req.getAttribute("url").toString() + "&" + urlParameters);
 
-	if (StringUtils.equalsIgnoreCase("GET", req.getMethod()))
-	{
-	    res.sendRedirect(url.toString());
-	}
-	else if (StringUtils.equalsIgnoreCase("POST", req.getMethod()))
+	res.sendRedirect(url.toString());
+	if (StringUtils.equalsIgnoreCase("POST", req.getMethod()))
 	{
 	    HttpURLConnection con = (HttpURLConnection) url.openConnection();
 	    con.setRequestMethod("POST");
@@ -54,18 +47,6 @@ public class AgileFormRedirect extends HttpServlet
 	    wr.writeBytes(urlParameters);
 	    wr.flush();
 	    wr.close();
-
-	    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8));
-	    String inputLine;
-	    StringBuffer response = new StringBuffer();
-	    ServletOutputStream sout = res.getOutputStream();
-	    while ((inputLine = in.readLine()) != null)
-	    {
-		response.append(inputLine);
-		sout.write(inputLine.getBytes());
-	    }
-	    in.close();
-	    sout.flush();
 	}
     }
 }
