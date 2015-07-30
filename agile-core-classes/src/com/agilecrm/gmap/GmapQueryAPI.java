c14efe0935a4ba0a84d63126aec4913d8f66af0d
vinay-patlolla-agile <vinay@agilecrm.com>
2015-07-30 17:01:38 +0530

    Crap utility classes were removed,visitors table view sql query and Ui changes included.

--- agile-core-classes/src/com/agilecrm/gmap/GmapQueryAPI.java
+++ agile-core-classes/src/com/agilecrm/gmap/GmapQueryAPI.java
@@ -11,6 +11,7 @@ import javax.ws.rs.core.MediaType;
 @Path ("api/gmap")
 public class GmapQueryAPI {
 	
+	/** API handles visitors page map request to find the latest unique visitors for a domain in a selected date range*/
 	@Path("daterange")
 	@GET 
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
@@ -18,12 +19,12 @@ public class GmapQueryAPI {
     {
 		try
 		{
-			List<GmapLogs> visitorsLatLong = GmapUtil.getGmapVisitors(userDomain, startDate, endDate, timeZoneOffset);
+			GmapService gmapService=new GmapServiceMysqlmpl();
+			List<GmapLogs> visitorsLatLong = gmapService.getLatestVisitors(userDomain, startDate, endDate, timeZoneOffset);
 			return visitorsLatLong;
 		}
 		catch (Exception e)
 		{
-			e.printStackTrace();
 			return null;
 		}
     }
@@ -34,8 +35,8 @@ public class GmapQueryAPI {
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public List<GmapLogs> getVisitorsBySession(@QueryParam("user_domain") String userDomain, @QueryParam("start_date") String startDate, @QueryParam("end_date") String endDate, @QueryParam("time_zone") String timeZoneOffset,@QueryParam("cursor") String offset,@QueryParam("page_size") String pageSize)
     {
-		List<GmapLogs> visitorsLatLong = GmapUtil.getGmapVisitorsbySession(userDomain, startDate, endDate, timeZoneOffset,offset,pageSize);
-
+		GmapService gmapService=new GmapServiceMysqlmpl();
+		List<GmapLogs> visitorsLatLong = gmapService.getPageViews(userDomain, startDate, endDate, timeZoneOffset,offset,pageSize);
 		try
 		{
 			if(visitorsLatLong.isEmpty())
@@ -46,7 +47,6 @@ public class GmapQueryAPI {
 		}
 		catch (Exception e)
 		{
-			e.printStackTrace();
 			return null;
 		}
     }
