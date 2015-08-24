d63173379c910f1e1bad92555d10fc3ee5d86e00
yaswanth.agile <yaswanth@agilecrm.com>
2015-08-24 16:08:35 +0530

    Api Stats

--- agile-taskqueue-puller/src/com/agilecrm/TaskQueueStatsDaemon.java
+++ agile-taskqueue-puller/src/com/agilecrm/TaskQueueStatsDaemon.java
@@ -22,6 +22,8 @@ import com.google.apphosting.api.ApiProxy;
 import com.google.apphosting.api.ApiProxy.Delegate;
 import com.google.apphosting.api.ApiProxy.Environment;
 import com.thirdparty.Mailgun;
+ronment;
+import com.thirdparty.Mailgun;
 
 public class TaskQueueStatsDaemon extends Thread
 {
@@ -251,6 +253,7 @@ public class TaskQueueStatsDaemon extends Thread
 		    .lease(Globals.PROJECT_NAME, TASK_QUEUE_NAME, PullScheduler.DEFAULT_COUNT_LIMIT,
 			    PullScheduler.DEFAULT_LEASE_PERIOD).set(Globals.GROUP_BY_TAG_PARAM, true);
 
+	    APIStats.incrementCounter();
 	    logger.info(lease.buildHttpRequestUrl().toString());
 	    com.google.api.services.taskqueue.model.Tasks tasks = lease.execute();
 
