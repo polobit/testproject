84df7b0b98101c6948ad050ed77538071d377862
premtammina <premtammina@gmail.com>
2015-09-11 10:59:23 +0530

    widget config

---
+++ agile-frontend/WebContent/flatfull/jscore/widgets/widget-config.js
@@ -0,0 +1,115 @@
+[
+    {
+        name: "Social",
+        description: "Know your customer social activity",
+        widgets: [
+            {
+                name: "Facebook",
+                description: "Facebook is a social utility that connects people with friends and others who work, study and live around them",
+                logo: "facebook.png",
+                icon: "Fb24x24icon.png"
+            },
+            {
+                name: "GooglePlus",
+                description: "Keep tabs on your customers' activity on Google+ and engage with them better.",
+                logo: "gplus.png",
+                icon: "gplusicon.png"
+            },
+            {
+                name: "Twitter",
+                description: " Twitter offers a great way of engaging with contacts in real time based on what they tweet.",
+                logo: "/img/plugins/twitter.png",
+                icon: "twitter-logo-small.png"
+            },
+            {
+                name: "Rapleaf",
+                description: " Towerdata makes it incredibly easy for you to personalize content for your customers.",
+                logo: "/img/plugins/towerdata.png",
+                icon: "towerdata-logo-small.png"
+            }
+        ]
+    },
+    {
+        name: "Customer Support",
+        description: "View tickets and chats",
+        widgets: [
+            {
+                name: "ClickDesk",
+                description: "Convert chat sessions with potential customers into contacts in Agile along with the conversation.",
+                logo: "/img/plugins/clickdesk-logo.png",
+                icon: "clickdesk-logo-small.png"
+            },
+            {
+                name: "Zendesk",
+                description: "Zendesk streamlines communication within a ticket that has organized workflow towards resolution of the customer issue.",
+                logo: "/img/plugins/zendesk_logo.png",
+                icon: "zendesk-logo-small.png"
+            },
+            {
+                name: "HelpScout",
+                description: "Help Scout is a help desk for teams that insist on a delightful customer experience without exposing to ticket numbers, portals or robotic emails.",
+                logo: "/img/plugins/helpscout-logo.png",
+                icon: "helpscout-logo-small.png"
+            }
+        ]
+    },
+    {
+        name: "Calling",
+        description: "Make & Receive calls from Agile",
+        widgets: [
+            {
+                name: "CallScript",
+                description: "Shows you the script for a call based on your preset rules."
+            },
+            {
+                name: "TwilioIO",
+                description: "Make and receive calls from your contacts using your Twilio account.",
+                logo: "/img/plugins/twilio.png",
+                icon: "twilio-small-logo.png"
+            },
+            {
+                name: "Sip",
+                description: "Make and receive calls from your contacts using any SIP account.",
+                logo: "sip-logo-small.png",
+                icon: "sip-logo-small.png"
+            },
+            {
+                name: "Twilio",
+                description: " This widget is now deprecated and a new improved Twilio widget is available.",
+                logo: "/img/plugins/twilio.png",
+                icon: "twilio-small-logo.png"
+            }
+        ]
+    },
+    {
+        name: "Billing",
+        description: "View and send invoices",
+        widgets: [
+            {
+                name: "FreshBooks",
+                description: "FreshBooks enables the ability to create and manage invoices, estimates, expenses, and projects all in one place.",
+                logo: "/img/plugins/freshbooks-logo.png",
+                icon: "freshbooks-small-logo.png"
+            },
+            {
+                name: "Stripe",
+                description: "Stripe enables individuals and businesses to accept payments over the internet.",
+                logo: "/img/plugins/Stripe.png",
+                icon: "stripe-small-logo.png"
+            },
+            {
+                name: "Xero",
+                description: "Xero is an online accounting software for small business - Explore invoicing, reconciliation anytime, anywhere.",
+                logo: "xero.png",
+                icon: "xero24x24.png"
+            },
+            {
+                name: "QuickBooks",
+                description: "Run your whole business better with QuickBooks wherever you are. Automate your invoicing, billing and reporting and simplify your business for life!",
+                logo: "intuit-quickbooks.png",
+                icon: "quickbooks_small.jpg",
+                
+            }
+        ]
+    }
+]
