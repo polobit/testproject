520918450f1f620b306643a1f9b98f21af54dd5a
sasiclickdesk <sasi@faxdesk.com>
2015-09-28 11:11:14 +0530

    Included basic MailHandlerServlet
    # Conflicts:
    #	agile-frontend/WebContent/WEB-INF/web.xml

---
+++ agile-core-classes/src/com/agilecrm/ticket/servlets/TicketWebhook.java
@@ -0,0 +1,101 @@
+package com.agilecrm.ticket.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.agilecrm.user.util.DomainUserUtil;
+
+/**
+ * Servlet implementation class TicketWebhook
+ */
+public class TicketWebhook extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+			IOException
+	{
+		try
+		{
+			String mandrillResponse = request.getParameter("mandrill_events");
+
+			System.out.println("MandrillResponse: " + mandrillResponse);
+
+			if (StringUtils.isBlank(mandrillResponse))
+				return;
+
+			// Mandrill Inbound event format -
+			// https://mandrill.zendesk.com/hc/en-us/articles/205583197-Inbound-Email-Processing-Overview#inbound-events-format
+			JSONObject mandrillInboundJSON = new JSONObject(mandrillResponse);
+
+			if (!mandrillInboundJSON.has("msg"))
+				return;
+
+			JSONObject msgJSON = mandrillInboundJSON.getJSONObject("msg");
+
+			if (!msgJSON.has("headers"))
+				return;
+
+			JSONArray recipientsArray = mandrillInboundJSON.getJSONArray("to");
+
+			if (recipientsArray == null || recipientsArray.length() == 0)
+				return;
+
+			String toAddress = "";
+			for (int i = 0; i < recipientsArray.length(); i++)
+			{
+				JSONArray recipientJSON = recipientsArray.getJSONArray(i);
+
+				toAddress = recipientJSON.getString(0);
+
+				if (toAddress.endsWith("helptor"))
+					break;
+			}
+
+			String[] toAddressArray = toAddress.split("+");
+
+			if (toAddressArray.length != 2)
+				return;
+
+			String domainName = toAddressArray[0], groupID = toAddressArray[1];
+
+			System.out.println("DomainName: " + domainName);
+			System.out.println("GroupID: " + groupID);
+			
+			if (DomainUserUtil.count(domainName) <= 0)
+			{
+				System.out.println("Invalid domain: " + domainName);
+				return;
+			}
+
+			JSONObject mimeHeaders = mandrillInboundJSON.getJSONObject("headers");
+		}
+		catch (Exception e)
+		{
+			System.out.println("ExceptionUtils.getFullStackTrace(e): " + ExceptionUtils.getFullStackTrace(e));
+			e.printStackTrace();
+		}
+	}
+}
