4396938489e4fcc592b38a225ef26a51233af6bf
Subrahmanyam <subrahmanyam@invox.com>
2015-10-09 18:19:49 +0530

    milestone changed time issue

--- agile-core-classes/src/com/agilecrm/deals/Opportunity.java
+++ agile-core-classes/src/com/agilecrm/deals/Opportunity.java
@@ -504,7 +504,7 @@ public class Opportunity extends Cursor implements Serializable
 	    //can update milestone changed time with old milestone changed time
 	    if (this.pipeline_id.equals(oldOpportunity.getPipeline_id())
 		    && this.milestone.equals(oldOpportunity.milestone) 
-		    && this.lost_reason_id != null && oldOpportunity.lost_reason_id == null)
+		    && this.lost_reason_id != null && ((oldOpportunity.lost_reason_id != null && oldOpportunity.lost_reason_id == 0L) || oldOpportunity.lost_reason_id == null))
 	    	this.milestone_changed_time = oldOpportunity.milestone_changed_time;
 	}
 	else if (oldOpportunity == null && this.milestone.equalsIgnoreCase(wonMilestone))
