1550313226c55a7449757cb70e8a6af4b0154877
jagadeeshAgilecrm <jagadeesh@invox.com>
2015-10-19 11:15:41 +0530

    data-sync code udpated

--- agile-frontend/WebContent/flatfull/jscore/widgets/data-sync.js
+++ agile-frontend/WebContent/flatfull/jscore/widgets/data-sync.js
@@ -124,7 +124,11 @@ function executeDataSyncReturnCallback(returnUrl,serviceName){
 		}
 		returnUrl=returnUrl.substr(returnUrl.indexOf('#'));
 
+<<<<<<< HEAD
 		if(window.location.hash==returnUrl && (serviceName=='quickbook-import' || serviceName=='stripe_import')){
+=======
+		if(window.location.hash==returnUrl){
+>>>>>>> 2973594... stripe delete bug fix
 
 			window.location.reload();
 			return;
