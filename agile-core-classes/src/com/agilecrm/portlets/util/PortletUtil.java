f9f966d31602e8a8bf4fc2fd634d235f0fa9db5a
Subrahmanyam <subrahmanyam@invox.com>
2015-10-29 18:11:32 +0530

    Tag report support issue fixed

--- agile-core-classes/src/com/agilecrm/portlets/util/PortletUtil.java
+++ agile-core-classes/src/com/agilecrm/portlets/util/PortletUtil.java
@@ -598,8 +598,8 @@ public class PortletUtil {
 			String time_zone = DateUtil.getCurrentUserTimezoneOffset();
 		    if (time_zone != null)
 		    {
-		    	start_date += (Long.parseLong(time_zone)*60*1000);
-		    	end_date += (Long.parseLong(time_zone)*60*1000);
+		    	start_date -= (Long.parseLong(time_zone)*60*1000);
+		    	end_date -= (Long.parseLong(time_zone)*60*1000);
 		    }
 			ReportsUtil.check(start_date, end_date);
 			
