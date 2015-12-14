968fe8befcc66393f9303154429f4af91971e2b2
govindclickdesk <govind@invox.com>
2015-12-14 12:15:44 +0530

    Activities loading issue with popover

--- agile-frontend/WebContent/flatfull/jscore/activities/calendar.js
+++ agile-frontend/WebContent/flatfull/jscore/activities/calendar.js
@@ -492,9 +492,11 @@ function showCalendar(users)
 																 '</div>' + '</div>';
 											$(this).after(popoverElement);
 										}else{
+											try{	
 											var popoverElement = '<div class="fc-overlayw ' + leftorright + '" style="width:100%;min-width:' + popover_min_width + 'px;max-width:' + popover_min_width + 'px;left:' + left + 'px;top:' + top + 'px;position:absolute;z-index:10;display:none;">' + '<div class="panel bg-white b-a pos-rlt p-sm">' + '<span class="arrow ' + leftorright + ' ' + pullupornot + '" style="top:11px;"></span>' + '<div class="h4 font-thin m-b-sm"><div class="pull-left text-ellipsis p-b-xs" style="width:100%;">' + event.title + '</div></div>' + '<div class="line b-b b-light"></div>' + '<div><i class="icon-clock text-muted m-r-xs"></i>' + event.start
 													.format('dd-mmm-yyyy HH:MM') + '<div class="pull-right" style="width:10%;"><img class="r-2x" src="' + event.ownerPic + '" height="20px" width="20px" title="' + event.owner.name + '"/></div></div>' + '<div class="text-ellipsis">' + reletedContacts + '</div>' + '<div class="text-ellipsis">' + meeting_type + '</div>' + '</div>' + '</div>';
 											$(this).after(popoverElement);
+											}catch(e){}
 										}
 										
 										$(this).parent().find('.fc-overlayw').find('.arrow').css({ "top" : "-9px", "left" : "11px" });
