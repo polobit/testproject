28ac6373e25a20a7507e4c3c4656e46a11dd45b3
nareshagile <naresh@faxdesk.com>
2015-12-16 18:10:09 +0530

    Changed help info for clicked node and added new image for SMS link clicked

--- agile-frontend/WebContent/json/nodes/email/clicked.js
+++ agile-frontend/WebContent/json/nodes/email/clicked.js
@@ -2,8 +2,8 @@
     "name": "Clicked?",
     "thumbnail": "json/nodes/images/email/clicked.png",
     "icon": "json/nodes/icons/email/clicked.png",
-    "info": "Check if a link in the email is clicked within a specified duration.",
-    "help": "Check if a link in the email is clicked within a specified duration.",
+    "info": "Check if a link in the email/sms is clicked within a specified duration.",
+    "help": "Check if a link in the email/sms is clicked within a specified duration.",
     "author": "John",
     "company": "mantra",
     "language": "en",
@@ -38,7 +38,7 @@
             "type": "select"
         },
         {
-            "label": "Check if the email recepient has clicked on a link in the email. Here, you need to mention the maximum waiting period before it proceeds to the next step. <br/><br/>This node has 2 exit paths - Yes and No.<br/><ul><li>If there is a click, then the control moves through the 'Yes' path without waiting any further.</li><br/><li> If the recepient did not click any link in the preceding email, it waits here for specified time. If there is no click till the max wait time, it moves through the 'No' path. </li></ul>",
+            "label": "Check if the email/sms recepient has clicked on a link in the email/sms. Here, you need to mention the maximum waiting period before it proceeds to the next step. <br/><br/>This node has 2 exit paths - Yes and No.<br/><ul><li>If there is a click, then the control moves through the 'Yes' path without waiting any further.</li><br/><li> If the recepient did not click any link in the preceding email/sms, it waits here for specified time. If there is no click till the max wait time, it moves through the 'No' path. </li></ul>",
             "category": "Help",
             "fieldType": "label",
             "type": "label"
