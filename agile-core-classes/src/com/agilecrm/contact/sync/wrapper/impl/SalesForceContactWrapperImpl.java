c213759da4084a48bd49441a7702e867b7a94e1b
govindclickdesk <govind@invox.com>
2015-12-25 20:48:09 +0530

    Salesforce Task added

--- agile-core-classes/src/com/agilecrm/contact/sync/wrapper/impl/SalesForceContactWrapperImpl.java
+++ agile-core-classes/src/com/agilecrm/contact/sync/wrapper/impl/SalesForceContactWrapperImpl.java
@@ -15,10 +15,10 @@ import org.json.JSONObject;
 import com.agilecrm.contact.Contact;
 import com.agilecrm.contact.ContactField;
 import com.agilecrm.contact.Note;
+import com.agilecrm.contact.deferred.SalesforceImportNoteDeferredTask;
 import com.agilecrm.contact.sync.ImportStatus;
 import com.agilecrm.contact.sync.service.impl.SalesforceSync;
 import com.agilecrm.contact.sync.wrapper.ContactWrapper;
-import com.agilecrm.contact.sync.wrapper.impl.SalesforceImportDeferred.ACTION_TYPE;
 import com.agilecrm.session.SessionManager;
 import com.agilecrm.session.UserInfo;
 import com.agilecrm.user.AgileUser;
@@ -307,10 +307,10 @@ public class SalesForceContactWrapperImpl extends ContactWrapper
 		}
 		
 		// Get notes from Salesforce
-		SalesforceImportDeferred def =  new SalesforceImportDeferred(NamespaceManager.get(), entry.toString(), ACTION_TYPE.ADD_NOTES, contact.id);
+		SalesforceImportNoteDeferredTask task =  new SalesforceImportNoteDeferredTask(NamespaceManager.get(), entry.toString(), SalesforceImportNoteDeferredTask.ACTION_TYPE.ADD_NOTES, contact.id);
 		
-		Queue queue = QueueFactory.getDefaultQueue();
-		queue.add(TaskOptions.Builder.withPayload(def));
+		Queue defaultQueue = QueueFactory.getDefaultQueue();
+		defaultQueue.addAsync(TaskOptions.Builder.withPayload(task));
 		
 		
     }
@@ -335,74 +335,4 @@ public class SalesForceContactWrapperImpl extends ContactWrapper
 		return null;
 	}
 
-}
-
-
-class SalesforceImportDeferred implements DeferredTask {
-
-	public enum ACTION_TYPE {
-		ADD_NOTES
-	}
-	
-	public ACTION_TYPE actionType;
-	public String domainName;
-	public String contactEntry;
-	
-	public Long contactId;
-	
-	public SalesforceImportDeferred(String domainName, String contactEntry, ACTION_TYPE actionType, Long contactId){
-		this.domainName = domainName;
-		this.actionType = actionType;
-		this.contactEntry = contactEntry;
-		this.contactId = contactId;
-	}
-	
-	@Override
-	public void run() {
-		
-		System.out.println("SalesforceImportDeferred = " + domainName + " : " + actionType + " : " + contactEntry);
-		
-		if(StringUtils.isBlank(contactEntry))
-			  return;
-		
-		System.out.println("contactId = " + contactId);
-		
-		try {
-			
-			JSONObject contactJSON = new JSONObject(contactEntry);
-			
-			JSONArray notesArray = new JSONArray(contactJSON.getString(SalesforceSync.CONTACTS_NOTES_KEY));
-			
-			NamespaceManager.set(domainName);
-			
-			for (int i = 0; i < notesArray.length(); i++) {
-				JSONObject noteEntry = notesArray.getJSONObject(i);
-				
-				try {
-					addNote(JSONUtil.getJSONValue(noteEntry, "Title") ,JSONUtil.getJSONValue(noteEntry, "Body"));	
-				} catch (Exception e) {
-				} finally{
-				}
-				
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	public void addNote(String title, String subject) {
-    	
-		try {
-			
-	    	Note note = new Note(title,subject);
-			note.addRelatedContacts(String.valueOf(contactId));
-			note.save();
-			System.out.println("In note " + note.id);
-			
-		} catch (Exception e) {
-		}
-		
-	}
-	
 }
