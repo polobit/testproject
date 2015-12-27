7937a87796731de2e6420013558dbd3f1c0855d5
premnathtammina <premtammina@gmail.com>
2015-12-27 18:11:29 +0530

    paypal js code updated

--- agile-frontend/WebContent/flatfull/widgets/paypal.js
+++ agile-frontend/WebContent/flatfull/widgets/paypal.js
@@ -1,10 +1,42 @@
 
+var paypalOBJ = {};
+var paymentINVCount = 1;
+var showMorePaypalINV = '<div class="widget_tab_footer paypal_inv_show_more" align="center"><a class="c-p text-info" id="paypal_inv_show_more" rel="tooltip" title="Click to see more tickets">Show More</a></div>';
+
+
+function loadPaypalInvoices(offSet){
+
+	if(offSet == 0){
+		var result = {};
+		result.invoices = paypalOBJ.invoices.slice(0, 5);
+
+		getTemplate('paypal-invoices', result, undefined, function(template_inv){			
+			$('#Paypal').html(template_inv);
+		},null);
+
+		if(paypalOBJ.invoices.length > 5){
+			$('#Paypal').append(showMorePaypalINV);
+		}
+	}else if(offSet > 0  && (offSet+5) < paypalOBJ.invoices.length){
+		var result = {};
+		result.invoices = paypalOBJ.invoices.slice(offSet, (offSet+5));
+		$('.paypal_inv_show_more').remove();
+		$('#Paypal').apped(getTemplate('paypal-invoices', result));
+		$('#Paypal').append(showMorePaypalINV);
+	}else{
+		var result = {};
+		result.invoices = paypalOBJ.invoices.slice(offSet, paypalOBJ.invoices.length);
+		$('.paypal_inv_show_more').remove();
+		$('#Paypal').append(getTemplate('paypal-invoices', result));
+	}
+
+}
+
 function loadProfile(contact_id, callback){	
 	var queueName = "widget_queue_"+ contact_id;
 	queueGetRequest(queueName, "/core/api/widgets/paypal/profile/" + PAYPAL_Plugin_Id, "", 
 	function success(data){
-		getDetails(data);
-
+		getInvoices(data);			
 		// If defined, execute the callback function
 		if (callback && typeof (callback) === "function")
 			callback(data);
@@ -14,12 +46,9 @@ function loadProfile(contact_id, callback){
 	});
 }
 
-function getDetails(accessToken) {	
-	console.log("accessToken "+accessToken);
+function getInvoices(accessToken){	
 	var tok = "Bearer " + accessToken;
 	var obj = {email : "prem.agilecrm@gmail.com"};
-
-//	{ email : Email};
 
 	$.ajax({
 		headers : {
@@ -32,18 +61,20 @@ function getDetails(accessToken) {
 		data : JSON.stringify(obj),
 		complete : function(result) {
 			console.log("Paypal *** Invoices");
-			console.log(result);
-			var data={};			
-			data = result.responseJSON;		
-			getTemplate('paypal-invoices', data, undefined, function(template_ui){				
-				$('#Paypal').html(template_ui);
-			});			
+			console.log(result);		
+			paypalOBJ = {};	
+			paymentINVCount = 1;			
+			paypalOBJ.invoices = result.responseJSON.invoices;
+			loadPaypalInvoices(0);					
 		}
 	});
 }
 
 function startPaypalWidget(contact_id){
 	PAYPAL_PLUGIN_NAME = "Paypal";
+
+	paypalOBJ = {};
+	paymentINVCount =1;
 
 	PAYPAL_UPDATE_LOAD_IMAGE = '<center><img id="paypal_load" src=' + '\"img/ajax-loader-cursor.gif\" style="margin-top: 10px;margin-bottom: 14px;"></img></center>';
 
@@ -53,6 +84,16 @@ function startPaypalWidget(contact_id){
 
 	PAYPAL_Plugin_Id = paypal_widget.id;	
 	Email = agile_crm_get_contact_property('email');
+
+
+	$("#widgets").off("click", "#paypal_inv_show_more");
+	$("#widgets").on("click", "#paypal_inv_show_more", function(e){
+		e.preventDefault();
+		var offSet = paymentINVCount * 5;
+		loadPaypalInvoices(offSet);
+		++paymentINVCount;
+	});
+
 
 	loadProfile(contact_id, function(){
 
