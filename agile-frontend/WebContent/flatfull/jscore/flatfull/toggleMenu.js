1201974c98639a63e59c1602de0c2008f37f6fe1
santhoshboga <santhosh@invox.com>
2016-01-08 15:36:21 +0530

    contact details mobile fix

--- agile-frontend/WebContent/flatfull/jscore/flatfull/toggleMenu.js
+++ agile-frontend/WebContent/flatfull/jscore/flatfull/toggleMenu.js
@@ -29,6 +29,49 @@ $('#app-aside-folded').on('click', function(e) {
 	
     
 	});
+
+
+ /* starting css for tabs */
+
+ $(document).ready(function() {
+
+    // DEPENDENCY: https://github.com/flatlogic/bootstrap-tabcollapse
+
+
+    // if the tabs are in a narrow column in a larger viewport
+    /*$('.sidebar-tabs').tabCollapse({
+        tabsClass: 'visible-tabs',
+        accordionClass: 'hidden-tabs'
+    });*/
+
+    // if the tabs are in wide columns on larger viewports
+    $('.content-tabs').tabCollapse();
+
+    // initialize tab function
+    $('.nav-tabs a').click(function(e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+
+    // slide to top of panel-group accordion
+    $('.panel-group').on('shown.bs.collapse', function() {
+        var panel = $(this).find('.in');
+        $('html, body').animate({
+            scrollTop: panel.offset().top + (-60)
+        }, 500);
+    });
+
+});
+
+ /*ending css for tabs */
+
+
+
+
+
+
+
+
 	
 $(document).ready(function(){
 
@@ -54,6 +97,16 @@ $(document).ready(function(){
 	
 
 	if(( $(window).width() ) < 768 ) {
+
+
+	// if the tabs are in wide columns on larger viewports
+    $('.content-tabs').tabCollapse();
+
+    // initialize tab function
+    $('.nav-tabs a').click(function(e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
 
 	/*$('body').on('click','#mobile-dropdown-click-sort',function(){
 			$("#contact-sort-views").css("display","block");
