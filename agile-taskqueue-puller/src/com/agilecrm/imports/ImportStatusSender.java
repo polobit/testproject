4e8081dbcd6091b9e34673a24dee15e8ac574a94
yaswanth.agile <yaswanth@agilecrm.com>
2016-01-11 09:20:57 +0530

    Link in import status

--- agile-taskqueue-puller/src/com/agilecrm/imports/ImportStatusSender.java
+++ agile-taskqueue-puller/src/com/agilecrm/imports/ImportStatusSender.java
@@ -1,9 +1,14 @@
 package com.agilecrm.imports;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.agilecrm.contact.upload.blob.status.specifications.StatusProcessor;
 import com.agilecrm.contact.upload.blob.status.specifications.StatusSender;
 import com.agilecrm.user.DomainUser;
+import com.agilecrm.util.VersioningUtil;
 import com.agilecrm.util.email.SendMail;
+import com.google.appengine.api.NamespaceManager;
 
 public class ImportStatusSender implements StatusSender
 {
@@ -15,6 +20,10 @@ public class ImportStatusSender implements StatusSender
 	{
 	    return;
 	}
+
+	Map<String, String> extraParams = new HashMap<String, String>();
+
+	extraParams.put("link", VersioningUtil.getHostURLByApp(NamespaceManager.get()) + "#import");
 
 	// TODO Auto-generated method stub
 	System.out.println("Email to be sent to email");
