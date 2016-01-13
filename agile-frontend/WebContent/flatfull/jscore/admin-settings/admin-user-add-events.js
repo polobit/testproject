5122d92ed020c05081a5cf3d8f929fb5bc94651b
Santhosh Reddy Mogulla <mogulla@invox.com>
2016-01-13 09:54:57 +0530

    uncheck admin event condition

--- agile-frontend/WebContent/flatfull/jscore/admin-settings/admin-user-add-events.js
+++ agile-frontend/WebContent/flatfull/jscore/admin-settings/admin-user-add-events.js
@@ -2,10 +2,13 @@ function bindAdminChangeAction(el, data)
 {
 	$('input[name="is_admin"]', el).on('change', function(e){
 	var is_admin = $(this).is(":checked");
-	if(is_admin == false)
-		$("input[type=checkbox]", $('div[name="newscopes"]', el)).removeAttr("disabled");
-	else
-		$("input[type=checkbox]", $('div[name="newscopes"]', el)).attr("checked", "checked" ).attr("disabled", "disabled");
+	if(_plan_restrictions.is_ACL_allowed[0]())
+	{
+		if(is_admin == false)
+			$("input[type=checkbox]", $('div[name="newscopes"]', el)).removeAttr("disabled");
+		else
+			$("input[type=checkbox]", $('div[name="newscopes"]', el)).attr("checked", "checked" ).attr("disabled", "disabled");
+	}
 	})
 	
 	$("input[type=checkbox]", $('div[name="newscopes"]', el)).on('change', function(e){
