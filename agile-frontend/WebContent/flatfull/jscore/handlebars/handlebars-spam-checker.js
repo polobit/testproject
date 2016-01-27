07353f241bd08efd9cac9980570b37182e727086
prashannjeet-agile <prashannjeet@agilecrm.com>
2016-01-27 10:42:39 +0530

    Checking Spam Score First commit

---
+++ agile-frontend/WebContent/flatfull/jscore/handlebars/handlebars-spam-checker.js
@@ -0,0 +1,66 @@
+$(function()
+{
+
+	/**
+	 * Helper function to return the value of a property matched with the given
+	 * name from the array of properties
+	 * 
+	 * @method getPropertyValue
+	 * @param {Object}
+	 *            items array of objects
+	 * @param {String}
+	 *            name to get matched object value
+	 * @returns value of the matched object
+	 */
+
+	
+	/**
+	 * Helper function to return the value of Bootstrap class name for 
+	 *  Spam Score Popup Panel based on the value of score property
+	 */
+	Handlebars.registerHelper( "checkSpamScore", function ( score ){
+    if (score > 5 )
+    {
+        return 'danger';
+    }
+    else if(score < 1)
+    {
+        return 'success';
+    }
+    else return 'info';
+ });
+
+  
+	/**
+	 * Helper function to return the value of Bootstrap class name for 
+	 *  Spam Score Popup Label points based on the value of score property
+	 */
+   Handlebars.registerHelper( "checkSpamScoreError", function ( score ){
+    if (score >= 1 )
+    {
+        return 'danger';
+    }
+    else if(score <= 0)
+    {
+        return 'success';
+    }
+    else return 'warning';
+ });
+
+   /**
+	 * Helper function to return the value of Spam Score Popup title 
+	 *  message based on the value of total spam score property
+	 */
+
+Handlebars.registerHelper( "checkSpamMessage", function ( score ){
+    if (score > 5 )
+    {
+        return "SpamAssassin Did Not Likes Your Email Template.. !";
+    }
+    else if(score < 1)
+    {
+        return "SpamAssassin Likes Your Email Templates....";
+    }
+    else return "SpamAssassin Partialy Likes Your Email Templates";
+});
+});
