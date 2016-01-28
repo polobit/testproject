6767bd0b94bafd6cc1ce7767c7aa9fd3e0ee1874
saiagilecrm <sayee@agilecrm.com>
2016-01-28 16:46:37 +0530

    Not textavatar in IE

--- agile-frontend/WebContent/flatfull/jscore/util.js
+++ agile-frontend/WebContent/flatfull/jscore/util.js
@@ -585,3 +585,7 @@ function getFormattedDateObjectWithString(value){
 	
 }
 
+function isIE() {
+  var myNav = navigator.userAgent.toLowerCase();
+  return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+}
