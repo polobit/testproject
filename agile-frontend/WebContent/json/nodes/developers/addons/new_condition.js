fb2262f7076eb71cae55daa694ca5dce90a28fc2
dharmakolla <dharmatejakolla@DHARMATEJAs-MacBook-Pro.local>
2016-02-09 11:02:56 +0530

    Changes to condition node And Or

---
+++ agile-frontend/WebContent/json/nodes/developers/addons/new_condition.js
@@ -0,0 +1,8 @@
+{
+    "name": "Condition",
+    "thumbnail": "json/nodes/images/sms/Condition.png",
+    "icon": "json/nodes/icons/sms/Condition.png",
+    "info": "Check for a condition (IF/Value/Len) in your workflow.",
+    "help": "Check for a condition (if/value/len) in your workflow.",
+    "path": "json/nodes/developers/new_condition.js"
+}
