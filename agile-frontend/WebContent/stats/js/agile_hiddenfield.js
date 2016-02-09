1ba359474faa8be87d845045f7a6d89004455ac2
sonisonali <sonali@agilecrm.com>
2016-02-09 14:29:52 +0530

    utm parameters stored

--- agile-frontend/WebContent/stats/js/agile_hiddenfield.js
+++ agile-frontend/WebContent/stats/js/agile_hiddenfield.js
@@ -1,22 +1,27 @@
 
-if(document.getElementById('agile-form')!=null){
-window.onload=function(){
-  utmHiddenField();
-}
-}
+
+
+window.addEventListener('load', 
+  function() { 
+    if(document.getElementById('agile-form')!=null){
+      utmHiddenField();
+    }    
+  }, false);
+
 
 function utmHiddenField(){  
   
- 
-  for ( var i = 0, len= localStorage.length;i<len; ++i ) {
+  
+    for ( var i = 0, len= localStorage.length;i<len; ++i ) {
 
-  if(new RegExp("agile_").test(localStorage.key(i))){
-  var input = document.createElement("input");
-  input.setAttribute("type", "hidden");
-  input.setAttribute("name", "_"+localStorage.key(i));
-  input.setAttribute("value",localStorage.getItem(localStorage.key(i) ));
-  document.getElementById("agile-form").appendChild(input);
-  }
-  }
+      if(new RegExp("agile_").test(localStorage.key(i))){
+       var input = document.createElement("input");
+      input.setAttribute("type", "hidden");
+      input.setAttribute("name", "_"+localStorage.key(i));
+      input.setAttribute("value",localStorage.getItem(localStorage.key(i) ));
+      document.getElementById("agile-form").appendChild(input);
+      }
+    }
+  
 }
 
