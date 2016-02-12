7d94b707788bafd33ef76f9c100c30ccc3931dd3
srijaagilecrm <srijaagilecrm>
2016-02-12 10:53:36 +0530

    solve conflits

--- agile-frontend/WebContent/flatfull/controllers/admin-settings.js
+++ agile-frontend/WebContent/flatfull/controllers/admin-settings.js
@@ -186,8 +186,9 @@ var AdminSettingsRouter = Backbone.Router.extend({
 			$('#content').html($(template_ui));	
 
 			that.usersListView = new Base_Collection_View({ url : '/core/api/users', restKey : "domainUser", templateKey : "admin-settings-users",
-			individual_tag_name : 'tr', sortKey : "name", postRenderCallback : function(el)
+			individual_tag_name : "div", sortKey : "name", postRenderCallback : function(el)
 			{
+				$("#admin-settings-users-model-list").append("<div class='p-b col-md-4 col-sm-6 col-xs-12'><div class='b r-5x p-sm bg-white' style='height: 121px;'><center><span style='font-size:30px;margin-left:13px;'><i class='icon-plus-sign'/></span><br><a href='#users-add' class='btn btn-sm btn-default btn-addon admin-user-adduser' id='addUser'> Add User</a></center></div>");
 
 				head.js(LIB_PATH + 'lib/jquery.timeago.js', function()
 				{
