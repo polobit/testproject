7de7b03c44bbc9edb61732e7a23815d67b4f96b6
dharmakolla <dharmatejakolla@DHARMATEJAs-MacBook-Pro.local>
2016-02-17 16:12:24 +0530

    Background color changes

--- agile-frontend/WebContent/misc/landingpage/source/js/builder/projects/project.js
+++ agile-frontend/WebContent/misc/landingpage/source/js/builder/projects/project.js
@@ -262,6 +262,7 @@ angular.module('builder.projects', [])
 				$timeout(function() {
                 	$(".saveLandingPageButton",parent.document).prop("disabled",false);
 					$(".saveLandingPageButton",parent.document).html("Save Page");
+					window.parent.location.hash = ("landing-page/"+returnDataFormat.pages[0].id);
             	}, 3000);
 
 				//alertify.log("Saved successfully.", "success");
