f805e2a0537fd3addcede2de0fa5c7e6ec990ba6
santhosh0101 <santhoshkumar@agilecrm.com>
2016-02-17 18:54:27 +0530

    bug fixed radio buttons

--- agile-frontend/WebContent/misc/formbuilder/assets/collections/my-form-snippets.js
+++ agile-frontend/WebContent/misc/formbuilder/assets/collections/my-form-snippets.js
@@ -26,12 +26,13 @@ define([
        var snippetType = snippet.attributes.fields.id.value;
        var idArray = "";
         for(var i=1; i<=saveform.length-1; i++){
+        if(saveform[i].title!="Multiple Radios")
         idArray += saveform[i].fields.id.value+",";
        }
        //regex function to seperate numbers form id
       var largest = Math.max.apply(null, idArray.match(/(\d+)/g));
        
-       if(typeof this.counter[snippetType] === "undefined") {
+       if(typeof this.counter[snippetType] == "undefined") {
         if(isFinite(largest))
           this.counter[snippetType] = largest+1;
         else
