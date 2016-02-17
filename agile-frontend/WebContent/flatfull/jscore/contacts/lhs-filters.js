5f995bc143d19096d1415ac043b3b42a48c7cd67
srijaagilecrm <srijaagilecrm>
2016-02-17 11:52:25 +0530

    add trash button to tag and add bulk-actions

--- agile-frontend/WebContent/flatfull/jscore/contacts/lhs-filters.js
+++ agile-frontend/WebContent/flatfull/jscore/contacts/lhs-filters.js
@@ -185,8 +185,8 @@ $('#' + container_id).on('click', 'a.clear-filter-condition-lhs', function(e)
 {
 	
 	$(this).addClass('hide');
-	var container = $(this).parents('.lhs-row-filter');
-	$(container).find('#RHS').children().val("").attr('prev-val', "");
+	var container = $(this).closest('.lhs-row-filter');
+	$(container).find('#RHS:not(.no-filter-action)').children().val("").attr('prev-val', "");
 	$(container).find('#RHS_NEW').filter(visibleFilter).children().val("").attr('prev-val', "");
 	$(container).find('select[name="CONDITION"]').val($(container).find('select[name="CONDITION"] option:first').val()).attr('prev-val', "");
 	$(container).find('select[name="CONDITION"]').trigger('change');
