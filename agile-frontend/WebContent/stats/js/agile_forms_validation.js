5f90a7ee7fe0172c70c225fbefbb3cd1b0d27af4
santhosh0101 <santhoshkumar@agilecrm.com>
2016-02-22 15:53:35 +0530

    added email validations and website validations

--- agile-frontend/WebContent/stats/js/agile_forms_validation.js
+++ agile-frontend/WebContent/stats/js/agile_forms_validation.js
@@ -1,5 +1,7 @@
 function agile_validations(){
+	
 	var isValid =  true;
+	var count = null;    //to count the spans in the form
 	var agile_form = document.forms["agile-form"];
 
 	for(var i=0; i<agile_form.length; i++){
@@ -16,17 +18,58 @@ function agile_validations(){
 						spanTag.innerHTML = "Enter a value for this field.";
 						spanTag.id = "agile_span"+i;
 						spanTag.style.color = "red";
-						spanTag.style.fontSize = "14px";
+						spanTag.style.fontSize = "12px";
 						inputNode.parentNode.insertBefore(spanTag,inputNode.nextSibling);
+						count++;    //if span created then we will increase by one
+						continue;
 					}
+
 				else if(inputNode.value && spans){
-					isValid = false;
-					document.getElementById("agile_span"+i).remove();		
+
+					// email validations
+					if(agile_form[i].type == "email"){
+						var reg = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+ 						if (reg.test(inputNode.value)){
+		 					document.getElementById("agile_span"+i).remove(); 
+		 					isValid = true;
+		 					continue;
+							}
+ 						else{
+ 							document.getElementById("agile_span"+i).innerHTML = "Please enter a valid email";
+ 							count++;
+ 							continue;
+ 						}	
+					}
+
+					//website validations
+					if(agile_form[i].type == "url"){
+						var reg = /^(ftp|http|https):\/\/[^ "]+$/;
+						if(reg.test(inputNode.value)){
+							document.getElementById("agile_span"+i).remove();
+							isValid = true;
+							continue;
+							}
+							else{
+ 							document.getElementById("agile_span"+i).innerHTML = "Please enter a valid website";
+ 							count++;
+ 							continue;
+ 						}	
+					}
+
+					//other fields if have value 
+					document.getElementById("agile_span"+i).remove();
+					isValid = true;
+					continue;		
 				}
 				else if(inputNode.value == "" && spans){
 					isValid = false;
+					count++;
+					continue;
 				}
 	}
+				if(count != null){	// if form having spans 
+					isValid =false;
+				}
 
    }
 	return isValid;
