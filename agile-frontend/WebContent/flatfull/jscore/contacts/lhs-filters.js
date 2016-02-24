a773d9802f4eb071fe3eb0780d52dea4de2230e4
vin9963 <vinod@agilecrm.com>
2016-02-24 12:21:53 -0800

    after adding new testing changes

--- agile-frontend/WebContent/flatfull/jscore/contacts/lhs-filters.js
+++ agile-frontend/WebContent/flatfull/jscore/contacts/lhs-filters.js
@@ -185,7 +185,7 @@ $('#' + container_id).on('click', 'a.clear-filter-condition-lhs', function(e)
 {
 	
 	$(this).addClass('hide');
-	var container = $(this).parents('.lhs-row-filter');
+	var container = $(this).parents('.lhs-contact-filter-row');
 	$(container).find('#RHS:not(.no-filter-action)').children().val("").attr('prev-val', "");
 	$(container).find('#RHS').children().val("").attr('prev-val', "");
 	$(container).find('#RHS_NEW').filter(visibleFilter).children().val("").attr('prev-val', "");
