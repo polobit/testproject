f246d9d61ed83924641ed8e83b6400dad7834b32
graut <ghanshyam.agile@gmail.com>
2016-03-02 23:46:05 +0530

    Removed wrapper
    Removed wrapper to resolve issue

--- agile-core-classes/src/com/agilecrm/filter/BasicAuthFilter.java
+++ agile-core-classes/src/com/agilecrm/filter/BasicAuthFilter.java
@@ -1,21 +1,14 @@
 package com.agilecrm.filter;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.codec.binary.Base64;
@@ -120,17 +113,6 @@ public class BasicAuthFilter implements Filter
 		    // given access
 		    if (isValidPassword(password, domainUser) || isValidAPIKey(password, domainUser))
 		    {
-			BufferedRequestWrapper bufferedReqest = new BufferedRequestWrapper(httpRequest);
-			try
-			{
-			    System.out.println("Input data = " + bufferedReqest.getRequestBody());
-			    System.out.println("Path Info = " + httpRequest.getPathInfo());
-			    System.out.println("done");
-			}
-			catch (Exception e)
-			{
-			    System.out.println("This is for log issue handle");
-			}
 			try
 			{
 			    setUser(domainUser);
@@ -139,7 +121,9 @@ public class BasicAuthFilter implements Filter
 			}
 			catch (Exception e)
 			{
-			    System.err.println("Error");
+			    System.out.println("Error");
+
+			    System.out.println(e.getMessage());
 
 			    JSONObject address = new JSONObject();
 
@@ -150,6 +134,7 @@ public class BasicAuthFilter implements Filter
 			    httpResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 
 			    httpResponse.getWriter().write(address.toString());
+			    System.err.println("This code has below error : ");
 			    e.printStackTrace();
 			    return;
 			}
@@ -228,86 +213,6 @@ public class BasicAuthFilter implements Filter
 	    return true;
 
 	return false;
-    }
-
-    private static final class BufferedRequestWrapper extends HttpServletRequestWrapper
-    {
-
-	private ByteArrayInputStream bais = null;
-	private ByteArrayOutputStream baos = null;
-	private BufferedServletInputStream bsis = null;
-	private byte[] buffer = null;
-
-	public BufferedRequestWrapper(HttpServletRequest req) throws IOException
-	{
-	    super(req);
-	    // Read InputStream and store its content in a buffer.
-	    InputStream is = req.getInputStream();
-	    this.baos = new ByteArrayOutputStream();
-	    byte buf[] = new byte[1024];
-	    int letti;
-	    while ((letti = is.read(buf)) > 0)
-	    {
-		this.baos.write(buf, 0, letti);
-	    }
-	    this.buffer = this.baos.toByteArray();
-	}
-
-	@Override
-	public ServletInputStream getInputStream()
-	{
-	    this.bais = new ByteArrayInputStream(this.buffer);
-	    this.bsis = new BufferedServletInputStream(this.bais);
-	    return this.bsis;
-	}
-
-	String getRequestBody() throws IOException
-	{
-	    BufferedReader reader = new BufferedReader(new InputStreamReader(this.getInputStream()));
-	    String line = null;
-	    StringBuilder inputBuffer = new StringBuilder();
-	    do
-	    {
-		line = reader.readLine();
-		if (null != line)
-		{
-		    inputBuffer.append(line.trim());
-		}
-	    } while (line != null);
-	    reader.close();
-	    return inputBuffer.toString().trim();
-	}
-
-    }
-
-    private static final class BufferedServletInputStream extends ServletInputStream
-    {
-
-	private ByteArrayInputStream bais;
-
-	public BufferedServletInputStream(ByteArrayInputStream bais)
-	{
-	    this.bais = bais;
-	}
-
-	@Override
-	public int available()
-	{
-	    return this.bais.available();
-	}
-
-	@Override
-	public int read()
-	{
-	    return this.bais.read();
-	}
-
-	@Override
-	public int read(byte[] buf, int off, int len)
-	{
-	    return this.bais.read(buf, off, len);
-	}
-
     }
 
 }
