73c7876eb2766bc660d285c23d7b7a26cf3e90f1
premnathtammina <premtammina@gmail.com>
2016-03-08 16:06:49 +0530

    Uservoice configuration updated

--- agile-core-classes/src/com/agilecrm/widgets/util/DefaultWidgets.java
+++ agile-core-classes/src/com/agilecrm/widgets/util/DefaultWidgets.java
@@ -84,6 +84,11 @@ public class DefaultWidgets {
 				"/widgets/facebook.js", "/widgets/facebook.png",
 				"/widgets/Fb24x24icon.png", null, WidgetType.SOCIAL));
 		widgets.add(new Widget(
+				"Uservoice",
+				"UserVoice makes Product Management & Customer Support Software to help companies build better products and support their users. We think helping you create happy users who love your products is big business.",
+				"/widgets/uservoice.js", "/widgets/Uservoice_lg_logo.png",
+				"/widgets/UserVoice_md_logo.png", null, WidgetType.SUPPORT));
+		widgets.add(new Widget(
 				"Shopify",
 				"Shopify is a powerful ecommerce solution that includes everything you need to create an online store.",
 				"/widgets/shopify.js", "/widgets/shopify.png",
