fdd6f8fd3bbf30fa3014e96ad085e8297157620d
dharmakolla <dharmateja@agilecrm.com>
2016-03-10 14:40:50 +0530

    Mandrill hard/soft bounce related changes

--- agile-core-classes/src/com/thirdparty/mandrill/MandrillSetBounceStatusDeferredTask.java
+++ agile-core-classes/src/com/thirdparty/mandrill/MandrillSetBounceStatusDeferredTask.java
@@ -1,48 +1,27 @@
-package com.thirdparty.mandrill.webhook;
+package com.thirdparty.mandrill;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.lang.StringUtils;
-import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.agilecrm.AgileQueues;
 import com.agilecrm.contact.Contact;
 import com.agilecrm.contact.email.bounce.EmailBounceStatus;
 import com.agilecrm.contact.email.bounce.EmailBounceStatus.EmailBounceType;
 import com.agilecrm.contact.util.ContactUtil;
-import com.agilecrm.queues.util.PullQueueUtil;
 import com.agilecrm.workflows.triggers.Trigger;
 import com.agilecrm.workflows.triggers.util.EmailBounceTriggerUtil;
-import com.campaignio.cron.Cron;
-import com.campaignio.cron.deferred.CronDeferredTask;
 import com.campaignio.logger.Log.LogType;
 import com.campaignio.logger.util.LogUtil;
 import com.google.appengine.api.NamespaceManager;
-import com.google.appengine.api.taskqueue.Queue;
-import com.google.appengine.api.taskqueue.QueueFactory;
-import com.google.appengine.api.taskqueue.TaskOptions;
-import com.thirdparty.mandrill.MandrillSetBounceStatusDeferredTask;
+import com.google.appengine.api.taskqueue.DeferredTask;
 import com.thirdparty.mandrill.subaccounts.MandrillSubAccounts;
 
-/**
- * <code>MandrillWebhook</code> is the webhook servlet to handle hard bounce and
- * soft bounces
- * 
- * @author naresh
- * 
- */
 @SuppressWarnings("serial")
-public class MandrillWebhook extends HttpServlet
-{
-
-    public static final String MANDRILL_EVENTS = "mandrill_events";
-
-    public static final String EVENT = "event";
+public class MandrillSetBounceStatusDeferredTask implements DeferredTask {
+	
+	public static final String EVENT = "event";
     public static final String HARD_BOUNCE = "hard_bounce";
     public static final String SOFT_BOUNCE = "soft_bounce";
     public static final String SPAM = "spam";
@@ -51,119 +30,85 @@ public class MandrillWebhook extends HttpServlet
     public static final String EMAIL = "email";
     public static final String SUBACCOUNT = "subaccount";
     public static final String SUBJECT = "subject";
-
     public static final String METADATA = "metadata";
     public static final String METADATA_CAMPAIGN_ID = "campaign_id";
+    
+	/**
+	 * Event JSON.
+	 */
+	JSONObject eventJSON;
 
-    public void service(HttpServletRequest req, HttpServletResponse res)
-    {
-	try
-	{
-	    String mandrillEvents = req.getParameter(MANDRILL_EVENTS);
-
-	    String event = "";
-
-	    if (StringUtils.isBlank(mandrillEvents))
-		return;
-
-	    JSONArray events = new JSONArray(mandrillEvents);
-
-	    for (int i = 0, len = events.length(); i < len; i++)
-	    {
-		JSONObject eventJSON = events.getJSONObject(i);
-
-		event = eventJSON.getString(EVENT);
-
-		System.out.println("Mandrill webhook event is " + event);
-
-		// Set to contact if event is HardBounce or SoftBounce
-		if (StringUtils.equalsIgnoreCase(event, HARD_BOUNCE)
-		        || StringUtils.equalsIgnoreCase(event, SOFT_BOUNCE)
-		        || StringUtils.equalsIgnoreCase(event, SPAM)){
-		    //setBounceStatusToContact(eventJSON);
-		MandrillSetBounceStatusDeferredTask mandrillSetBounceStatusDeferredTask = new MandrillSetBounceStatusDeferredTask(eventJSON.toString());
-
-		
-			// Interruptted crons like Click, Open
-			Queue queue = QueueFactory.getQueue(AgileQueues.MANDRILL_QUEUE);
-			queue.add(TaskOptions.Builder.withPayload(mandrillSetBounceStatusDeferredTask));
+	public MandrillSetBounceStatusDeferredTask(String eventJSON){
+		try {
+			this.eventJSON = new JSONObject(eventJSON);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-	    }
-	}
-	catch (Exception e)
-	{
-	    System.err.println("Exception occured in Mandrill Webhook post..." + e.getMessage());
-	    e.printStackTrace();
-	}
-    }
-
-    /**
-     * Sets bounce state for a contact within namespace
-     * 
-     * @param eventJSON
-     *            webhook event
-     */
-    private void setBounceStatusToContact(JSONObject eventJSON)
-    {
-	String oldNamespace = NamespaceManager.get();
-	JSONObject metadata = null;
-	String subject = null;
-
-	try
-	{
-	    if (!eventJSON.has(MSG))
-		return;
-
-	    JSONObject msgJSON = eventJSON.getJSONObject(MSG);
-
-	    // If no subaccount or email, return
-	    if (!msgJSON.has(SUBACCOUNT) || !msgJSON.has(EMAIL))
-		return;
-
-	    // Return if empty namespace
-	    if (StringUtils.isBlank(msgJSON.getString(SUBACCOUNT)))
-		return;
-
-	    NamespaceManager.set(msgJSON.getString(SUBACCOUNT));
-
-	    // Get mandrill metadata
-	    if (msgJSON.has(METADATA))
-		metadata = msgJSON.getJSONObject(METADATA);
-
-	    // Get email subject
-	    if (msgJSON.has(SUBJECT))
-		subject = msgJSON.getString(SUBJECT);
-
-	    // By default SOFT_BOUNCE
-	    EmailBounceType type = EmailBounceType.SOFT_BOUNCE;
-
-	    if (HARD_BOUNCE.equals(eventJSON.getString(EVENT)))
-		type = EmailBounceType.HARD_BOUNCE;
-
-	    if (SPAM.equals(eventJSON.getString(EVENT)))
-		type = EmailBounceType.SPAM;
-
-	    // Set status to Agile Contact
-	    setContactEmailBounceStatus(msgJSON.getString(EMAIL), subject, type, metadata);
-	    
-	    // If spam, verify reputation
-	    if(type.equals(EmailBounceType.SPAM))
-		MandrillSubAccounts.checkReputation(msgJSON.getString(SUBACCOUNT));
-
-	}
-	catch (Exception e)
-	{
-	    System.err.println("Exception occured while setting email bounce status..." + e.getMessage());
-	    e.printStackTrace();
-	}
-	finally
-	{
-	    NamespaceManager.set(oldNamespace);
 	}
 
-    }
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		String oldNamespace = NamespaceManager.get();
+		JSONObject metadata = null;
+		String subject = null;
 
-    /**
+		try
+		{
+		    if (!eventJSON.has(MSG))
+			return;
+
+		    JSONObject msgJSON = eventJSON.getJSONObject(MSG);
+
+		    // If no subaccount or email, return
+		    if (!msgJSON.has(SUBACCOUNT) || !msgJSON.has(EMAIL))
+			return;
+
+		    // Return if empty namespace
+		    if (StringUtils.isBlank(msgJSON.getString(SUBACCOUNT)))
+			return;
+
+		    NamespaceManager.set(msgJSON.getString(SUBACCOUNT));
+
+		    // Get mandrill metadata
+		    if (msgJSON.has(METADATA))
+			metadata = msgJSON.getJSONObject(METADATA);
+
+		    // Get email subject
+		    if (msgJSON.has(SUBJECT))
+			subject = msgJSON.getString(SUBJECT);
+
+		    // By default SOFT_BOUNCE
+		    EmailBounceType type = EmailBounceType.SOFT_BOUNCE;
+
+		    if (HARD_BOUNCE.equals(eventJSON.getString(EVENT)))
+			type = EmailBounceType.HARD_BOUNCE;
+
+		    if (SPAM.equals(eventJSON.getString(EVENT)))
+			type = EmailBounceType.SPAM;
+
+		    // Set status to Agile Contact
+		    setContactEmailBounceStatus(msgJSON.getString(EMAIL), subject, type, metadata);
+		    
+		    // If spam, verify reputation
+		    if(type.equals(EmailBounceType.SPAM))
+			MandrillSubAccounts.checkReputation(msgJSON.getString(SUBACCOUNT));
+
+		}
+		catch (Exception e)
+		{
+		    System.err.println("Exception occured while setting email bounce status..." + e.getMessage());
+		    e.printStackTrace();
+		}
+		finally
+		{
+		    NamespaceManager.set(oldNamespace);
+		}
+		
+	}
+	
+	/**
      * Set bounce status to contact having obtained email
      * 
      * @param email
@@ -234,7 +179,7 @@ public class MandrillWebhook extends HttpServlet
 	    e.printStackTrace();
 	}
     }
-
+    
     /**
      * Sets bounce log
      * 
