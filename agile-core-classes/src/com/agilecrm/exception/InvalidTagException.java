0fcab664d07b7a0e13ae87aea2837aa641e73930
sundarsarma <gkssarma59@gmail.com>
2016-03-17 12:07:56 +0530

    wrongmessage_intags

--- agile-core-classes/src/com/agilecrm/exception/InvalidTagException.java
+++ agile-core-classes/src/com/agilecrm/exception/InvalidTagException.java
@@ -8,7 +8,7 @@ public class InvalidTagException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public InvalidTagException() {
-		super("Tags should not contain any special characters other than underscore, space and hypen");
+		super("Tags should not start with numbers and should not contain special characters except space and underscore.");
 	}
 
 	public InvalidTagException(String message) {
