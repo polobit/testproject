2b27cfe3caa1b2ea4330642fafd8fa924e968e96
dharmakolla <dharmateja@agilecrm.com>
2016-03-22 12:44:28 +0530

    Update deal changes

--- agile-core-classes/src/com/agilecrm/Globals.java
+++ agile-core-classes/src/com/agilecrm/Globals.java
@@ -94,7 +94,7 @@ public class Globals
     public static final String DEV_STRIPE_CLIENT_ID = STRIPE_LIVE_CLIENT_ID;
 
     // Stripe live keys
-    public static final String STRIPE_API_KEY = STRIPE_TEST_API_KEY;
+    public static final String STRIPE_API_KEY = STRIPE_LIVE_API_KEY;
     public static final String STRIPE_CLIENT_ID = STRIPE_LIVE_CLIENT_ID;
 
     // Xero live keys
