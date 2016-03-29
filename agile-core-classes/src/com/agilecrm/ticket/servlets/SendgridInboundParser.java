3ef48aa2c998ec088e61354885c6b1c6eccb9b46
= <=>
2016-03-29 15:39:38 +0530

    Ticketing: Fixed parsing issues

--- agile-core-classes/src/com/agilecrm/ticket/servlets/SendgridInboundParser.java
+++ agile-core-classes/src/com/agilecrm/ticket/servlets/SendgridInboundParser.java
@@ -1,11 +1,8 @@
 package com.agilecrm.ticket.servlets;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.Enumeration;
-import java.util.Iterator;
 import java.util.Properties;
 
 import javax.mail.Header;
@@ -16,12 +13,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.fileupload.FileItemIterator;
-import org.apache.commons.fileupload.FileItemStream;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.exception.ExceptionUtils;
-import org.json.JSONObject;
+
 
 public class SendgridInboundParser extends HttpServlet
 {
@@ -48,13 +41,15 @@ public class SendgridInboundParser extends HttpServlet
 
 			// Reading the input Mail
 			MimeMessage message = new MimeMessage(session, request.getInputStream());
+			MimeMessageParser mmp = new MimeMessageParser(message).parse();
+			
+			System.out.println("mmp.getFrom()..." + mmp.getFrom());
+			System.out.println("mmp.getSubject()..." + mmp.getSubject());
+			System.out.println("mmp.getTo()..." + mmp.getTo());
+			System.out.println("mmp.getPlainContent()..." + mmp.getPlainContent());
 
 			Enumeration headers = message.getAllHeaders();
 			System.out.println("Message Headers: \r\n");
-			
-			System.out.println("getFrom..." + message.getFrom());
-			System.out.println("getContentType..." + message.getContentType());
-			System.out.println("getContent..." + message.getContent());
 			
 			while (headers.hasMoreElements())
 			{
