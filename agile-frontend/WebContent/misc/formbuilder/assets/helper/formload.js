85592bf9698c44652178449b7a19ef686d93277e
santhosh0101 <santhoshkumar@agilecrm.com>
2016-03-31 16:33:48 +0530

    form custom fields

--- agile-frontend/WebContent/misc/formbuilder/assets/helper/formload.js
+++ agile-frontend/WebContent/misc/formbuilder/assets/helper/formload.js
@@ -28,19 +28,54 @@ define([
 			    		  }
 			    	  }
 				} else {
-					$.getJSON( "/core/api/custom-fields", function(fields) { 
-					for ( var j = 0; j < fields.length; j++)
+					$.getJSON( "/core/api/custom-fields", function(fields) {
+
+					for ( var j = 0; j < saveform.length; j++){
+
+							if(saveform[j].fields.agilefield){							
+							
+							var field = saveform[j].fields.agilefield.value;
+							
+							var agileFields = field.slice(0,15);
+							
+							if(field.length>15){
+								for(var k=field.length-1; k>=15; k--){
+									if(field[k].selected)
+										agileFields.push(field[k]);
+								}
+							}								
+					
+					if(fields.length != 0)
+					{ 
+					for ( var i = 0; i < fields.length; i++)
 						{
 							var value = {};
-							value.value = fields[j].field_label;
-							value.label = fields[j].field_label;
+							value.value = fields[i].field_label;
+							value.label = fields[i].field_label;
 							value.selected = false;
-							for ( var i = 0; i < saveform.length; i++)
-						{
-							if(saveform[i].fields.agilefield)
-								saveform[i].fields.agilefield.value.push(value);
+							var count = 0;
+							for(var k=agileFields.length-1; k>=15; k--){
+								if(value.label == agileFields[k].label)
+									count++;								
+							}
+							if(count == 0)
+								agileFields.push(value);
+						}
+						saveform[j].fields.agilefield.value = agileFields;
+					}else{
+						for ( var i = 0; i < saveform.length; i++){
+							
+							if(saveform[i].fields.agilefield){		
+							var field = saveform[i].fields.agilefield.value;							
+							if(field.length>15){
+								for(var j=field.length; j>15; j--)
+									field.pop(field[j]);								
+							}
 						}
 					}
+				}
+			}
+		}
 					$('#form-label').text('Edit Form');
 					new MyFormView({ title : "Original", collection : new MyFormSnippetsCollection(saveform) });				
 				});
