5e51caed0caea8f35458505ffb064e26558102b0
rameshrudra <ramesh@invox.com>
2016-04-01 18:32:31 +0530

    selecting distinct emails in segmentation query

--- agile-frontend/WebContent/stats/js/agile_trackpageview.js
+++ agile-frontend/WebContent/stats/js/agile_trackpageview.js
@@ -46,7 +46,7 @@ function agile_trackPageview(callback)
 	// Sets UTM params
 	agile_setUtmParams();	
 
-	var agile_url = "https://" + agile_id.getNamespace() + ".agilecrm.com/stats?callback=?&" + params;
+//	var agile_url = "https://" + agile_id.getNamespace() + ".agilecrm.com/stats?callback=?&" + params;
 	var agile_url_new =  Track_Visitor_Server_URL + "/addstats?callback=?&" + params;
 
 	// Callback
