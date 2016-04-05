76889bd1d6b9e15c7690f33376472a04594a741b
nareshagile <naresh@faxdesk.com>
2016-04-05 13:03:14 +0530

    Added method for getting sendgrid statistics

--- agile-core-classes/src/com/thirdparty/sendgrid/subusers/SendGridSubUser.java
+++ agile-core-classes/src/com/thirdparty/sendgrid/subusers/SendGridSubUser.java
@@ -1,6 +1,7 @@
 package com.thirdparty.sendgrid.subusers;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,8 +12,12 @@ import org.apache.http.HttpEntity;
 import org.apache.http.entity.StringEntity;
 import org.json.JSONObject;
 
+import com.agilecrm.Globals;
+import com.agilecrm.account.EmailGateway;
 import com.agilecrm.util.Base64Encoder;
+import com.agilecrm.util.HTTPUtil;
 import com.agilecrm.util.HttpClientUtil;
+import com.campaignio.reports.DateUtil;
 import com.thirdparty.sendgrid.lib.SendGridLib;
 
 public class SendGridSubUser extends SendGridLib
@@ -144,4 +149,56 @@ public class SendGridSubUser extends SendGridLib
 		throw new IllegalArgumentException("Domain " + domain + " is not valid.");
 	}
 	
+	public static String getSubUserStatistics(String domain, EmailGateway gateway)
+	{
+		String response = null;
+		
+		try
+		{
+			if (StringUtils.isBlank(domain))
+				return null;
+			
+			String username = null, password = null;
+			
+			if (gateway != null)
+			{
+				username = gateway.api_user;
+				password = gateway.api_key;
+			}
+			
+			if(username == null || password == null)
+			{
+				username = Globals.SENDGRID_API_USER_NAME;
+				password = Globals.SENDGRID_API_KEY;
+			}
+			
+			long timestamp = System.currentTimeMillis();
+
+			String queryString = "subusers"
+					+ "="
+					+ URLEncoder.encode(username, "UTF-8")
+					+ "&"
+					+ "start_date"
+					+ "="
+					+ URLEncoder.encode(DateUtil.getDateInGivenFormat(timestamp - (30 * 24 * 60 * 60 * 1000l), "YYYY-MM-dd", null), "UTF-8")
+					+ "&"
+					+ "end_date"
+					+ "="
+					+ URLEncoder.encode(DateUtil.getDateInGivenFormat(timestamp, "YYYY-MM-dd", null)
+							,
+							"UTF-8")
+					+"&"+ "aggregated_by" + "=" + URLEncoder.encode("month", "UTF-8");
+			
+			response = HTTPUtil.accessURLUsingAuthentication("https://api.sendgrid.com/v3/subusers/stats", "agilecrm1", "send@agile1",
+					"GET", queryString, false, null, "application/json");
+		}
+		catch (Exception e)
+		{
+			// TODO: handle exception
+			e.printStackTrace();
+			System.out.println("Exception occured...." + e.getMessage());
+		}
+		
+		return response;
+	}
 }
