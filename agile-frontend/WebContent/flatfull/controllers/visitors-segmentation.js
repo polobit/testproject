817a3fd6d10eebf5280e835b8d2f19c1c2331686
sonisonali <sonali@agilecrm.com>
2016-04-06 14:10:32 +0530

    modeltemplate for segments

--- agile-frontend/WebContent/flatfull/controllers/visitors-segmentation.js
+++ agile-frontend/WebContent/flatfull/controllers/visitors-segmentation.js
@@ -21,42 +21,8 @@ var VisitorsSegmentationRouter = Backbone.Router
             if (VISITORS_HARD_RELOAD == true) {
                 this.webstatsListView = undefined;
                 VISITORS_HARD_RELOAD = false;
-                view_data = undefined;
-
             }
-
-            // If id is definesd get the respective custom view object
-            if (!view_data) {
-                // Once view id fetched we use it without fetching it.
-                if (!App_VisitorsSegmentation.webstatsViewModel) {
-                    var view = new Backbone.Model();
-                    view.url = 'core/api/contact-view-prefs';
-                    view
-                        .fetch({
-                            success: function(data) {
-
-                                if ($.isEmptyObject(data.toJSON())) {
-                                    console.log("no data found");
-                                    return;
-                                }
-                                App_VisitorsSegmentation.webstatsViewModel = data
-                                    .toJSON();
-                                App_VisitorsSegmentation
-                                    .visitorssegmentation(
-                                        undefined,
-                                        is_lhs_filter,
-                                        App_VisitorsSegmentation.webstatsViewModel);
-
-                            }
-                        });
-                    return;
-                }
-                if(App_Contacts.contactViewModel)
-                    App_VisitorsSegmentation.webstatsViewModel.fields_set=App_Contacts.contactViewModel.fields_set;
-                view_data = App_VisitorsSegmentation.webstatsViewModel;
-
-            }
-
+           
             var template_key = "segmentation-custom-view";
 
             // Default url for contacts route
@@ -70,7 +36,7 @@ var VisitorsSegmentationRouter = Backbone.Router
                 $('#content').html(el);
                 initWebstatsDateRange();
                 contactFiltersListeners("lhs_filters_segmentation");
-                addClickEventSEgments($('#content').find("#visitors-table"));
+                //addClickEventSEgments($('#content').find("#visitors-table"));
 
                 $(".active").removeClass("active");
                 $("#segmentationmenu").addClass("active");
@@ -120,7 +86,6 @@ var VisitorsSegmentationRouter = Backbone.Router
                             url: url,
                             restKey: "contact",
                             templateKey: template_key,
-                            modelData: view_data,
                             individual_tag_name: 'tr',
                             slateKey: slateKey,
                             cursor: true,
@@ -139,8 +104,7 @@ var VisitorsSegmentationRouter = Backbone.Router
                                 el, collection) {
 
                                 abortCountQueryCall();
-                                addClickEventSEgments($('#content').find("#visitors-table"));
-                                
+                                                                
                                if(collection.models.length==0)
                                      $("#visitors-count").html("<small> (" + 0 + " Total) </small>");
                                 else if(collection.models[collection.models.length-1].attributes.count){
@@ -159,13 +123,7 @@ var VisitorsSegmentationRouter = Backbone.Router
                             }
                         });
 
-                        // Defines appendItem for custom view
-                        web_scope.webstatsListView.appendItem = function(
-                            base_model) {
-                            SegmentsTableView(base_model,web_scope.webstatsListView);
-                        };
-
-                        // Fetch collection
+                       // Fetch collection
                         web_scope.webstatsListView.collection
                             .fetch();
 
@@ -190,30 +148,4 @@ var VisitorsSegmentationRouter = Backbone.Router
 
     });
 
-
-function addClickEventSEgments(element){
-    $(element).on('click', '.agile-edit-row > tr > td:not(".checkbox")',
-            function(e) {
-        e.preventDefault();
-        //alert("click events");
-        var route = $(this).parents('.agile-edit-row').attr('route');
-        
-        // Newly added code for displaying contacts and companies in same table with different routes.
-        if($(this).closest('tr').find('[route]').length != 0)
-            route = $(this).closest('tr').find('[route]').attr('route');
-        
-        var data = $(this).closest('tr').find('.data').attr('data');
-
-        if(route == "contact/")
-            SCROLL_POSITION = window.pageYOffset;               
-
-                console.log(data);
-
-                if (data) {
-                    Backbone.history.navigate(route + data, {
-                        trigger : true
-                    });
-                }
-            });
-}
 
