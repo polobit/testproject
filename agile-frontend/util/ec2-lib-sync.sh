2925e74288edc6bfeecb55240f627f20737c3928
yaswanth.agile <yaswanth@agilecrm.com>
2016-04-13 12:52:52 +0530

    Ec2 lib sync

--- agile-frontend/util/ec2-lib-sync.sh
+++ agile-frontend/util/ec2-lib-sync.sh
@@ -3,7 +3,7 @@
 if [ "$2" = true ] ; then
 	scp -i $EC2_PERMISSIONS -r ../WebContent/jscore/min/flatfull/ ec2-user@ec2-54-210-171-176.compute-1.amazonaws.com:~/cdn/app/$1/jscore/min/flatfull/
 	scp -i $EC2_PERMISSIONS -r ../WebContent/tpl/min/precompiled/flatfull/ ec2-user@ec2-54-210-171-176.compute-1.amazonaws.com:~/cdn/app/$1/tpl/min/precompiled/
-	echo "Updated production version : $2"
+	echo "Updated production version : $1"
 else
 	echo "Beta update"
 	scp -i $EC2_PERMISSIONS -r ../WebContent/jscore/min/flatfull/ ec2-user@ec2-54-210-171-176.compute-1.amazonaws.com:~/cdn/beta/$1/jscore/min/
