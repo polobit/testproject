dfbb3e1e4712c650c81d7f88663b147b24f3fb89
sreedevigadiyaaram <sreedevi.agilecrm@gmail.com>
2016-05-02 12:14:08 +0530

    Helpcenter:Adding helpcenter events file

---
+++ agile-frontend/WebContent/flatfull/jscore/helpcenter/helpcenter-events.js
@@ -0,0 +1,65 @@
+var Helcenter_Events = {
+
+	initializeStatuscheckbox : function(el){
+   		
+
+   		$(".article_status",el).off();	
+   		$(".article_status",el).on("change" ,function(){
+              
+         $('[data-toggle="tooltip"]', el).tooltip();
+             //console.log("xyz"); 
+            var msg = "Do you want to publish the article";
+            var article_id = $(this,el).closest("td").data("id");   
+            var flag = $( "."+article_id ).is(":checked");
+            
+            if(!flag)
+            	msg = "Do you want to save the article as draft";     
+
+   				var data = true;
+
+                   if(!flag)
+                      data = false; 
+                    
+               showModalConfirmation("Status", msg, function(){
+   					
+                  if(!App_Helpcenter_Module.articlesCollection)
+                        return;
+                
+                  var newArticleModel = new BaseModel();
+                 
+                  newArticleModel.url = "/core/api/knowledgebase/article/update-status/" +article_id;
+               
+                  var json = {} ;
+                   
+                  json = {"id":article_id,"is_article_published":flag };
+
+                  newArticleModel.save(json, {
+                    
+                     success: function(model){
+
+                        if(App_Helpcenter_Module.articlesCollection.collection){
+                           
+                             // Get data from collection with id
+                              var model = App_Helpcenter_Module.articlesCollection.collection.get(article_id);
+
+                              //Update data in model
+                              model.set({"id":article_id,"is_article_published":data}, {silent: true});
+                        }
+                           
+                     }
+
+               });
+                  
+   				}, function(){
+                    
+                     if(!flag)
+                     $("."+article_id,el).prop('checked','true');
+                     else   
+                     $("."+article_id,el).removeAttr('checked');  
+                                    
+               },null);
+   		});
+	}
+
+
+};
