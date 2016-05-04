e1850724696ede0fafd9e766331f7cd300fc2390
santhoshmogulla <mogulla@invox.com>
2016-05-04 17:52:28 +0530

    removed phone number validation

--- agile-frontend/WebContent/stats/js/agile_forms_validation.js
+++ agile-frontend/WebContent/stats/js/agile_forms_validation.js
@@ -43,22 +43,6 @@ function agile_validations(){
  							continue;
  						}	
  					}
-
- 					if(agile_form[i].name == "phone"){
-						
-						if(validatePhonenumber(inputNode.value)){
-							document.getElementById("agile_span"+i).remove(); 
-		 					isValid = true;
-		 					continue;
-						}
- 						else {
- 							document.getElementById("agile_span"+i).innerHTML = "Please enter valid phone number (+xxxxxxxxxx)";
- 							count++;
- 							continue;
- 						}	
- 					}
-	
-
 	
 
 					//other fields if have value 
@@ -87,26 +71,7 @@ function agile_validations(){
 						continue;
  						}
 
- 					}	
- 					// phone number validations
-					if(agile_form[i].name == "phone"){
-
-					if(validatePhonenumber(inputNode.value)){ 
-		 					isValid = true;
-		 					continue;
-						}
- 						else{
-	 						var spanTag = document.createElement("span");
-							spanTag.innerHTML = "Please enter valid phone number (+xxxxxxxxxx)";
-							spanTag.id = "agile_span"+i;
-							spanTag.style.color = "red";
-							spanTag.style.fontSize = "12px";
-							inputNode.parentNode.insertBefore(spanTag,inputNode.nextSibling);
-							count++;    //if span created then we will increase by one
-							continue;
- 						}
-
- 					}	
+ 					}		
 		}
 
 		else if(inputNode.value == "" && spans){ //if field having spans not a value
@@ -135,14 +100,3 @@ function validateEmail(email){
  			else
  				return false;
 }
-
-function validatePhonenumber(str)  
-{  
-	var pattern=/^((\+)(\d)+)$/;
-
-	if(pattern.test(str))
-		return true;	
-	else
-		return false;
-}
-
