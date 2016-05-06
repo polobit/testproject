c78671fd1c08fb256a5cb4a96aa9f0ddd6f1eea9
Govind Chunchula <govind@invox.com>
2016-05-06 15:27:13 +0530

    Send otp email from appengine

--- agile-core-classes/src/com/agilecrm/LoginServlet.java
+++ agile-core-classes/src/com/agilecrm/LoginServlet.java
@@ -359,10 +359,10 @@ public class LoginServlet extends HttpServlet {
 				subject =  "New sign-in from " + data.get("browser_name") + " on " + data.get("browser_os"); 
 			}
 			
-			if(resend)
+			// if(resend)
 				AppengineMail.sendMail(email, subject, template, data);
-			else 
-				SendMail.sendMail(email, subject, template, data);
+			// else 
+				// SendMail.sendMail(email, subject, template, data);
 		}
 		catch (Exception e) {
 			e.printStackTrace();
