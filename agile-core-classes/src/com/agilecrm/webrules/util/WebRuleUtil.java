2a3458a8e07bf9ff3b8fa77b3e211e1317904793
Govind Chunchula <govind@invox.com>
2016-05-17 16:26:21 +0530

    Webrules cache

--- agile-core-classes/src/com/agilecrm/webrules/util/WebRuleUtil.java
+++ agile-core-classes/src/com/agilecrm/webrules/util/WebRuleUtil.java
@@ -3,21 +3,58 @@ package com.agilecrm.webrules.util;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.agilecrm.contact.Contact;
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.agilecrm.util.CacheUtil;
 import com.agilecrm.webrules.WebRule;
-import com.googlecode.objectify.Key;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.google.appengine.api.NamespaceManager;
 
 public class WebRuleUtil
 {
+	// Cache key
+	public static final String CACHE_KEY = "webrules_cache";
+	
     public static List<WebRule> getAllWebRules()
     {
-	    return WebRule.dao.fetchAllByKeys(WebRule.dao.listKeysByProperty(new HashMap()));
+    	return getCachedWebRules();
     }
+    
+    // Get cached webrules
+    public static List<WebRule> getCachedWebRules(){
+    	
+    	String keyName = NamespaceManager.get() + "_" + CACHE_KEY;
+    	Object cachedRules = CacheUtil.getCache(keyName);
+    	ObjectMapper mapper = new ObjectMapper();
+    	List<WebRule> rulesList = null;
+    	
+    	try {
+    		if(cachedRules == null){
+        		rulesList = WebRule.dao.fetchAllByKeys(WebRule.dao.listKeysByProperty(new HashMap()));
+        		CacheUtil.setCache(keyName, mapper.writeValueAsString(rulesList));
+        	} else {
+        		rulesList = mapper.readValue(cachedRules.toString(), mapper.getTypeFactory().constructCollectionType(List.class, WebRule.class));
 
+        	}
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(ExceptionUtils.getFullStackTrace(e));
+		}
+    	
+    	
+	    return rulesList;
+    }
+    
+    // Delete rules from cache
+    public static void deleteRulesFromCache() {
+    	CacheUtil.deleteCache(NamespaceManager.get() + "_" + CACHE_KEY);
+    }
+    
+    
     //returns all webrule count 
     public static int getCount(){
     	
