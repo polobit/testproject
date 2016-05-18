88c4530bae1ebc19536f5f1c6f6ce41cd2b7627c
sankar <soma.savarala@gmail.com>
2016-05-18 19:59:32 +0530

    contact filter last contacted-never added in lhs and static filters

--- agile-frontend/WebContent/flatfull/jscore/form/deserialize.js
+++ agile-frontend/WebContent/flatfull/jscore/form/deserialize.js
@@ -409,9 +409,6 @@ function deserializeChainedSelect(form, el, el_self)
 
 function deserializeChainedElement(data, rule_element)
 {
-	if(data.CONDITION == "NOT_DEFINED"){
-		data.CONDITION = "NEVER" ;
-	}
 	$(rule_element).removeClass('hide');
 	// hide campaign status filter.
 	/*
