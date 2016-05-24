422048e21ead155486807b6aec068671b9828780
sundarsarma <gkssarma59@gmail.com>
2016-05-24 16:48:01 +0530

    removebordderindashlets

--- agile-frontend/WebContent/flatfull/lib/jquery.multi-select.js
+++ agile-frontend/WebContent/flatfull/lib/jquery.multi-select.js
@@ -43,7 +43,7 @@
             if ($(this).is('optgroup')){
               optgroupLabel = $(this).attr('label');
               optgroupId = 'ms-'+ms.attr('id')+'-optgroup-'+optgroupCpt;
-              selectableUl.append($('<li class="ms-optgroup-container" id="'+
+               selectableUl.append($('<li class="ms-optgroup-container" id="'+
                                   optgroupId+'"><ul class="ms-optgroup"><li class="ms-optgroup-label" style="padding-top: 0px;">'+
                                   optgroupLabel+'</li></ul></li>'));
               optgroupCpt++;
