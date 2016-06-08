f1892a6ff2104987f5bd23d97794deb23537cd0e
sonisonali <sonali@agilecrm.com>
2016-06-08 21:14:30 +0530

    images for log call node

--- agile-frontend/WebContent/json/nodes/crm/addons/call_node.js
+++ agile-frontend/WebContent/json/nodes/crm/addons/call_node.js
@@ -1,7 +1,7 @@
 {
     "name": "Call",
-    "thumbnail": "json/nodes/images/crm/territory.png",
-    "icon": "json/nodes/icons/email/score.png",
+    "thumbnail": "json/nodes/images/callnode.png",
+    "icon": "json/nodes/icons/sms/callnode.png",
     "info": "Call and Play an automated voice message",
     "help": "Play a voice message using Text",
     "path": "json/nodes/crm/call_node.js"
