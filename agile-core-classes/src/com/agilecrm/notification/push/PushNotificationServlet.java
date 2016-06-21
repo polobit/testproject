c3a46ac807db01cebc2d42a6cbdd41f1ebaf0bc1
prashannjeet-agile <prashannjeet@agilecrm.com>
2016-06-21 18:13:42 +0530

    push notification issue solved

--- agile-core-classes/src/com/agilecrm/notification/push/PushNotificationServlet.java
+++ agile-core-classes/src/com/agilecrm/notification/push/PushNotificationServlet.java
@@ -15,14 +15,21 @@ import com.campaignio.tasklets.agile.util.AgileTaskletUtil;
 
 public class PushNotificationServlet extends HttpServlet {
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1587951517703088575L;
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
 	    {
 		    String browser_id=request.getParameter("id");
+		    System.out.println("Browser id :"+browser_id);
 		    
 		    if(StringUtils.isEmpty(browser_id))
 		    	return;
 		    
 		    PushNotificationMessage pushNotificationMessage = PushNotificationMessageUtil.getPushNotificationMessage(browser_id);
+		    System.out.println("Message Created Time :"+pushNotificationMessage.created_time);
 		    
 		    if(pushNotificationMessage == null)
 		    	return;
@@ -36,7 +43,7 @@ public class PushNotificationServlet extends HttpServlet {
 		    
 		    //Adding push notification shown log in campaign
 		    LogUtil.addLogToSQL(campaign_id, subscriber_id, "Push notification showm to subscriber",LogType.PUSH_NOTIFICATION_FAILED.toString());
-		    
+		    response.setHeader("Access-Control-Allow-Origin", "*");
 		    response.setContentType("application/json");
 		    response.getWriter().print(message);
 		
