e625ee9b398fdf890b11d0401a5120f7cc6fadc7
srijaagilecrm <srijaagilecrm>
2016-06-22 16:22:10 +0530

    add filter to search and have to add logic to server

--- agile-frontend/WebContent/flatfull/jscore/flatfull/toggleMenu.js
+++ agile-frontend/WebContent/flatfull/jscore/flatfull/toggleMenu.js
@@ -292,6 +292,38 @@ $("#activityModal").on("click", "#eventDescriptionLink", function(e){
 	        }
 	    });
 	});
+
+	var options = [];
+
+	$( '#advanced-search-fields-group a' ).on( 'click', function( event ) {
+
+   	   var $target = $( event.currentTarget ),
+       val = $target.attr( 'data-value' ),
+       $inp = $target.find( 'input' ),
+       idx;
+	   if ( ( idx = options.indexOf( val ) ) > -1 ) {
+	      options.splice( idx, 1 );
+	      setTimeout( function() { $inp.prop( 'checked', false ) }, 0);
+	   } else {
+	      options.push( val );
+	      setTimeout( function() { $inp.prop( 'checked', true ) }, 0);
+	   }
+
+	   $( event.target ).blur();
+	      
+	   console.log("options="+options );
+	   var json = serializeForm("advanced-search-filter");
+	   console.log(json);
+	   return false;
+	   
+	});
+
+	$(".searchtypeall").on('click',function(e){
+		console.log("checked");
+		$(".searchtype").prop('checked', $(this).prop("checked"));
+	});
+
+
 	// initializing need help popover for header page
    $(".need_help").popover({ placement : 'left',
 					html:true,
