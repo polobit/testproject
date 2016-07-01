1eae831d93f50fb1fafe393b2faab5919dd9561c
santhoshmogulla <mogulla@invox.com>
2016-07-01 09:25:11 +0530

    commission for first payment

--- agile-frontend/WebContent/flatfull/controllers/subscribe.js
+++ agile-frontend/WebContent/flatfull/controllers/subscribe.js
@@ -514,6 +514,11 @@ var SubscribeRouter = Backbone.Router
 							},
 							saveCallback : function(data)
 							{
+								if(data && !USER_BILLING_PREFS.status){
+									var cus_id = JSON.parse(data.billingData).id;
+									var appendItem = '<img src="https://shareasale.com/sale.cfm?amount='+plan_json.cost+'&tracking='+cus_id+'&transtype=sale&merchantID=YOURMERCHANTID" width="1" height="1">'
+									$("body").append(appendItem);
+								}
 								window.navigate("subscribe", { trigger : true });
 								showNotyPopUp("information", "Your plan has been updated successfully", "top");
 
