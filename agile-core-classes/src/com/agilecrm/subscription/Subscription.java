083c9a9379a67f37f5bdd72e7a65298c0f3af5c7
santhoshmogulla <mogulla@invox.com>
2016-07-04 11:25:11 +0530

    subscription dynamic default plan

--- agile-core-classes/src/com/agilecrm/subscription/Subscription.java
+++ agile-core-classes/src/com/agilecrm/subscription/Subscription.java
@@ -13,6 +13,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 import org.codehaus.jackson.annotate.JsonIgnore;
 import org.codehaus.jettison.json.JSONObject;
 
+import com.agilecrm.Globals;
 import com.agilecrm.db.ObjectifyGenericDao;
 import com.agilecrm.subscription.limits.PlanLimits;
 import com.agilecrm.subscription.restrictions.db.BillingRestriction;
@@ -496,7 +497,7 @@ public class Subscription {
 	void PostLoad() {
 		try {
 			if (this.plan == null) {
-				plan = new Plan(PlanType.FREE.toString(), 2);
+				plan = new Plan(PlanType.FREE.toString(), Globals.TRIAL_USERS_COUNT);
 			}
 
 			planLimits = PlanLimits.getPlanDetails(plan);
