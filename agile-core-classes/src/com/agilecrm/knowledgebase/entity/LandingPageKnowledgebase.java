624a8042abd0739d4872ef5d8c06f8117de59cf2
sreedevigadiyaaram <sreedevi.agilecrm@gmail.com>
2016-07-08 12:23:45 +0530

    KB:loading issues

--- agile-core-classes/src/com/agilecrm/knowledgebase/entity/LandingPageKnowledgebase.java
+++ agile-core-classes/src/com/agilecrm/knowledgebase/entity/LandingPageKnowledgebase.java
@@ -3,6 +3,9 @@ package com.agilecrm.knowledgebase.entity;
 import javax.persistence.Id;
 
 import com.agilecrm.db.ObjectifyGenericDao;
+import com.agilecrm.landingpages.LandingPage;
+import com.agilecrm.landingpages.LandingPageUtil;
+
 import java.util.List;
 
 public class LandingPageKnowledgebase
@@ -17,9 +20,13 @@ public static ObjectifyGenericDao<LandingPageKnowledgebase> dao = new ObjectifyG
 
 public  Long getkbLandingpageid()
 {
-	try{
+	try{	
 	List<LandingPageKnowledgebase> landingpagekblist = LandingPageKnowledgebase.dao.fetchAll();
 	Long lpkbid =  landingpagekblist.get(0).kbLandingpageid;
+	LandingPage landingPage = LandingPageUtil.getLandingPage(lpkbid);
+	if(landingPage == null)
+		return null;
+	
 	return lpkbid;
 	}
 	catch(Exception e){
