48f88e8c82541f41d0a166ce396d5b23a0b66f6d
sankar <soma.savarala@gmail.com>
2016-07-11 10:06:20 +0530

    contacts elete in text search

--- agile-core-classes/src/com/agilecrm/DeleteContactsFromTextSearchServlet.java
+++ agile-core-classes/src/com/agilecrm/DeleteContactsFromTextSearchServlet.java
@@ -1,6 +1,7 @@
 package com.agilecrm;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,6 +11,7 @@ import com.agilecrm.search.AppengineSearch;
 import com.agilecrm.search.query.QueryDocument;
 import com.agilecrm.search.query.util.QueryDocumentUtil;
 import com.agilecrm.search.ui.serialize.SearchRule;
+import com.google.appengine.api.NamespaceManager;
 import com.google.appengine.api.search.ScoredDocument;
 import com.agilecrm.search.document.ContactDocument;
 
@@ -19,23 +21,28 @@ public class DeleteContactsFromTextSearchServlet {
 		doGet(req, res);
 	}
 
-	public void doGet(HttpServletRequest req, HttpServletResponse res)
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
 	{
-		AppengineSearch<Contact> search = new AppengineSearch<Contact>(Contact.class);
-		QueryDocument<Contact> q = new QueryDocument<Contact>(search.index, Contact.class);
-		ContactFilter cf = new ContactFilter();
-		SearchRule rule = new SearchRule();
-		rule.LHS = "type";
-		rule.CONDITION = SearchRule.RuleCondition.EQUALS;
-		rule.RHS = "PERSON";
-		cf.rules.add(rule);
-		String query = QueryDocumentUtil.constructFilterQuery(cf);
-		List <ScoredDocument> scoredDocs = q.getDocuments(query, null);
-		if(scoredDocs != null && scoredDocs.size() > 0)
-		{
-			for(ScoredDocument sd : scoredDocs)
+		String domain = request.getParameter("domain");
+		System.out.println("given domain is "+domain);
+		if(!domain.isEmpty() && !domain.equalsIgnoreCase(null) && !domain.equalsIgnoreCase("null")){
+			NamespaceManager.set(domain);
+			AppengineSearch<Contact> search = new AppengineSearch<Contact>(Contact.class);
+			QueryDocument<Contact> q = new QueryDocument<Contact>(search.index, Contact.class);
+			ContactFilter cf = new ContactFilter();
+			SearchRule rule = new SearchRule();
+			rule.LHS = "type";
+			rule.CONDITION = SearchRule.RuleCondition.EQUALS;
+			rule.RHS = "PERSON";
+			cf.rules.add(rule);
+			String query = QueryDocumentUtil.constructFilterQuery(cf);
+			List <ScoredDocument> scoredDocs = q.getDocuments(query, null);
+			if(scoredDocs != null && scoredDocs.size() > 0)
 			{
-				search.delete(sd.getId());
+				for(ScoredDocument sd : scoredDocs)
+				{
+					search.delete(sd.getId());
+				}
 			}
 		}
 	}
