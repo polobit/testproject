7d7aaa767352ed50b4f0678e96a11e528aed8b44
Purushotham <purushotham.agile@gmail.com>
2016-07-22 15:25:34 +0530

    Add video placeholder event in skeleton

--- agile-frontend/WebContent/misc/pagebuilder/source/elements/js/custom.js
+++ agile-frontend/WebContent/misc/pagebuilder/source/elements/js/custom.js
@@ -16,20 +16,4 @@ $(function(){
         $(this).append( overlay );
     });
 
-
-    $('.videoWrapper').on('click', function(e) {
-        e.preventDefault();
-        $currentEl = $(this);
-        var videoPlaceholder = $currentEl.find("img.video_placeholder");
-        var videoUrl = videoPlaceholder.attr('data-video');
-        var splitArr = videoUrl.split("?");
-        var queryStringArr = ["autoplay=1"];
-        if(typeof splitArr[1] != "undefined") {
-            queryStringArr = queryStringArr.concat(splitArr[1].split("&"));
-        }
-        var autoPlayVideoUrl = splitArr[0] + "?" + queryStringArr.join("&");
-        var videoIFrame = '<iframe src="' + autoPlayVideoUrl + '" frameborder="0" allowfullscreen></iframe>';
-        $currentEl.html(videoIFrame);
-    });
-
 });
