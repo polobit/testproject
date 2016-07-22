c4964eced17d0ae03423beb94cbea3411dad5ef3
Sri Gottipati <gsribhagavan@gmail.com>
2016-07-22 12:01:44 +0530

    Debug trace

--- agile-core-classes/src/com/agilecrm/search/document/ContactDocument.java
+++ agile-core-classes/src/com/agilecrm/search/document/ContactDocument.java
@@ -204,7 +204,15 @@ public class ContactDocument extends com.agilecrm.search.document.Document imple
 			
 			if(contact.campaignStatus != null && !contact.campaignStatus.isEmpty())
 			    doc.addField(Field.newBuilder().setName("campaign_status").setText(SearchUtil.getCampaignStatus(contact)));
-
+			//Trace points
+			try{
+				System.out.println("contact.id:" + contact.id.toString());
+				doc.addField(Field.newBuilder().setName("id_dup").setText(contact.id.toString()));
+			}catch(Exception e)
+			{
+			  System.out.println("Debug errror:" + e.getMessage());	
+			}
+			
 			doc.setId(contact.id.toString()).build();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
