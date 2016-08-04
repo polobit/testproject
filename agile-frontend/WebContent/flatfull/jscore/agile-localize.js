d31e68168756277b3a696d85df590662f896f848
Govind Chunchula <govind@invox.com>
2016-08-04 15:18:10 +0530

    js changes

--- agile-frontend/WebContent/flatfull/jscore/agile-localize.js
+++ agile-frontend/WebContent/flatfull/jscore/agile-localize.js
@@ -10,7 +10,9 @@ var _agile_month_short_names_localize = {
 // Date picker language settings
 $.fn.datepicker.dates['en'] = {
     days: ["{{agile_lng_translate 'prefs-settings' 'sunday'}}", "{{agile_lng_translate 'prefs-settings' 'monday'}}", "{{agile_lng_translate 'calendar' 'tuesday'}}", "{{agile_lng_translate 'calendar' 'wednesday'}}", "{{agile_lng_translate 'calendar' 'thursday'}}", "{{agile_lng_translate 'calendar' 'friday'}}", "{{agile_lng_translate 'calendar' 'saturday'}}", "{{agile_lng_translate 'prefs-settings' 'sunday'}}"],
+    daysExact: ["{{agile_lng_translate 'prefs-settings' 'sunday'}}", "{{agile_lng_translate 'prefs-settings' 'monday'}}", "{{agile_lng_translate 'calendar' 'tuesday'}}", "{{agile_lng_translate 'calendar' 'wednesday'}}", "{{agile_lng_translate 'calendar' 'thursday'}}", "{{agile_lng_translate 'calendar' 'friday'}}", "{{agile_lng_translate 'calendar' 'saturday'}}"],
     daysShort: ["{{agile_lng_translate 'prefs-online-calendar' 'Sun'}}", "{{agile_lng_translate 'prefs-online-calendar' 'Mon'}}", "{{agile_lng_translate 'prefs-online-calendar' 'Tue'}}", "{{agile_lng_translate 'prefs-online-calendar' 'Wed'}}", "{{agile_lng_translate 'prefs-online-calendar' 'Thu'}}", "{{agile_lng_translate 'prefs-online-calendar' 'Fri'}}", "{{agile_lng_translate 'prefs-online-calendar' 'Sat'}}", "{{agile_lng_translate 'prefs-online-calendar' 'Sun'}}"],
+    daysShortExact: ["{{agile_lng_translate 'prefs-online-calendar' 'Sun'}}", "{{agile_lng_translate 'prefs-online-calendar' 'Mon'}}", "{{agile_lng_translate 'prefs-online-calendar' 'Tue'}}", "{{agile_lng_translate 'prefs-online-calendar' 'Wed'}}", "{{agile_lng_translate 'prefs-online-calendar' 'Thu'}}", "{{agile_lng_translate 'prefs-online-calendar' 'Fri'}}", "{{agile_lng_translate 'prefs-online-calendar' 'Sat'}}"],
     daysMin: ["{{agile_lng_translate 'days' 'Su'}}","{{agile_lng_translate 'days' 'Mo'}}","{{agile_lng_translate 'days' 'Tu'}}","{{agile_lng_translate 'days' 'We'}}","{{agile_lng_translate 'days' 'Th'}}","{{agile_lng_translate 'days' 'Fr'}}","{{agile_lng_translate 'days' 'Sa'}}","{{agile_lng_translate 'days' 'Su'}}"],
     months: ["{{agile_lng_translate 'months' 'January'}}", "{{agile_lng_translate 'months' 'february'}}", "{{agile_lng_translate 'months' 'March'}}", "{{agile_lng_translate 'months' 'April'}}", "{{agile_lng_translate 'months' 'May'}}", "{{agile_lng_translate 'months' 'June'}}", "{{agile_lng_translate 'months' 'July'}}", "{{agile_lng_translate 'months' 'August'}}", "{{agile_lng_translate 'months' 'September'}}", "{{agile_lng_translate 'months' 'October'}}", "{{agile_lng_translate 'months' 'November'}}", "{{agile_lng_translate 'months' 'December'}}"],
     monthsShort: _agile_month_short_names_localize['months-xs'],
@@ -21,7 +23,7 @@ $.fn.datepicker.dates['en'] = {
 
 // Date format 
 dateFormat.i18n = {
-    dayNames: $.fn.datepicker.dates['en'].daysShort.concat($.fn.datepicker.dates['en'].days),
+    dayNames: $.fn.datepicker.dates['en'].daysShortExact.concat($.fn.datepicker.dates['en'].daysExact),
     monthNames: $.fn.datepicker.dates['en'].monthsShort.concat($.fn.datepicker.dates['en'].months)
 };
 
