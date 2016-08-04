632fef48cb9b73cab6bf81b2a33e6eee2f04d129
sonisonali <sonali@agilecrm.com>
2016-08-04 14:40:45 +0530

    trigger add smsurl

--- agile-core-classes/src/com/thirdparty/twilio/ReplySMSTwilioServlet.java
+++ agile-core-classes/src/com/thirdparty/twilio/ReplySMSTwilioServlet.java
@@ -1,6 +1,7 @@
 package com.thirdparty.twilio;
 
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.List;
 
 import javax.servlet.http.HttpServlet;
@@ -8,10 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.json.JSONObject;
 
 import com.agilecrm.contact.Contact;
 import com.agilecrm.contact.util.ContactUtil;
+import com.agilecrm.widgets.util.ExceptionUtil;
 import com.agilecrm.workflows.triggers.Trigger;
 import com.agilecrm.workflows.triggers.util.TriggerUtil;
 import com.agilecrm.workflows.util.WorkflowSubscribeUtil;
@@ -25,31 +28,39 @@ public class ReplySMSTwilioServlet extends HttpServlet
     try
 	{
 	  System.out.println("this is smsurl for only replying to twilio number");
-	  Contact contact  = null;	
+	   
+	  Contact contact=null;
 	  System.out.println(request.getParameter("To"));
 	  System.out.println(request.getParameter("From"));
-	    /*List<Trigger> triggers = TriggerUtil.getAllTriggers();
+   
+	  String phoneNumber=request.getParameter("To");
+	  String replyMsg=request.getParameter("Body");
+	  if(!StringUtils.isBlank(phoneNumber)){
+		contact = ContactUtil.searchContactByPhoneNumber(phoneNumber);
+	  }
+	 
+	    List<Trigger> triggers = TriggerUtil.getAllTriggers();
 	    for (Trigger trigger : triggers)
 	    {
 		if (StringUtils.equals(trigger.type.toString(), "REPLY_SMS"))
 		{
 		    System.out.println("trigger condition, event match ...");
-		    if (StringUtils.equals(trigger.trigger_form_event, form.id.toString()))
-		    {
-			System.out.println("Assigning campaign to contact ...");
-			if(!StringUtils.isBlank(phone)){
-				contact = ContactUtil.searchContactByPhoneNumber(phone);
+		    if(!StringUtils.isBlank(replyMsg)){
+			
+			if (StringUtils.equals(trigger.call_disposition,replyMsg)){
+			    System.out.println("Assigning campaign to contact ...");
+			    WorkflowSubscribeUtil.subscribeDeferred(contact, trigger.campaign_id,
+					new JSONObject().put("replysms", trigger));
 			}
-			WorkflowSubscribeUtil.subscribeDeferred(contact, trigger.campaign_id,
-				new JSONObject().put("form", formFields));
-		    }
+		    }    
 		}
-	    }*/
+	    }		 
 	   
 	}
     catch (Exception e)
 	{
 	    e.printStackTrace();
+	    System.out.println(ExceptionUtils.getFullStackTrace(e));
 	    return;
 	}
     }
