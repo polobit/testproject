a6841ccb075d0576554425eb074b4b6bf0c5d1e8
Prakash <prakash.agile@gmail.com>
2016-08-17 17:22:04 +0530

    url changed before live

--- agile-core-classes/src/com/agilecrm/social/TwilioUtil.java
+++ agile-core-classes/src/com/agilecrm/social/TwilioUtil.java
@@ -727,8 +727,8 @@ public class TwilioUtil
 		 */
 
 		// For Main
-		//params.put("VoiceUrl", "https://" + NamespaceManager.get() + ".agilecrm.com/twilioiovoice?record=" + record
-		//		+ "&agileuserid=" + agileUserID + "&twimleturl=" + twimletUrlToSend);
+		params.put("VoiceUrl", "https://" + NamespaceManager.get() + ".agilecrm.com/twilioiovoice?record=" + record
+				+ "&agileuserid=" + agileUserID + "&twimleturl=" + twimletUrlToSend);
 
 		// For Beta
 		/*
@@ -740,10 +740,10 @@ public class TwilioUtil
 
 		// For Version
 		
-		  params.put("VoiceUrl", "https://" + NamespaceManager.get() +
+		/*  params.put("VoiceUrl", "https://" + NamespaceManager.get() +
 		  "-dot-29-0-dot-agile-crm-cloud.appspot.com/twilioiovoice?record=" +
 		  record + "&agileuserid=" + agileUserID + "&twimleturl=" +
-		  twimletUrlToSend);
+		  twimletUrlToSend);*/
 		 
 
 		params.put("VoiceMethod", "GET");
@@ -980,7 +980,7 @@ public class TwilioUtil
 			Map<String, String> params = new HashMap<String, String>();
 				params.put("From",from);
 				params.put("To", to);
-				params.put("Url","https://"+NamespaceManager.get()+"-dot-29-0-dot-agile-crm-cloud.appspot.com/conftwiml?conference=" + conferenceName +  "&endConferenceOnExit=no&recordConference=" + record + "&maxParticipants=3");
+				params.put("Url","https://"+NamespaceManager.get()+".agilecrm.com/conftwiml?conference=" + conferenceName +  "&endConferenceOnExit=no&recordConference=" + record + "&maxParticipants=3");
 				TwilioRestResponse response = client.request("/" + APIVERSION + "/Accounts/" + account_sid + "/Calls/", "POST", params);
 				JSONObject responseTextJson = XML.toJSONObject(response.getResponseText()).getJSONObject("TwilioResponse");
 				JSONObject callResponse = responseTextJson.getJSONObject("Call");
@@ -1047,7 +1047,7 @@ public class TwilioUtil
 				childCallSid = responseTextJson.getString("parent_call_sid");
 			}
 			
-				params.put("Url","https://"+NamespaceManager.get()+"-dot-29-0-dot-agile-crm-cloud.appspot.com/conftwiml?conference=" + conferenceName + "&endConferenceOnExit=yes&recordConference=" + record+ "&maxParticipants=3");
+				params.put("Url","https://"+NamespaceManager.get()+".agilecrm.com/conftwiml?conference=" + conferenceName + "&endConferenceOnExit=yes&recordConference=" + record+ "&maxParticipants=3");
 				params.put("Method", "POST");
 				TwilioRestResponse response = client.request("/" + APIVERSION + "/Accounts/" + account_sid + "/Calls/" +childCallSid, "POST", params);
 				System.out.println("respomse for modify call is -" + XML.toJSONObject(response.getResponseText()).getJSONObject("TwilioResponse"));
