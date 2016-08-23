3cfb40ac71e7e367dc67020b8ffa80e362c7ea83
Govind Chunchula <govind@invox.com>
2016-08-23 15:21:06 +0530

    deployment changes

--- agile-frontend/util/deploy.sh
+++ agile-frontend/util/deploy.sh
@@ -24,6 +24,7 @@ do
    mkdir ../WebContent/tpl/min/precompiled/locales/"$i"
    java -jar precompile.jar ../WebContent/tpl/localestmp/locales/"$i" ../../../tmp/handlebars ../WebContent/tpl/min/precompiled/locales/"$i"
    sh yui-flat-full.sh "$i"
+   sh tpl-flatfull.sh "$i"
 done
 #Delete locales
 rm -r ../WebContent/tpl/localestmp
@@ -36,8 +37,6 @@ java -jar precompile.jar ../WebContent/helpcenter/helpcenter-tpl ../../../tmp/ha
 sh yui.sh
 
 sh tpl.sh
-
-sh tpl-flatfull.sh
 
 sh lib-minifier.sh
 
