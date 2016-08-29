f72e1373fa66363d14077777a0e8665b5b4a2341
sankar <soma.savarala@gmail.com>
2016-08-29 15:30:14 +0530

    contacts fetch by cursor

--- agile-core-classes/src/com/agilecrm/contact/deferred/DeleteTextSearchContactsDeferredTask.java
+++ agile-core-classes/src/com/agilecrm/contact/deferred/DeleteTextSearchContactsDeferredTask.java
@@ -1,10 +1,14 @@
 package com.agilecrm.contact.deferred;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.agilecrm.contact.Contact;
 import com.agilecrm.contact.filter.ContactFilter;
+import com.agilecrm.deals.Opportunity;
 import com.agilecrm.search.AppengineSearch;
+import com.agilecrm.search.document.ContactDocument;
+import com.agilecrm.search.document.OpportunityDocument;
 import com.agilecrm.search.query.QueryDocument;
 import com.agilecrm.search.query.util.QueryDocumentUtil;
 import com.agilecrm.search.ui.serialize.SearchRule;
@@ -27,20 +31,41 @@ public class DeleteTextSearchContactsDeferredTask  implements DeferredTask{
 	public void run()
 	{
 		if(!domain.isEmpty() && !domain.equalsIgnoreCase(null) && !domain.equalsIgnoreCase("null") && !type.isEmpty() && !type.equalsIgnoreCase(null) && !type.equalsIgnoreCase("null") ){
-			NamespaceManager.set(domain);
+			NamespaceManager.set(domain);String cursor = null;
 			AppengineSearch<Contact> search = new AppengineSearch<Contact>(Contact.class);
-			QueryDocument<Contact> q = new QueryDocument<Contact>(search.index, Contact.class);
+			List<ScoredDocument> totalDocs = new ArrayList<ScoredDocument>();
+			List<ScoredDocument> scoredDocs = new ArrayList<ScoredDocument>();
 			ContactFilter cf = new ContactFilter();
 			SearchRule rule = new SearchRule();
 			rule.LHS = "type";
 			rule.CONDITION = SearchRule.RuleCondition.EQUALS;
 			rule.RHS = type ;
 			cf.rules.add(rule);
-			String query = QueryDocumentUtil.constructFilterQuery(cf);
-			List <ScoredDocument> scoredDocs = q.getDocuments(query, null);
-			if(scoredDocs != null && scoredDocs.size() > 0)
+			if(type.equals("PERSON") || type.equals("COMPANY")){
+				QueryDocument<Contact> queryInstace = new QueryDocument<Contact>(new ContactDocument().getIndex(), Contact.class);
+				scoredDocs = queryInstace.advancedSearchOnlyIds(cf, 200, cursor, null);
+				 while(scoredDocs != null && scoredDocs.size() > 0){
+					 totalDocs.addAll(scoredDocs);
+					 ScoredDocument doc = scoredDocs.get(scoredDocs.size() - 1);
+					 cursor = doc.getCursor().toWebSafeString();
+					 scoredDocs.clear();
+					 scoredDocs = queryInstace.advancedSearchOnlyIds(cf, 200, cursor, null); 
+				 }
+			}
+			else {			
+				QueryDocument<Opportunity> queryInstace = new QueryDocument<Opportunity>(new OpportunityDocument().getIndex(), Opportunity.class);
+				scoredDocs = queryInstace.advancedSearchOnlyIds(cf, 200, cursor, null);
+				 while(scoredDocs != null && scoredDocs.size() > 0){
+					 totalDocs.addAll(scoredDocs);
+					 ScoredDocument doc = scoredDocs.get(scoredDocs.size() - 1);
+					 cursor = doc.getCursor().toWebSafeString();
+					 scoredDocs.clear();
+					 scoredDocs = queryInstace.advancedSearchOnlyIds(cf, 200, cursor, null); 
+				 }
+			}			
+			if(totalDocs != null && totalDocs.size() > 0)
 			{
-				for(ScoredDocument sd : scoredDocs)
+				for(ScoredDocument sd : totalDocs)
 				{
 					search.delete(sd.getId());
 				}
