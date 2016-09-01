f6d77acae3ad840d6e5d52f840969f7c1e4a02f9
Prakash <prakash.agile@gmail.com>
2016-09-01 17:30:23 +0530

    added manual call to timeline

--- agile-frontend/WebContent/flatfull/jscore/contact-management/logCall.js
+++ agile-frontend/WebContent/flatfull/jscore/contact-management/logCall.js
@@ -136,9 +136,6 @@ $(function()
 		var m = json.min;
 		var s =	json.sec;
 		var duration = 0;
-			if(json['status'] == 'busy' || json['status'] == 'failed' || json['status'] == 'missed' ){
-				duration = 0;
-			}else{
 				if(!h || !m || !s){
 					$("#"+formName +" #logPhone_duration_error1").show().delay(5000).hide(1);
 					return;
@@ -168,7 +165,6 @@ $(function()
 					}
 					
 				}
-			}
 		
 
 		disable_save_button($(this));
