2e9fa4d4107a101100460ee8a25262d30544ee57
poulami guin <poulamibitian@gmail.com>
2016-09-14 16:48:58 +0530

    webrule report modified

--- agile-core-classes/src/com/analytics/servlets/WebruleReportsServlet.java
+++ agile-core-classes/src/com/analytics/servlets/WebruleReportsServlet.java
@@ -1,7 +1,5 @@
 package com.analytics.servlets;
 
-
-
 import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,21 +10,23 @@ import com.webruleio.reports.WebruleReportsSQLUtil;
 public class WebruleReportsServlet extends HttpServlet
 {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException
-    { 
-   String emailid=req.getParameter("email").toLowerCase();
-  String domainname=req.getParameter("domain");    
-	String webruleid=req.getParameter("webruleid"); 
-  String webruleAction=req.getParameter("webruletype"); 
-  System.out.println(emailid+" "+domainname+""+webruleid+""+webruleAction);
- 
-  WebruleReportsSQLUtil.insertData(emailid,domainname,webruleid,webruleAction);
-      
- }    
-    
+    {
+	String emailid=null;
+	if (req.getParameter("email") != null)
+	    emailid = req.getParameter("email").toLowerCase();
+	String domainname = req.getParameter("domain");
+	String webruleid = req.getParameter("webruleid");
+	String webruleAction = req.getParameter("webruletype");
+	System.out.println(emailid + " " + domainname + "" + webruleid + "" + webruleAction);
+
+	WebruleReportsSQLUtil.insertData(emailid, domainname, webruleid, webruleAction);
+
+    }
+
     public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException
     {
-	 
-    	doGet( req,res);
-    	
+
+	doGet(req, res);
+
     }
 }
