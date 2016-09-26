94bbcdfa1b4dcf80a510080f0352692ffba06d77
sonisonali <sonali@agilecrm.com>
2016-09-26 16:29:23 +0530

    print numbers in smstrigger

--- agile-core-classes/src/com/thirdparty/twilio/ReplySMSTwilioServlet.java
+++ agile-core-classes/src/com/thirdparty/twilio/ReplySMSTwilioServlet.java
@@ -25,26 +25,27 @@ public class ReplySMSTwilioServlet extends HttpServlet
 	    System.out.println("this is smsurl for only replying to twilio number");
 	    
 	    Contact contact = null;
-	    String phoneNumber = request.getParameter("To");
+	    String contactNumber = request.getParameter("From");
+	    String replyNumber = request.getParameter("To");
+	    System.out.println("contactNumber:"+contactNumber);
+	    System.out.println("replyNumber:"+replyNumber);
 	    String replyMsg = request.getParameter("Body");
 	    if (!StringUtils.isBlank(replyMsg))
 	    {
-        	    if (!StringUtils.isBlank(phoneNumber))
+        	    if (!StringUtils.isBlank(contactNumber))
         	    {
-        		contact = ContactUtil.searchContactByPhoneNumber(phoneNumber);
+        		contact = ContactUtil.searchContactByPhoneNumber(contactNumber);
         	    }
-        	    List<Trigger> triggers = TriggerUtil.getAllTriggers();
+        	    List<Trigger> triggers = TriggerUtil.getTriggersByCondition(Trigger.Type.REPLY_SMS);
         	    for (Trigger trigger : triggers)
         	    {
-        		if (StringUtils.equals(trigger.type.toString(), "REPLY_SMS"))
-        		{		   
-        			if (trigger.sms_keyword.equalsIgnoreCase(replyMsg) && trigger.sms_reply.equalsIgnoreCase(phoneNumber))
-        			{
-        			    System.out.println("Assigning campaign to contact ...");
-        			    WorkflowSubscribeUtil.subscribeDeferred(contact, trigger.campaign_id,
-        				    new JSONObject().put("replysms", trigger));
-        			}
-        		}
+        		if (trigger.sms_keyword.equalsIgnoreCase(replyMsg) && trigger.sms_reply.equalsIgnoreCase(replyNumber))
+			{
+			    System.out.println("Assigning campaign to contact ...");
+			    WorkflowSubscribeUtil.subscribeDeferred(contact, trigger.campaign_id,
+				    new JSONObject().put("replysms", trigger));
+			}
+        		
         	    }
     	    }
 	    
