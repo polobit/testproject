f334714b849a31d173c72014e00a30028141e25c
sudheepthi <sudheepthi@agilecrm.com>
2016-09-28 12:01:54 +0530

    added app_contacts in contact instead of app_companies

--- agile-frontend/WebContent/flatfull/jscore/handlebars/handlebars-agile.js
+++ agile-frontend/WebContent/flatfull/jscore/handlebars/handlebars-agile.js
@@ -659,16 +659,16 @@ function getContactCustomProperties(items)
 
 	var position_arr = {};
 	var position_max = 0;
-	if(App_Companies.customFieldsList!=undefined && App_Companies.customFieldsList!=null){
-		position_max = App_Companies.customFieldsList.collection.models.length+1;
-		for(var i=0;i<App_Companies.customFieldsList.collection.models.length;i++){
-			curr_scope = App_Companies.customFieldsList.collection.models[i].get("scope");
+	if(App_Contacts.customFieldsList!=undefined && App_Contacts.customFieldsList!=null){
+		position_max = App_Contacts.customFieldsList.collection.models.length+1;
+		for(var i=0;i<App_Contacts.customFieldsList.collection.models.length;i++){
+			curr_scope = App_Contacts.customFieldsList.collection.models[i].get("scope");
 			if(curr_route==curr_scope){
-				if(App_Companies.customFieldsList.collection.models[i].get("position")==0 || App_Companies.customFieldsList.collection.models[i].get("position")==""){
-					position_arr[''+App_Companies.customFieldsList.collection.models[i].get("field_label")] = position_max;
+				if(App_Contacts.customFieldsList.collection.models[i].get("position")==0 || App_Contacts.customFieldsList.collection.models[i].get("position")==""){
+					position_arr[''+App_Contacts.customFieldsList.collection.models[i].get("field_label")] = position_max;
 					position_max++;
 				}else{
-					position_arr[''+App_Companies.customFieldsList.collection.models[i].get("field_label")] = App_Companies.customFieldsList.collection.models[i].get("position");
+					position_arr[''+App_Contacts.customFieldsList.collection.models[i].get("field_label")] = App_Contacts.customFieldsList.collection.models[i].get("position");
 				}
 			}
 		}
