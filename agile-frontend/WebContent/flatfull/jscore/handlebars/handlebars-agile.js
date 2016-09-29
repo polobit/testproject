efe540457b54f6e74998a712ebeb7e5ecb650ccc
sudheepthi <sudheepthi@agilecrm.com>
2016-09-29 10:29:16 +0530

    custom fields - bug fixing

--- agile-frontend/WebContent/flatfull/jscore/handlebars/handlebars-agile.js
+++ agile-frontend/WebContent/flatfull/jscore/handlebars/handlebars-agile.js
@@ -689,7 +689,7 @@ function getContactCustomProperties(items)
 	
 	for (var i = 0; i < temp_fields.length; i++){
 		if(temp_fields[i]!=undefined && temp_fields[i]!=""){
-			fields.push(items[i]);
+			fields.push(temp_fields[i]);
 		}
 	}
 
@@ -855,7 +855,7 @@ function getCompanyCustomProperties(items)
 	
 	for (var i = 0; i < temp_fields.length; i++){
 		if(temp_fields[i]!=undefined && temp_fields[i]!=""){
-			fields.push(items[i]);
+			fields.push(temp_fields[i]);
 		}
 	}
 
