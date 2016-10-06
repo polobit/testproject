d44ee8177d1f4f551509407d98fed75a74cc6e92
santhoshmogulla <mogulla@invox.com>
2016-10-06 18:07:45 +0530

    email credits decrement issue while auto recharge

--- agile-core-classes/src/com/agilecrm/subscription/restrictions/db/BillingRestriction.java
+++ agile-core-classes/src/com/agilecrm/subscription/restrictions/db/BillingRestriction.java
@@ -602,7 +602,7 @@ public class BillingRestriction
     	BillingRestriction restriction = BillingRestrictionUtil.getBillingRestrictionFromDB();
     	restriction.email_credits_count -= decrementCount;
     	if(restriction.email_credits_count <= restriction.autoRenewalPoint){	
-			RenewalCreditsDeferredTask task = new RenewalCreditsDeferredTask(NamespaceManager.get(), quantity, decrementCount);			
+			RenewalCreditsDeferredTask task = new RenewalCreditsDeferredTask(NamespaceManager.get(), 0, decrementCount);			
 			task.run();
     	}
 	}
