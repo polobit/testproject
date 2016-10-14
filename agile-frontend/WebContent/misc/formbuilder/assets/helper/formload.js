bc5b25039b0104ee636fe6ca72cbdd4be9cc4004
Priyanka-25 <veidhey@agilecrm.com>
2016-10-14 17:12:33 +0530

    old form issue

--- agile-frontend/WebContent/misc/formbuilder/assets/helper/formload.js
+++ agile-frontend/WebContent/misc/formbuilder/assets/helper/formload.js
@@ -99,6 +99,14 @@ define([
 							agileinlinesubmit.value = "Great! Thanks for filling out my form!";
 							saveform[0].fields.agileinlinesubmit=agileinlinesubmit;
 					}
+
+					if(saveform[0].fields.agileapi){
+							saveform[0].fields.agileapi.label="";
+					}
+					if(saveform[0].fields.agiledomain){
+							saveform[0].fields.agiledomain.label="";
+					}
+
 					$('#form-label').text('Edit Form');
 					new MyFormView({ title : "Original", collection : new MyFormSnippetsCollection(saveform) });				
 				});
