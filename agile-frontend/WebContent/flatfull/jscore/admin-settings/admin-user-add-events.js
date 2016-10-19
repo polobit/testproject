58fdf1789c77c441023715eb69cee2bb79d7f8fd
Santhosh Reddy Mogulla <mogulla@invox.com>
2016-10-19 10:25:33 +0530

    allow acl to gaspumptv domain

--- agile-frontend/WebContent/flatfull/jscore/admin-settings/admin-user-add-events.js
+++ agile-frontend/WebContent/flatfull/jscore/admin-settings/admin-user-add-events.js
@@ -95,7 +95,7 @@ function bindAdminChangeAction(el, data)
 
 // Allow acls for specific domains
 function checkForACLExceptionalUsers(){
-	var specialUsers = ["savourychef","organicleads","cutrone","sunsationalswimschoo","aviation", "mybandmarket", "grupocsi", "orcamortgages", "nexusworkspace"];
+	var specialUsers = ["savourychef","organicleads","cutrone","sunsationalswimschoo","aviation", "mybandmarket", "grupocsi", "orcamortgages", "nexusworkspace", "gaspumptv"];
 	if($.inArray(CURRENT_DOMAIN_USER.domain, specialUsers) != -1)
 		return true;
 	else
