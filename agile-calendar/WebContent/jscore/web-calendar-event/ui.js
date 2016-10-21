5e77c39b155aec43ae058ef4d41181125a248a4d
vijay <vijay.agilecrm@gmail.com>
2016-10-21 11:31:46 +0530

    bold text issue

--- agile-calendar/WebContent/jscore/web-calendar-event/ui.js
+++ agile-calendar/WebContent/jscore/web-calendar-event/ui.js
@@ -7,8 +7,7 @@ $(function()
 	var MIDNIGHT_END_TIME = null;
 
 	// Select slot duration 60/30/15min
-	//$(".selected_meeting_time").die().live('click', function(e)
-	$('body').on('click','.selected_meeting_time',function(e)
+	$('body').on('click', '.selected_meeting_time', function(e)
 	{
 		// e.preventDefault();
 
@@ -35,7 +34,7 @@ $(function()
 		{
 			$('#datepick').DatePickerSetDate(current_date_mozilla, true);
 		}
-		$('.checkbox-main-grid').html('<img class="loading-img" src="../../img/21-0.gif" style="width: 40px;margin-left: 216px;"></img>');
+		$('.checkbox-main-grid').html('<img class="loading-img" src="'+updateImageS3Path("../../img/21-0.gif")+'" style="width: 40px;margin-left: 216px;"></img>');
 		if (!selecteddate)
 		{
 			selecteddate = new Date();
@@ -61,8 +60,7 @@ $(function()
 	});
 
 	// Only single slot selection is allowed
-	//$(".selected-slot").die().live('click', function(e)
-	$('body').on('click','.selected-slot',function(e)
+	$('body').on('click', '.selected-slot', function(e)
 	{
 		var currentId = $(this).attr('id');
 
@@ -82,8 +80,7 @@ $(function()
 
 	});
 
-	//$("#multi-user-avatar").die().live('click', function(e)
-	$('body').on('click','#multi-user-avatar',function(e)
+	$('body').on('click', '#multi-user-avatar', function(e)
 	{
 		$(".thumbnail").css("background", "none");
 		$(this).css("background", "#4A90E2");
@@ -107,7 +104,7 @@ $(function()
 				{
 					$(".meetingtypes").show();
 					$(".meetingtypes").empty();
-					$(".meetingtypes").append("<option selected disabled>" + LOCALES_JSON['meeting-type'] + "</option>");
+					$(".meetingtypes").append("<option selected disabled>{{agile_lng_translate 'calendar' 'meeting-type'}}</option>");
 					for (var i = 0; i < meeting_types.length; i++)
 					{
 						$(".meetingtypes").append("<option value='" + meeting_types[i] + "'>" + meeting_types[i] + "</option>");
@@ -117,7 +114,8 @@ $(function()
 					$(".meetingtypes").hide();
 
 				slot_details = SELECTED_DOMAIN_USER['slot_details'];
-				$('.show_slots').hide();
+				//$('.show_slots').hide();
+				$('.show_slots').empty();
 				getSlotDurations();
 				$(".segment1").fadeIn("slow");
 				$(".panel-body").height(parseInt(getPanelBodyMaxHeight()) + 26);
@@ -128,17 +126,15 @@ $(function()
 
 	});
 
-	//$('#user_timezone').die().change(function()
 	$('#user_timezone').change(function()
 	{
 
-		SELECTED_TIMEZONE = $('#user_timezone').val();		
-		updateUserBusinessHoursInVisitorTimezone();
+		SELECTED_TIMEZONE = $('#user_timezone').val();
 
 		if (!selecteddate || !Selected_Time)
 			return;
-		$("#current_local_time").html("Current Time: " + getConvertedTimeFromEpoch(new Date().getTime() / 1000));
-		$('.checkbox-main-grid').html('<img class="loading-img" src="../../img/21-0.gif" style="width: 40px;margin-left: 216px;"></img>');
+		$("#current_local_time").html("{{agile_lng_translate 'timezone' 'current-time'}}: " + getConvertedTimeFromEpoch(new Date().getTime() / 1000));
+		$('.checkbox-main-grid').html('<img class="loading-img" src="'+updateImageS3Path("../../img/21-0.gif")+'" style="width: 40px;margin-left: 216px;"></img>');
 		get_slots(selecteddate, Selected_Time);
 	});
 
