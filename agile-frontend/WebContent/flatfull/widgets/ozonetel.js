75e0bc53eefb0bc9f338d30b50c8f36cd90c6599
rajesh-agilecrm <rajesh@agilecrm.com>
2016-11-10 19:01:39 +0530

    updated to place a call from contacts details page

---
+++ agile-frontend/WebContent/flatfull/widgets/ozonetel.js
@@ -0,0 +1,16 @@
+$(function(){
+	$('body').off('click', '.Ozonetel_call');
+	$('body').on('click', '.Ozonetel_call', function(e){
+		e.preventDefault();
+		e.stopPropagation();
+		if($(this).closest(".contact-make-call").hasClass('popover-call')){
+			var from;
+			var contactPopoverObj = App_Contacts.contact_popover.toJSON();
+			callToNumber($(this).closest(".contact-make-call").attr("phone"), from, "Ozonetel",contactPopoverObj, "");
+			return;
+		}else{
+			var contactDetailsObj = agile_crm_get_contact();
+			dialFromOzonetel($(this).closest(".contact-make-call").attr("phone"), getContactName(contactDetailsObj), null, contactDetailsObj);
+		}
+	});
+});
