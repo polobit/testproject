3cd47db0640201d74cf482867f840c707b50b0a4
prashannjeet-agile <prashannjeet@agilecrm.com>
2016-11-11 17:06:49 +0530

    live and beta url added for open link

--- agile-core-classes/src/com/agilecrm/util/EmailUtil.java
+++ agile-core-classes/src/com/agilecrm/util/EmailUtil.java
@@ -29,6 +29,7 @@ import com.agilecrm.user.EmailPrefs;
 import com.campaignio.tasklets.util.MergeFieldsUtil;
 import com.google.appengine.api.NamespaceManager;
 import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.utils.SystemProperty;
 import com.thirdparty.sendgrid.SendGrid;
 
 public class EmailUtil
@@ -38,7 +39,15 @@ public class EmailUtil
     public static String[] agileEmails = { "care@agilecrm.com", "sales@agilecrm.com" };
     public static List<String> agileEmailsList = Arrays.asList(agileEmails);
     
-    public static final String OPEN_TRACKING_URL = "https://agle.me/open";
+    /**
+     * Beta open email track url
+     */
+    public static final String BETA_OPEN_TRACKING_URL = "http://open-beta.agle.me";
+    
+    /**
+     * Live open email track url
+     */
+    public static final String LIVE_OPEN_TRACKING_URL = "http://open.agle.me";
 
     /**
      * Parses html body of an email using jsoup.
@@ -176,16 +185,22 @@ public class EmailUtil
 	if (!StringUtils.isEmpty(campaignId))
 	    queryParams = "c=" + campaignId;
 
-	// If not emtpy add '&'
+	/*// If not emtpy add '&'
 	if (!StringUtils.isEmpty(queryParams))
-	    queryParams += "&";
+	    queryParams += "&";*/
 
 	// Contact id (for campaigns) or Tracker Id (for personal emails)
 	if (!StringUtils.isEmpty(trackerId))
 	    queryParams += "s=" + trackerId;
 	
+	String trackURL = LIVE_OPEN_TRACKING_URL;
+	
+	//If environment is beta then use beta url
+	if(SystemProperty.applicationId.get().equals("agilecrmbeta"))
+		trackURL = BETA_OPEN_TRACKING_URL;
+	
 	String trackingImage = "<div class=\"ag-img\"><img src="
-	        + OPEN_TRACKING_URL + "?" + queryParams
+	        + trackURL + "?" + queryParams
 	        + " nosend=\"1\" style=\"display:none!important;\" width=\"1\" height=\"1\"></img></div>";
 
 	return html + trackingImage;
