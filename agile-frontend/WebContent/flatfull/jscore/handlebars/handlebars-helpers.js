2d71103ba9697156848a1fa5bf501c5d8d9307c5
Govind Chunchula <govind@invox.com>
2016-11-15 16:46:31 +0530

    Issue fix with empty dashboard selection

--- agile-frontend/WebContent/flatfull/jscore/handlebars/handlebars-helpers.js
+++ agile-frontend/WebContent/flatfull/jscore/handlebars/handlebars-helpers.js
@@ -7209,6 +7209,9 @@ Handlebars.registerHelper('convert_toISOString', function(dateInepoch, options) 
 			CURRENT_USER_DASHBOARDS.sort(function(a,b){return a.name.trim() < b.name.trim() ? -1 : a.name.trim() > b.name.trim() ? 1 : 0;});
 			var is_active_added = false;
 			var selected_li_id = _agile_get_prefs("dashboard_"+CURRENT_DOMAIN_USER.id);
+			// If there is no selected dashboard in cookie, get type from Role
+			if(!selected_li_id)
+				selected_li_id = menuServiceDashboard(CURRENT_DOMAIN_USER.role);
 
 			$.each(CURRENT_USER_DASHBOARDS, function(index, value){
 				if(selected_li_id == this.id)
