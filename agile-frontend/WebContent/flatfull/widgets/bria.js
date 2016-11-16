fc4e7c6f7c4427d88260384d63fd49362f5a9435
Prakash <prakash.agile@gmail.com>
2016-11-16 15:35:54 +0530

    loading logs fast

--- agile-frontend/WebContent/flatfull/widgets/bria.js
+++ agile-frontend/WebContent/flatfull/widgets/bria.js
@@ -5,13 +5,46 @@
  */
 
 function startBriaWidget(contact_id){
-	setTimeout(function()
-	{
-		if(!Pubnub.is_connected_call){
-			startBriaWidget();
-			return;
-		}
-		getLogsForBria();
-	}, 8000);
-
+	sendTestCommandToClient(function(){
+		getLogsIfPubnubIsConnectedBria();
+	});
 }
+
+
+function getLogsIfPubnubIsConnectedBria(){
+	
+	//two cases 
+	//1. if the client is opened
+	//2. if the client is not opened
+	// If the client is opened then we will wait for the pubnub to load
+	// if the client is not opened then we will not wait for the pubnub and show the message
+	
+	if(!callJar.running){
+		getLogsForBria();
+		return;
+	}
+	setTimeout(function()
+			{
+				if(!Pubnub){
+					getLogsIfPubnubIsConnectedBria();
+					return;
+				}
+				if(!Pubnub.is_connected_call){
+					getLogsIfPubnubIsConnectedBria();
+					return;
+				}
+				getLogsForBria();
+			}, 1000);
+}
+
+/*function showEmptyLog(){
+	 var l = {};
+     l.data = "";
+     if (c == "Bria") {
+         handleLogsForBria(l)
+     } else {
+         if (c == "Skype") {
+             handleLogsForSkype(l)
+         }
+     }
+}*/
