22b92f185b381e0eb81e9e13110902cfdab81bf2
premnathtammina <premtammina@gmail.com>
2016-11-24 10:32:52 +0530

    appcalling js file done updated

--- agile-frontend/WebContent/widgets/appcalling.js
+++ agile-frontend/WebContent/widgets/appcalling.js
@@ -1,3 +1,23 @@
+function appDialer(to, contact){
+	if(to){
+		var requestURL = "core/api/android/call?phone_number="+to;	 
+	 	console.log(requestURL);
+	 
+	 	$.ajax({	 		
+	 		url : requestURL,
+	 		type : "GET",	 		
+	 		success : function(result) {
+	 			console.log("AppCalling *** success : "+ JSON.stringify(result));						
+	 			//{{agile_lng_translate 'other' 'cancel'}}			
+	 			console.log(to + " : "+ contact);				
+	 		},
+	 		error : function(result) {
+	 			console.log("AppCalling *** error : "+ JSON.stringify(result));								
+	 		}
+	 	});
+ 	}
+}
+
 function startAppCallingWidget(contact_id){
 	APPCALLING_PLUGIN_NAME = "AppCalling";
 
@@ -10,4 +30,13 @@ function startAppCallingWidget(contact_id){
 	APPCALLING_Plugin_Id = appcalling_widget.id;	
 	
 	$('#AppCalling').html("No Logs");
+
+	$(".contact-make-call-div").off("click", ".AppCalling_call");
+ 	$(".contact-make-call-div").on("click", ".AppCalling_call", function(e){
+ 		e.preventDefault();
+ 		e.stopPropagation();
+ 		var contactDetailsObj = agile_crm_get_contact();
+ 		var contactPhoneNumber = $(this).closest(".contact-make-call").attr("phone");		
+ 	 	appDialer(contactPhoneNumber, contactDetailsObj);	
+ 	});
 }
