9dd072c67b611faeca2c17bb85ecc2aa9b942926
sonisonali <sonali@agilecrm.com>
2016-11-25 17:17:48 +0530

    change loader animation for all

--- agile-frontend/WebContent/misc/pagebuilder/source/js/modules/builder.js
+++ agile-frontend/WebContent/misc/pagebuilder/source/js/modules/builder.js
@@ -267,7 +267,7 @@
                 $(overlay).hide();
                 overlay.id = 'canvasOverlay';
 
-                overlay.innerHTML = '<div class="loader"><span>{</span><span>}</span></div>';
+                overlay.innerHTML = '<div class="loader"><i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i><br/><span>Loading...</span></div>';
 
                 document.getElementById('frameWrapper').appendChild(overlay);
 
