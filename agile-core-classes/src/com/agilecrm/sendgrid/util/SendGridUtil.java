08a63b3f23ef917bbf5c207c46a35baad616dd11
rameshrudra <ramesh@invox.com>
2016-11-30 15:10:06 +0530

    blocking spam emails by identifying spam urls

--- agile-core-classes/src/com/agilecrm/sendgrid/util/SendGridUtil.java
+++ agile-core-classes/src/com/agilecrm/sendgrid/util/SendGridUtil.java
@@ -64,7 +64,7 @@ public class SendGridUtil
 	public static final String SENDGRID_AUTOMATIC_SECURITY = "automatic_security";
 	
      // 
-     private static final String[] blockedBodyStringList = new String[] { "paypal", "unknown device"};
+     private static final String[] blockedBodyStringList = new String[] { "pp-secure-review", "paypal", "unknown device"};
      private static final String[] blockedSubjectStringList = new String[] {"unknown device"};
 
     /**
@@ -810,6 +810,9 @@ public static String validateSendgridWhiteLabelDomain(String emailDomain, EmailG
 		
 		return false;
 	}
+	
+	
+	
 	
 	
 	
