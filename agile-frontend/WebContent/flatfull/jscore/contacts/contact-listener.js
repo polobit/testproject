2651d3138f875974570034bcc3cd68b8b1eb2b36
Nidso <nidhi@agilecrm.com>
2016-11-30 12:04:59 +0530

    Timer for popover

--- agile-frontend/WebContent/flatfull/jscore/contacts/contact-listener.js
+++ agile-frontend/WebContent/flatfull/jscore/contacts/contact-listener.js
@@ -49,7 +49,7 @@ var timer = undefined;
 		 	});
 		}
 		 	
-		 }, 2000);
+		 }, 1500);
 });
 
 
@@ -745,7 +745,7 @@ function popoverEnter(that,left,top,listView,campaigns_view)
 		 		return false;
 		 	}
 		 	}
-		 }, 2000);
+		 }, 1500);
 }
 
 function popout(that)
