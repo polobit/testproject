bfa15c4e6906b8835059555d58e3da8d458be863
santhoshmogulla <mogulla@invox.com>
2016-12-02 09:56:40 +0530

    deployment changes

--- live-deploy.sh
+++ live-deploy.sh
@@ -19,9 +19,9 @@ git checkout .
 
 git stash
 
-git checkout agile_production_1_0
+git checkout version33-6
 
-git pull origin agile_production_1_0
+git pull origin version33-6
 
 ant create-target  -DRELEASE_VERSION=$2 -DPRODUCTION=true;
 
