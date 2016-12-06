2be6afd78a6c93337f9a24c5c1d7a8dd20765b60
sonisonali <sonali@agilecrm.com>
2016-12-06 12:16:19 +0530

    slider issue for background color

--- agile-frontend/WebContent/misc/pagebuilder/source/elements/js/builder_in_block.js
+++ agile-frontend/WebContent/misc/pagebuilder/source/elements/js/builder_in_block.js
@@ -6,4 +6,14 @@
 
 	for ( var i = 0; i < overlays.length; i++ ) overlays[i].remove();
 
+	$('.carousel-control.left').click(function(e) {
+        $('#myCarousel').carousel('prev');
+        e.stopPropagation();
+    });
+
+    $('.carousel-control.right').click(function(e) {
+        $('#myCarousel').carousel('next');
+        e.stopPropagation();
+	});
+
 } ());
