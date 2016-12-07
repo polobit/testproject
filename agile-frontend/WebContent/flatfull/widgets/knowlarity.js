32cebce5eed3a3680f4b9ca57bb3ae4be446a9d3
premnathtammina <premtammina@gmail.com>
2016-12-07 17:02:28 +0530

    knowlarity issue fixed

--- agile-frontend/WebContent/flatfull/widgets/knowlarity.js
+++ agile-frontend/WebContent/flatfull/widgets/knowlarity.js
@@ -266,24 +266,24 @@ function changeCallNotyBasedOnStatus(event){
 				if(eventType){				
 					if(eventType == "AGENT_CALL"){
 						KNOWLARITY_PREVIOUS_EVENT = "AGENT_CALL";									
-						var btns = [{"id":"", "class":"btn btn-default btn-sm noty_twilio_cancel","title":"{{agile_lng_translate 'other' 'cancel'}}"}];						
+						var btns = [{"id":"", "class":"btn btn-default btn-sm noty_twilio_cancel", "title":"{{agile_lng_translate 'other' 'cancel'}}"}];						
 						showDraggableNoty("Knowlarity", globalCall.contactedContact, "connecting", globalCall.callNumber, btns);
 					}else if(eventType == "CUSTOMER_CALL"){
 						KNOWLARITY_PREVIOUS_EVENT = "CUSTOMER_CALL";
 						var json = {"callId": agentNumber};				
-						var btns = [{"id":"", "class":"btn btn-default btn-sm noty_twilio_cancel","title":"{{agile_lng_translate 'other' 'cancel'}}"}];							
+						var btns = [{"id":"", "class":"btn btn-default btn-sm noty_twilio_cancel", "title":"{{agile_lng_translate 'other' 'cancel'}}"}];							
 						showDraggableNoty("Knowlarity", globalCall.contactedContact, "ringing", globalCall.callNumber, btns, json, callDirection);					
 					}else if(eventType == "BRIDGE"){
 						KNOWLARITY_PREVIOUS_EVENT = "BRIDGE";
-						var btns = [{"id":"", "class":"btn btn-default btn-sm noty_twilio_cancel","title":"{{agile_lng_translate 'other' 'cancel'}}"}];	
+						var btns = [{"id":"", "class":"btn btn-default btn-sm noty_twilio_cancel", "title":"{{agile_lng_translate 'other' 'cancel'}}"}];	
 						showDraggableNoty("Knowlarity", globalCall.contactedContact, "connected", globalCall.callNumber, btns);					
 					}else if(KNOWLARITY_PREVIOUS_EVENT && KNOWLARITY_PREVIOUS_EVENT == "BRIDGE" && eventType == "HANGUP"){
 						KNOWLARITY_PREVIOUS_EVENT = "HANGUP";
-						var btns = [{"id":"", "class":"btn btn-default btn-sm noty_twilio_cancel","title":"{{agile_lng_translate 'other' 'cancel'}}"}];	
+						var btns = [{"id":"", "class":"btn btn-default btn-sm noty_twilio_cancel", "title":"{{agile_lng_translate 'other' 'cancel'}}"}];	
 						showDraggableNoty("Knowlarity", globalCall.contactedContact, "answered", globalCall.callNumber, btns);					
 					}else if(KNOWLARITY_PREVIOUS_EVENT && KNOWLARITY_PREVIOUS_EVENT == "AGENT_CALL" && eventType == "HANGUP"){
 						KNOWLARITY_PREVIOUS_EVENT = "HANGUP";
-						var btns = [{"id":"", "class":"btn btn-default btn-sm noty_twilio_cancel","title":"{{agile_lng_translate 'other' 'cancel'}}"}];	
+						var btns = [{"id":"", "class":"btn btn-default btn-sm noty_twilio_cancel", "title":"{{agile_lng_translate 'other' 'cancel'}}"}];	
 						showDraggableNoty("Knowlarity", globalCall.contactedContact, "failed", globalCall.callNumber, btns);					
 					}
 				}
