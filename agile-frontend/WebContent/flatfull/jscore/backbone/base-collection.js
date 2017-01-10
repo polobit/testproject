ac66805f88ef1527aceb8e99e6a93a9b9185b9b9
srijaagilecrm <srija.vanapalli.agilecrm>
2017-01-10 14:37:39 +0530

    Add page size condition for enterprise users

--- agile-frontend/WebContent/flatfull/jscore/backbone/base-collection.js
+++ agile-frontend/WebContent/flatfull/jscore/backbone/base-collection.js
@@ -354,9 +354,17 @@ var Base_Collection_View = Backbone.View
            * If page size is not defined then sets page size to 20.
            */
           this.page_size = this.options.page_size;
-          if(this.page_size != CURRENT_USER_PREFS.page_size){
-            this.page_size = CURRENT_USER_PREFS.page_size;
+          var plan_name = USER_BILLING_PREFS.plan.plan_type;
+          var plan_fragments = plan_name.split("_");
+          var db_plan_name = plan_fragments[0].toLowerCase();
+          if(db_plan_name == "enterprise"){
+                this.page_size = CURRENT_USER_PREFS.page_size;
           }
+          else
+              this.page_size = "25";
+          /*if(this.page_size != CURRENT_USER_PREFS.page_size){
+            this.page_size = CURRENT_USER_PREFS.page_size;
+          }*/
           this.global_sort_key = this.options.global_sort_key;
           this.request_method = this.options.request_method;
           this.post_data = this.options.post_data;
