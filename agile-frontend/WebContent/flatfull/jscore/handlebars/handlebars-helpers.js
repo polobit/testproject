85f2c12aa326ba39a0799fed17953752961c66d7
sankar <soma.savarala@gmail.com>
2017-01-16 12:28:56 +0530

    currency symbol in deal details page

--- agile-frontend/WebContent/flatfull/jscore/handlebars/handlebars-helpers.js
+++ agile-frontend/WebContent/flatfull/jscore/handlebars/handlebars-helpers.js
@@ -1121,10 +1121,18 @@ $(function()
 	/**
 	 * Returns currency symbol based on the currency value (deals)
 	 */
+	Handlebars.registerHelper('currencyType', function(currency_type)
+	{
+		var value = (currency_type ? currency_type : CURRENT_USER_PREFS.currency);
+		value = (value ? value : "USD-$");
+		var symbol = ((value.length < 4) ? "$" : value.substring(4, value.length));
+		return symbol;
+	});
 	Handlebars.registerHelper('currencySymbol', function()
 	{
 		var value = ((CURRENT_USER_PREFS.currency != null) ? CURRENT_USER_PREFS.currency : "USD-$");
 		var symbol = ((value.length < 4) ? "$" : value.substring(4, value.length));
+	    return symbol;
 		if(symbol=='Rs')
 			symbol='Rs.';
 		return symbol;
