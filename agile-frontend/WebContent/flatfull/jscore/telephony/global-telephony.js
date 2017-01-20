edb80edf0c5a1d9ce9487b8978c6ce93fb495fc4
Sri Gottipati <gsribhagavan@gmail.com>
2017-01-20 10:31:31 +0530

    bria test command

--- agile-frontend/WebContent/flatfull/jscore/telephony/global-telephony.js
+++ agile-frontend/WebContent/flatfull/jscore/telephony/global-telephony.js
@@ -393,13 +393,13 @@ function sendTestCommand()
 						sendTestCommand();
 						return;
 					}
-					
+					sendTestCommandToClient();			
 				}, 5000);
 		
 	
 	}
 
-function sendCommandToClient(command,widget)
+/*function sendCommandToClient(command,widget)
 {
 			var command = command;
 			var number =  "";
@@ -411,7 +411,7 @@ function sendCommandToClient(command,widget)
 			}
 			
 			return;
-}
+}*/
 
 function replicateglobalCallVariable()
 {
