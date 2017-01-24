fde5661506d747db8724ed9c6cdd7b756c955d9a
rajesh-agilecrm <rajesh@agilecrm.com>
2017-01-24 15:06:54 +0530

    updated for linkedin contact sync

--- agile-frontend/WebContent/flatfull/widgets/linkedin.js
+++ agile-frontend/WebContent/flatfull/widgets/linkedin.js
@@ -12,13 +12,13 @@ $(function(){
 	    console.log(data.image_src+"received==="+data.p_link+"====="+data.p_phone+"===="+data.p_email+"===="+data.p_twitter);
 
 	    var contact_image = agile_crm_get_contact_property("image");
-	    var propertiesArray = [{ "name" : "website", "value" : data.p_link, "subtype" : "LINKEDIN" }];
+	    var propertiesArray = [{ "name" : "website", "value" : data.p_link, "subtype" : "LINKEDIN","type" : "SYSTEM" }];
 	    var website_url = data.p_url;
 	    if(website_url){
 	    	var w_urls = website_url.split(",");
 	    	for(var i=0;i<w_urls.length;i++){
 	    		if(!checkPropertyValue("website",w_urls[i].trim(),"URL")){
-	    			propertiesArray.push({ "name" : "website", "value" : w_urls[i].trim(),"subtype" : "URL"});
+	    			propertiesArray.push({ "name" : "website", "value" : w_urls[i].trim(),"subtype" : "URL","type" : "SYSTEM"});
 	    		}
 	    	}
 	    }
@@ -27,7 +27,7 @@ $(function(){
 	    	var t_profiles = twitter_profile.split(",");
 	    	for(var i=0;i<t_profiles.length;i++){
 	    		if(!checkPropertyValue("website","@"+t_profiles[i].trim(),"TWITTER")){
-	    			propertiesArray.push({ "name" : "website", "value" : "@"+t_profiles[i].trim(),"subtype" : "TWITTER"});
+	    			propertiesArray.push({ "name" : "website", "value" : "@"+t_profiles[i].trim(),"subtype" : "TWITTER","type" : "SYSTEM"});
 	    		}
 	    	}
 	    }
@@ -36,7 +36,7 @@ $(function(){
 	    	var c_email = l_email.split(",");
 	    	for(var i=0;i<c_email.length;i++){
 		    	if(!checkPropertyValueWithOutSubType("email",c_email[i].split(" (")[0].trim())){
-					propertiesArray.push({ "name" : "email", "value" : c_email[i].split(" (")[0].trim()});
+					propertiesArray.push({ "name" : "email", "value" : c_email[i].split(" (")[0].trim(),"type" : "SYSTEM"});
 				}
 			}
 		}
@@ -45,7 +45,7 @@ $(function(){
 			var p_no = l_phone.split(",");
 			for(var i=0;i<p_no.length;i++){
 				if(!checkPropertyValueWithOutSubType("email",p_no[i].split(" (")[0].trim())){
-					propertiesArray.push({ "name" : "phone", "value" : p_no[i].split(" (")[0].trim()});
+					propertiesArray.push({ "name" : "phone", "value" : p_no[i].split(" (")[0].trim(),"type" : "SYSTEM"});
 				}
 			}
 		}
@@ -55,7 +55,7 @@ $(function(){
 		}
 		verifyUpdateImgPermission(function(can_update){
 			if(can_update){
-				agile_crm_update_contact_properties(propertiesArray);
+				agile_crm_update_contact_properties_linkedin(propertiesArray);
 			}
 		});
 	},false);
@@ -127,6 +127,74 @@ function checkPropertyValueWithOutSubType(propertyName,propValue){
 	}else{
 		return false;
 	}
+}
+/**
+ * Updates a contact with the list of property name and its value specified in
+ * propertiesArray. If property name already exists with the given then replaces
+ * the value, if property is new then creates a new field and saves it
+ * 
+ * @param propertiesArray
+ *            Array of the properties to be created/updated
+ * @param callback
+ */
+function agile_crm_update_contact_properties_linkedin(propertiesArray, callback){	
+	var contact_model = App_Contacts.contactDetailView.model;
+	var contactId = contact_model.id;
+	var properties = contact_model.toJSON()['properties'];
+	for ( var i in propertiesArray){
+		var flag = false;
+		$.each(properties, function(index, property){
+			if (property.name == propertiesArray[i].name){
+				flag = true;
+				if (propertiesArray[i].subtype){
+					if (propertiesArray[i].subtype == property.subtype)
+						property.value = propertiesArray[i].value;
+					else
+						flag = false;
+				}
+				else
+					property.value = propertiesArray[i].value;
+
+				// break each if match is found
+				return false;
+			}
+		});
+		// If flag is false, given property is new then new field is created
+		if (!flag){
+			if(propertiesArray[i].type != "SYSTEM"){
+				properties.push({ "name" : propertiesArray[i].name, "value" : propertiesArray[i].value, "subtype" : propertiesArray[i].subtype, "type" : "CUSTOM" });
+			}else{
+				properties.push({ "name" : propertiesArray[i].name, "value" : propertiesArray[i].value, "subtype" : propertiesArray[i].subtype, "type" : "SYSTEM" });
+			}
+		}
+
+	}
+	// If property is new then new field is created
+	contact_model.set({ "properties" : properties });
+	// Save model
+	var model = new Backbone.Model();
+	model.url = "core/api/contacts";
+	model.save(contact_model.toJSON(), {
+		success : function(data){
+			if(data){
+				console.log("Contact ID ****** ");				
+				var currentContactId = App_Contacts.contactDetailView.model.id;
+				console.log(currentContactId + " : "+ contactId);
+				if(currentContactId && contactId == currentContactId){
+					console.log("fullcontact Updated **** ");
+					App_Contacts.contactDetailView.model = data;
+
+					var contactDetailsBlock = new Base_Model_View({ 
+						template : "contact-details-block",					
+						data : App_Contacts.contactDetailView.model					
+					});
+
+					var block_el = contactDetailsBlock.render(true).el;
+	      			$('#contact-details-block').html($(block_el)); 
+	      		}
+	      	}
+		}
+	});
 }
 /*function loadframe(){
 	console.log("load frmae valueee");
