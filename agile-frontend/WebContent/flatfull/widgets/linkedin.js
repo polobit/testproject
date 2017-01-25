c09377e8fe3448f6e90627e8298aff07f25dfc6c
rajesh-agilecrm <rajesh@agilecrm.com>
2017-01-25 12:24:56 +0530

    updated for linkedin sync button

--- agile-frontend/WebContent/flatfull/widgets/linkedin.js
+++ agile-frontend/WebContent/flatfull/widgets/linkedin.js
@@ -1,6 +1,6 @@
 $(function(){
 	showLinkedinMatchingProfilesBasedOnName();
-
+	
 	var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
 	var eventer = window[eventMethod];
 	var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
@@ -10,54 +10,58 @@ $(function(){
 	    var key = e.message ? "message" : "data";
 	    var data = e[key];
 	    console.log(data.image_src+"received==="+data.p_link+"====="+data.p_phone+"===="+data.p_email+"===="+data.p_twitter);
-
-	    var contact_image = agile_crm_get_contact_property("image");
-	    var propertiesArray = [{ "name" : "website", "value" : data.p_link, "subtype" : "LINKEDIN","type" : "SYSTEM" }];
-	    var website_url = data.p_url;
-	    if(website_url){
-	    	var w_urls = website_url.split(",");
-	    	for(var i=0;i<w_urls.length;i++){
-	    		if(!checkPropertyValue("website",w_urls[i].trim(),"URL")){
-	    			propertiesArray.push({ "name" : "website", "value" : w_urls[i].trim(),"subtype" : "URL","type" : "SYSTEM"});
-	    		}
-	    	}
-	    }
-	    var twitter_profile = data.p_twitter;
-	    if(twitter_profile){
-	    	var t_profiles = twitter_profile.split(",");
-	    	for(var i=0;i<t_profiles.length;i++){
-	    		if(!checkPropertyValue("website","@"+t_profiles[i].trim(),"TWITTER")){
-	    			propertiesArray.push({ "name" : "website", "value" : "@"+t_profiles[i].trim(),"subtype" : "TWITTER","type" : "SYSTEM"});
-	    		}
-	    	}
-	    }
-	    var l_email = data.p_email;
-	    if(l_email){
-	    	var c_email = l_email.split(",");
-	    	for(var i=0;i<c_email.length;i++){
-		    	if(!checkPropertyValueWithOutSubType("email",c_email[i].split(" (")[0].trim())){
-					propertiesArray.push({ "name" : "email", "value" : c_email[i].split(" (")[0].trim(),"type" : "SYSTEM"});
+	    if(data == "loadsearchpage"){
+	    	var source = "https://www.linkedin.com/search/results/people/?keywords="+$("#contact_name").text().trim()+"&origin=GLOBAL_SEARCH_HEADER";
+			$("#linkedin-iframe").get(0).contentWindow.location.replace(source);
+	    }else{
+		    var contact_image = agile_crm_get_contact_property("image");
+		    var propertiesArray = [{ "name" : "website", "value" : data.p_link, "subtype" : "LINKEDIN" ,"type" : "SYSTEM"}];
+		    var website_url = data.p_url;
+		    if(website_url){
+		    	var w_urls = website_url.split(",");
+		    	for(var i=0;i<w_urls.length;i++){
+		    		if(!checkPropertyValue("website",w_urls[i].trim(),"URL")){
+		    			propertiesArray.push({ "name" : "website", "value" : w_urls[i].trim(),"subtype" : "URL","type" : "SYSTEM"});
+		    		}
+		    	}
+		    }
+		    var twitter_profile = data.p_twitter;
+		    if(twitter_profile){
+		    	var t_profiles = twitter_profile.split(",");
+		    	for(var i=0;i<t_profiles.length;i++){
+		    		if(!checkPropertyValue("website","@"+t_profiles[i].trim(),"TWITTER")){
+		    			propertiesArray.push({ "name" : "website", "value" : "@"+t_profiles[i].trim(),"subtype" : "TWITTER","type" : "SYSTEM"});
+		    		}
+		    	}
+		    }
+		    var l_email = data.p_email;
+		    if(l_email){
+		    	var c_email = l_email.split(",");
+		    	for(var i=0;i<c_email.length;i++){
+			    	if(!checkPropertyValueWithOutSubType("email",c_email[i].split(" (")[0].trim())){
+						propertiesArray.push({ "name" : "email", "value" : c_email[i].split(" (")[0].trim(),"type" : "SYSTEM"});
+					}
 				}
 			}
-		}
-		var l_phone = data.p_phone;
-		if(l_phone){
-			var p_no = l_phone.split(",");
-			for(var i=0;i<p_no.length;i++){
-				if(!checkPropertyValueWithOutSubType("email",p_no[i].split(" (")[0].trim())){
-					propertiesArray.push({ "name" : "phone", "value" : p_no[i].split(" (")[0].trim(),"type" : "SYSTEM"});
+			var l_phone = data.p_phone;
+			if(l_phone){
+				var p_no = l_phone.split(",");
+				for(var i=0;i<p_no.length;i++){
+					if(!checkPropertyValueWithOutSubType("email",p_no[i].split(" (")[0].trim())){
+						propertiesArray.push({ "name" : "phone", "value" : p_no[i].split(" (")[0].trim(),"type" : "SYSTEM"});
+					}
 				}
 			}
-		}
-		if (!contact_image && data.image_src){
-			var linkedin_image = data.image_src;
-			propertiesArray.push({ "name" : "image", "value" : linkedin_image});
-		}
-		verifyUpdateImgPermission(function(can_update){
-			if(can_update){
-				agile_crm_update_contact_properties_linkedin(propertiesArray);
+			if (!contact_image && data.image_src){
+				var linkedin_image = data.image_src;
+				propertiesArray.push({ "name" : "image", "value" : linkedin_image});
 			}
-		});
+			verifyUpdateImgPermission(function(can_update){
+				if(can_update){
+					agile_crm_update_contact_properties_linkedin(propertiesArray);
+				}
+			});
+		}
 	},false);
 });
 /**
