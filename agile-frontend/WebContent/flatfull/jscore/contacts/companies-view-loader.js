7ebcde19c1edb0f49b7320a20abf41a6aa046eba
Subrahmanyam <subrahmanyam@invox.com>
2017-01-31 23:07:15 +0530

    Count issue in contacts, companies fixes

--- agile-frontend/WebContent/flatfull/jscore/contacts/companies-view-loader.js
+++ agile-frontend/WebContent/flatfull/jscore/contacts/companies-view-loader.js
@@ -191,7 +191,7 @@ var companies_view_loader = {
 				count = App_Companies.companiesListView.collection.models[0].attributes.count || App_Companies.companiesListView.collection.models.length;
 			}
 			var count_message = "<small> (" + count + " Total) </small>";
-			if(Current_Route == "companies")
+			if(window.location.hash == "companies")
 			{
 				$('#contacts-count', el).html(count_message);
 			}
