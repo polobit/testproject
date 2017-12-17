68e7c18daccdce1ccf211cc021f359451bea4799
poulami guin <poulamiguin@poulamis-MacBook-Pro.local>
2017-12-17 14:32:03 +0530

    gynoBlog start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,64 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 
+import {
+  MatToolbarModule,
+  MatSidenavModule,
+  MatListModule,
+  MatLineModule,
+  MatButtonModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatSelectModule,
+  MatMenuModule,
+  MatCardModule,
+  MatTabsModule,
+  MatDialogModule,
+  MatButtonToggleModule,
+  MatCheckboxModule,
+  MatTooltipModule,
+  MatNativeDateModule,
+  MatChipsModule
+} from '@angular/material';
+import { HomePageComponent } from './home-page/home-page.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatLineModule,
+    MatCardModule,
+    FormsModule,
+    MatGridListModule,
+    MatSelectModule,
+    MatButtonToggleModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatMenuModule,
+    MatCardModule,
+    MatTabsModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatTooltipModule,
+    // MatExpansionModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
